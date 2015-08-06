ca0b6ac08b7dc45a3d9276510ebb183014750791
Mark Stewart <mark47@uw.edu>
2015-08-06 15:08:48 -0700

    Tweaks to portal UI and wording of login.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,23 @@
 /*** Portal specific javascript. Topnav.js is separate and will be used across domains. **/
+
+function equalHeightBoxes(passClass) {
+    var windowsize = $(window).width();
+    // Switch back to auto for small screen or to recalculate on larger
+    $('.'+passClass).css("height","auto");
+    if (windowsize > 768) {
+        // Get an array of all element heights
+        var elementHeights = $('.'+passClass).map(function() {
+            return $(this).height();
+        }).get();
+        // Math.max takes a variable number of arguments
+        // `apply` is equivalent to passing each height as an argument
+        var maxHeight = Math.max.apply(null, elementHeights);
+        // Set each height to the max height
+        $('.'+passClass).height(maxHeight);
+    }
+}
+
+//$(document).ready(function() {
+//
+//
+//});
