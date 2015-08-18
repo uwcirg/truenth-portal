1356cd8d9f9c1ae11bbb2efcbd82c5f3627a1080
Mark Stewart <mark47@uw.edu>
2015-08-18 15:37:40 -0700

    TrueNTH portal - Text for portal page. Changed overall portal to call topnav in the same way the interventions do.

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -17,6 +17,52 @@ function equalHeightBoxes(passClass) {
     }
 }
 
+// Return an XHR without XHR header so  it doesn't need to be explicitly allowed with CORS
+function xhr_function(){
+    // Get new xhr object using default factory
+    var xhr = jQuery.ajaxSettings.xhr();
+    // Copy the browser's native setRequestHeader method
+    var setRequestHeader = xhr.setRequestHeader;
+    // Replace with a wrapper
+    xhr.setRequestHeader = function(name, value) {
+        // Ignore the X-Requested-With header
+        if (name == 'X-Requested-With') return;
+        // Otherwise call the native setRequestHeader method
+        // Note: setRequestHeader requires its 'this' to be the xhr object,
+        // which is what 'this' is here when executed.
+        setRequestHeader.call(this, name, value);
+    }
+    // pass it on to jQuery
+    return xhr;
+}
+// AJAX callback
+function embed_page(data){
+    $("#mainNav")
+        // Embed data returned by AJAX call into container element
+        .html(data)
+    // Todo: add "data-*" HTML attribute
+}
+$(document).ready(function() {
+    var initial_xhr = $.ajax({
+        url: PORTAL_NAV_PAGE,
+        type:'GET',
+        contentType:'text/plain',
+        //dataFilter:data_filter,
+        //xhr: xhr_function,
+        crossDomain: true
+        //xhrFields: {withCredentials: true},
+    }, 'html')
+        .done(function(data) {
+            embed_page(data);
+            showSearch();
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            alert("Error loading nav elements from " + PORTAL_HOSTNAME);
+        })
+        .always(function() {
+            // alert( "complete" );
+        });
+});
 //$(document).ready(function() {
 //
 //
