911cb1838dcb974dfa1e28712a1f884c89c2b600
Paul Bugni <pbugni@u.washington.edu>
2015-11-17 13:05:04 -0800

    Add cascade for easier deletion of users from database.

---
+++ migrations/versions/577ad345788e_.py
@@ -0,0 +1,35 @@
+"""empty message
+
+Revision ID: 577ad345788e
+Revises: 3b655c2d1a85
+Create Date: 2015-11-17 11:18:22.685983
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '577ad345788e'
+down_revision = '3b655c2d1a85'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    command = """
+    ALTER TABLE auth_providers
+    DROP CONSTRAINT auth_providers_user_id_fkey,
+    ADD CONSTRAINT auth_providers_user_id_fkey
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+    """
+    op.execute(command)
+
+
+def downgrade():
+    command = """
+    ALTER TABLE auth_providers
+    DROP CONSTRAINT auth_providers_user_id_fkey,
+    ADD CONSTRAINT auth_providers_user_id_fkey
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    """
+    op.execute(command)
