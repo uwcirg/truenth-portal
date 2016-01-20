2a98d9f7330e62273a865bf59660767645f7d0e5
Paul Bugni <pbugni@u.washington.edu>
2016-01-20 13:46:51 -0800

    Comma in config makes scalar a tuple.

--- portal/config.py
+++ portal/config.py
@@ -19,7 +19,7 @@ class BaseConfig(object):
     MAIL_DEFAULT_SENDER = '"TrueNTH" <noreply@truenth-demo.cirg.washington.edu'
 
     OAUTH2_PROVIDER_TOKEN_EXPIRES_IN = 60 * 60  # units: seconds
-    PIWIK_DOMAINS = "",
+    PIWIK_DOMAINS = ""
     PIWIK_SITEID = 0
     PROJECT = "portal"
     PROJECT_ROOT =\
