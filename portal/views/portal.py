fc502ba7e1eec60be04b43b2e21c4e3ac1ab4cc7
Justin McReynolds <mcjustin@uw.edu>
2016-03-28 13:36:12 -0700

    Fix to my previous commit a few minutes ago...

--- portal/views/portal.py
+++ portal/views/portal.py
@@ -50,11 +50,12 @@ def home():
 
         # If the user hasn't already answered any upfront questions
         # ask them now - otherwise, off to the portal home..
+        return render_template('portal.html', user=user, interventions=named_interventions())
         # "questions" DISABLED FOR 4/1/16 LAUNCH 
         #for c in (CC.BIOPSY, CC.PCaDIAG, CC.TX):
         #    if user.fetch_values_for_concept(c):
-                return render_template('portal.html', user=user,
-                                       interventions=named_interventions())
+        #        return render_template('portal.html', user=user,
+        #                               interventions=named_interventions())
         #return render_template('questions.html', user=user)
 
     # 'next' is optionally added as a query parameter during login
