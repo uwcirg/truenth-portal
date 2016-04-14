ddbc5c60b4a1525f0adf9a5e3603decd6329c75b
Ivan Cvitkovic <ivanc@uw.edu>
2016-04-14 11:55:17 -0700

    Build assessment initialization URLs from instrument names and client redirect_uris

--- portal/views/assessment_engine.py
+++ portal/views/assessment_engine.py
@@ -8,6 +8,7 @@ import jsonschema
 from ..audit import auditable_event
 from ..models.auth import validate_client_origin
 from ..models.fhir import QuestionnaireResponse
+from ..models.intervention import Intervention, STATIC_INTERVENTIONS
 from ..models.user import current_user
 from ..extensions import oauth
 from ..extensions import db
@@ -1095,6 +1096,8 @@ def present_assessment(instrument_id):
         description: if missing valid OAuth token or bad `next` parameter
 
     """
+    AE_intervention = Intervention.query.filter_by(name='assessment_engine').one()
+
     # Todo: replace with proper models
     instruments = current_app.config['INSTRUMENTS']
     clients_instruments = current_app.config['CLIENTS_INSTRUMENTS']
@@ -1102,9 +1105,12 @@ def present_assessment(instrument_id):
     if not instrument_id or not instrument_id in instruments:
         abort(404, "No matching assessment found: %s" % instrument_id)
 
-    for client_id, instrument_urls in clients_instruments.items():
-        if instrument_id in instrument_urls:
-            assessment_url = instrument_urls[instrument_id]
+    for client_id, instruments in clients_instruments.items():
+        if instrument_id in instruments and client_id == AE_intervention.client_id:
+            assessment_url = "%s/surveys/new_session?project=%s" % (
+                AE_intervention.client.redirect_uris.pop(),
+                instrument_id
+            )
             break
     else:
         abort(404, "No assessment available: %s" % instrument_id)
