ca8aab99f0065611773fa745722edc9638c72bf9
Mark Stewart <mark47@uw.edu>
2016-05-02 17:10:48 -0700

    https://www.pivotaltracker.com/story/show/114367841 - Initial work to add race and ethnicty to profile UI. Some refactoring to move JS functions from profile.html into main.js.

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -41,6 +41,101 @@ function embed_page(data){
         .html(data)
     // Todo: add "data-*" HTML attribute
 }
+
+var getDemo = function(userId) {
+    $.ajax ({
+        type: "GET",
+        url: '/api/demographics/'+userId
+    }).done(function(data) {
+        console.log(data);
+        // Get ethnicity
+        $.each(data.extension[0].valueCodeableConcept.coding,function(i,val){
+            $("#userEthnicity input:radio[value="+val.code+"]").prop('checked', true);
+            // Way to handle non-standard codes
+            if ($("#userEthnicity input:radio[value="+val.code+"]").length == 0) {
+                console.log('other one');
+            }
+        });
+        // Get Races
+        $.each(data.extension[1].valueCodeableConcept.coding,function(i,val){
+            $("#userRace input:checkbox[value="+val.code+"]").prop('checked', true);
+            // Way to handle non-standard codes
+            if ($("#userRace input:checkbox[value="+val.code+"]").length == 0) {
+                console.log('other one');
+            }
+        });
+    }).fail(function() {
+        console.log("Problem retrieving data from server.")
+    });
+};
+
+// Used on profile page to submit user demo data
+var putDemo = function(userId,demoArray,isAdmin) {
+    $.ajax ({
+        type: "PUT",
+        url: '/api/demographics/'+userId,
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        data: JSON.stringify(demoArray)
+    }).done(function() {
+        $("#saveMsg").hide()
+        setTimeout(function(){
+            $("#confirmMsg").fadeIn("slow").delay(3000).fadeOut("slow");
+            //$("#profileIntro").html("<strong>Your changes have been saved.</strong> Return to the <a href='/'>main portal page</a> {% if current_user.has_roles('admin') %} or <a href='/admin'> user administration</a>{% endif %}.");
+            $("#profileForm").removeClass("loading");
+            $("#loadingIndicator").fadeOut();
+        }, 1200);
+    }).fail(function() {
+        alert("There was a problem updating your profile. Please try again.");
+        $("#loadingIndicator").fadeOut();
+        $("#profileForm").removeClass("loading");
+    });
+};
+
+var assembleProfile = function(userId) {
+
+    $("#profileForm").addClass("loading");
+    $("#loadingIndicator").show();
+
+    // Grab profile field values
+    var ethnicityIDs = $("#userEthnicity input:radio:checked").map(function(){
+        return { code: $(this).val(), system: "http://hl7.org/fhir/v3/Ethnicity" };
+    }).get();
+    var raceIDs = $("#userRace input:checkbox:checked").map(function(){
+        return { code: $(this).val(), system: "http://hl7.org/fhir/v3/Race" };
+    }).get();
+    // Testing random code
+    ethnicityIDs.push({code: "2143-6", system: "http://hl7.org/fhir/v3/Ethnicity"});
+
+    // Put form data into FHIR array
+    var demoArray = {};
+    demoArray["resourceType"] = "Patient";
+    demoArray["birthDate"] = $("input[name=birthDate]").val();
+    demoArray["gender"] = $("input[name=sex]:checked").val();
+    demoArray["name"] = {
+        "given": $("input[name=firstname]").val(),
+        "family": $("input[name=lastname]").val()
+    };
+    demoArray["telecom"] = [
+        { "system": "email", "value": $("input[name=email]").val() },
+        { "system": "phone", "value": $("input[name=phone]").val() }
+    ];
+    demoArray["extension"] = [
+        { "url": "http://hl7.org/fhir/StructureDefinition/us-core-ethnicity",
+            "valueCodeableConcept": {
+                "coding": ethnicityIDs
+            }
+        },
+        { "url": "http://hl7.org/fhir/StructureDefinition/us-core-race",
+            "valueCodeableConcept": {
+                "coding": raceIDs
+            }
+        }
+    ];
+    /** Send the AJAX **/
+    putDemo(userId,demoArray);
+}
+
 $(document).ready(function() {
     var initial_xhr = $.ajax({
         url: PORTAL_NAV_PAGE,
