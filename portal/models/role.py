6921d4e8c5e7d3ab40c795e1d0b39f97610b7695
Paul Bugni <pbugni@u.washington.edu>
2016-06-06 15:25:06 -0700

    Added missing __str__ for better logging of role changes.

--- portal/models/role.py
+++ portal/models/role.py
@@ -21,6 +21,8 @@ class Role(db.Model):
     name = db.Column(db.String(50), unique=True)
     description = db.Column(db.Text)
 
+    def __str__(self):
+        return "Role {}".format(self.name)
 
 #Source definition for roles, as dictionary {name: description,}
 STATIC_ROLES = IterableUserDict({
