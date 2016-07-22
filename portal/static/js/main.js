7d861e87b4bb33a762cfebbcd0f9534b9fcfe0ed
Justin McReynolds <mcjustin@uw.edu>
2016-07-22 15:46:15 -0700

    moving tnthDates to main.js

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -593,3 +593,239 @@ $(document).ready(function() {
     }).off('input.bs.validator change.bs.validator'); // Only check on blur (turn off input and change)
 
 });
+
+
+var tnthDates = {
+    /***
+     * changeFormat - changes date format, particularly for submitting to server
+     * @param currentDate - date to change
+     * @param reverse - use to switch from yyyy-mm-dd to mm/dd/yyyy
+     * @param shorten - removes padding from zeroes (only in reverse)
+     * @returns - a date as a string
+     *
+     * If language is es_MX or en_AU then uses dd/mm/yyyy format
+     *
+     * Examples:
+     * changeFormat("04/29/2016") returns "2016-04-29"
+     * changeFormat("2016-04-29",true) returns "04/29/2016"
+     * changeFormat("2016-04-29",true,true) returns "4/29/2016"
+     ***/
+    "changeFormat": function(currentDate,reverse,shorten) {
+        if (currentDate == null || currentDate == "") {
+            return null;
+        }
+        var yearToPass, convertDate, dateFormatArray;
+        if (reverse) {
+            dateFormatArray = currentDate.split("-");
+            yearToPass = dateFormatArray[0];
+            if (shorten) {
+                dateFormatArray[1] = dateFormatArray[1].replace(/^0+/, '');
+                dateFormatArray[2] = dateFormatArray[2].replace(/^0+/, '');
+            }
+            if (userSetLang == 'es_MX' || userSetLang == 'en_AU') {
+                convertDate = dateFormatArray[2]+"/"+dateFormatArray[1]+"/"+yearToPass;
+            } else {
+                convertDate = dateFormatArray[1]+"/"+dateFormatArray[2]+"/"+yearToPass;
+            }
+        } else {
+            dateFormatArray = currentDate.split("/");
+            // If patient manuals enters two digit year, then add 19 or 20 to year.
+            // TODO - this is susceptible to Y2K for 2100s. Be better to force
+            // user to type 4 digits.
+            if (dateFormatArray[2].length == 2) {
+                var currentTime = new Date();
+                var shortYear = currentTime.getFullYear().toString().substr(2,2);
+                if (dateFormatArray[2] > shortYear) {
+                    yearToPass = '19'+dateFormatArray[2];
+                } else {
+                    yearToPass = '20'+dateFormatArray[2];
+                }
+            } else {
+                yearToPass = dateFormatArray[2];
+            }
+            if (userSetLang == 'es_MX' || userSetLang == 'en_AU') {
+                convertDate = yearToPass+"-"+dateFormatArray[1]+"-"+dateFormatArray[0]
+            } else {
+                convertDate = yearToPass+"-"+dateFormatArray[0]+"-"+dateFormatArray[1]
+            }
+        }
+        return convertDate
+    },
+    /***
+     * parseDate - Fancier function for changing javascript date yyyy-mm-dd (with optional time) to a mm/dd/yyyy (optional time) format. Used with mPOWEr
+     * @param date - the date to be converted
+     * @param noReplace - prevent replacing any spaces with "T" to get in proper javascript format. 2016-02-24 15:28:09-0800 becomes 2016-02-24T15:28:09-0800
+     * @param padZero - if true, will add padded zero to month and date
+     * @param keepTime - if true, will output the time as part of the date
+     * @param blankText - pass a value to display if date is null
+     * @returns date as a string with optional time
+     *
+     * parseDate("2016-02-24T15:28:09-0800",true,false,true) returns "2/24/2016 3:28pm"
+     */
+    "parseDate": function(date,noReplace,padZero,keepTime,blankText) {
+        if(date == null) {
+            if (blankText) {
+                return blankText;
+            } else {
+                return "";
+            }
+        }
+        // Put date in proper javascript format
+        if (noReplace == null) {
+            date = date.replace(" ", "T");
+        }
+        // Need to reformat dates b/c of date format issues in Safari (and others?)
+        // http://stackoverflow.com/questions/6427204/date-parsing-in-javascript-is-different-between-safari-and-chrome
+        var a = date.split(/[^0-9]/);
+        var toConvert;
+        if (a[3]) {
+            toConvert=new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5]);
+        } else {
+            toConvert=new Date (a[0],a[1]-1,a[2]);
+        }
+
+        // Switch date to mm/dd/yyyy
+        //var toConvert = new Date(Date.parse(date));
+        var month = toConvert.getMonth() + 1;
+        var day = toConvert.getDate();
+        if (padZero) {
+            if (month <= 9)
+                month = '0' + month;
+            if (day <= 9)
+                day = '0' + day;
+        }
+        if (keepTime) {
+            var amPm = "am";
+            var hour = a[3];
+            if (a[3] > 11) {
+                amPm = "pm";
+                if (a[3] > 12) {
+                    hour = (a[3]-12);
+                }
+            }
+            return month + "/" + day + "/" + toConvert.getFullYear()+" "+hour+":"+a[4]+amPm;
+        } else {
+            return month + "/" + day + "/" + toConvert.getFullYear();
+        }
+    },
+    /***
+     * parseForSorting - changes date to a YYYYMMDDHHMMSS string for sorting (note that this is a string rather than a number)
+     * @param date - the date to be converted
+     * @param noReplace - prevent replacing any spaces with "T" to get in proper javascript format. 2016-02-24 15:28:09-0800 becomes 2016-02-24T15:28:09-0800. Adding T indicates UTC time
+     * @returns date as a string used by system for sorting
+     *
+     * parseDate("2016-02-24T15:28:09-0800",true) returns "201600224152809"
+     */
+    "parseForSorting": function(date,noReplace) {
+        if (date == null) {
+            return ""
+        }
+        // Put date in proper javascript format
+        if (noReplace == null) {
+            date = date.replace(" ", "T");
+        }
+        // Need to reformat dates b/c of date format issues in Safari (and others?)
+        // http://stackoverflow.com/questions/6427204/date-parsing-in-javascript-is-different-between-safari-and-chrome
+        var a = date.split(/[^0-9]/);
+        var toConvert=new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5]);
+        // Switch date to mm/dd/yyyy
+        //var toConvert = new Date(Date.parse(date));
+        var month = toConvert.getMonth() + 1;
+        var day = toConvert.getDate();
+        if (month <= 9)
+            month = '0' + month;
+        if (day <= 9)
+            day = '0' + day;
+        return toConvert.getFullYear() + month + day + a[3] + a[4] + a[5]
+
+    },
+    /***
+     * spellDate - spells out date in a format based on language/local. Currently not in use.
+     * @param passDate - date to use. If empty, defaults to today.
+     * @param ymdFormat - false by default. false = mm/dd/yyyy. true = yyyy-mm-dd
+     * @returns spelled out date, localized
+     */
+    "spellDate": function(passDate,ymdFormat) {
+        var todayDate = new Date();
+        if (passDate) {
+            // ymdFormat is true, we are assuming it's being received as YYYY-MM-DD
+            if (ymdFormat) {
+                todayDate = passDate.split("-");
+                todayDate = new Date(todayDate[2], todayDate[0] - 1, todayDate[1])
+            } else {
+                // Otherwide mm/dd/yyyy
+                todayDate = passDate.split("/");
+                todayDate = new Date(todayDate[2], todayDate[0] - 1, todayDate[1])
+            }
+        }
+        var returnDate;
+        var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ]
+        // If user's language is Spanish then use dd/mm/yyyy format and changes words
+        if (userSetLang == 'es_MX') {
+            monthNames = ["enero","febrero","marzo","abril","mayo","junio","julio", "agosto","septiembre","octubre","noviembre","diciembre"];
+            returnDate = ('0' + todayDate.getDate()).slice(-2)+" de "+monthNames[todayDate.getMonth()]+" de "+todayDate.getFullYear()
+        } else if(userSetLang == "en_AU") {
+            returnDate = ('0' + todayDate.getDate()).slice(-2)+" "+monthNames[todayDate.getMonth()]+" "+todayDate.getFullYear()
+        } else {
+            returnDate = monthNames[todayDate.getMonth()]+" "+('0' + todayDate.getDate()).slice(-2)+", "+todayDate.getFullYear()
+        }
+        return returnDate
+    },
+    /***
+     * Calculates number of days between two dates. Used in mPOWEr for surgery/discharge
+     * @param startDate - required. Assumes YYYY-MM-DD. This is typically the date of surgery or discharge
+     * @param dateToCalc - optional. If empty, then assumes today's date
+     * @returns number of days
+     */
+    "getDateDiff": function(startDate,dateToCalc) {
+        var a = startDate.split(/[^0-9]/);
+        var dateTime = new Date(a[0], a[1]-1, a[2]).getTime();
+        var d;
+        if (dateToCalc) {
+            var c = dateToCalc.split(/[^0-9]/);
+            d = new Date(c[0], c[1]-1, c[2]).getTime()
+        } else {
+            // If no baseDate, then use today to find the number of days between dateToCalc and today
+            d = new Date().getTime()
+        }
+        // Round down to floor so we don't add an extra day if session is 12+ hours into the day
+        return Math.floor((d - dateTime) / (1000 * 60 * 60 * 24))
+    },
+    "getAge": function (birthDate, otherDate) {
+        birthDate = new Date(birthDate);
+        // Use today's date to calc, unless otherwise specified
+        var secondDate = new Date();
+        if (otherDate) {
+            secondDate = new Date(otherDate);
+        }
+        var years = (secondDate.getFullYear() - birthDate.getFullYear());
+
+        if (secondDate.getMonth() < birthDate.getMonth() ||
+            secondDate.getMonth() == birthDate.getMonth() && secondDate.getDate() < birthDate.getDate()) {
+            years--;
+        }
+        return years;
+    },
+    /***
+     * Simple function to add "days" label to a number of days. Not localized, used for mPOWEr
+     * @param dateVal - required. Often derived via getDateDiff
+     * @returns {string}
+     */
+    "addDays": function(dateVal) {
+        var toReturn = "N/A";
+        if (dateVal && typeof dateVal != undefined) {
+            if (dateVal == 1) {
+                toReturn = "1 day";
+            } else if (dateVal < 0) {
+                toReturn = "--";
+            } else {
+                toReturn = dateVal + " days";
+            }
+        } else if (dateVal === 0) {
+            toReturn = "Today";
+        }
+        return toReturn
+    }
+}
