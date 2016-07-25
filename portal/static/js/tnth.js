d7f422b9a378a7de5a8ab23aa0c25b56d5b2ed5c
Mark Stewart <mark47@uw.edu>
2016-07-25 16:45:56 -0700

    https://www.pivotaltracker.com/story/show/114240469 - Trying to avoid any menu flashes during loading of TrueNTH topnav.

--- portal/static/js/tnth.js
+++ portal/static/js/tnth.js
@@ -157,4 +157,8 @@ $('.tnth-navbar-toggle').click(function(){
   $('#tnthNavbarXs').slideToggle('fast');
 });
 
+$(document).ready(function(){
+  // Once nav is loaded, make the wrapper visible
+  $("#tnthNavWrapper").css("visibility","visible");
+});
 var userSetLang = 'en_US';// FIXME scope? defined in both tnth.js/banner and main.js
