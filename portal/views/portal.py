e58b1c3977710f4b881aedab6d7f8628dc594ffb
Justin McReynolds <mcjustin@uw.edu>
2016-07-27 23:34:36 -0700

    debugging Liferay CDATA xml parsing

--- portal/views/portal.py
+++ portal/views/portal.py
@@ -1,7 +1,6 @@
 """Portal view functions (i.e. not part of the API or auth)"""
 import requests
-import xml.etree.ElementTree
-#from lxml import etree
+from lxml import etree
 from flask import current_app, Blueprint, jsonify, render_template
 from flask import abort, redirect, request, session, url_for
 from flask_login import login_user
@@ -181,10 +180,7 @@ def profile_test(user_id):
 def legal():
     """ Legal/terms of use page"""
     content = requests.get('https://stg-cms.us.truenth.org/c/journal/get_latest_article_content?groupId=20182&articleId=43478', verify=False)
-    tree = ElementTree.parse(content)
-    cdata = tree.Element('![CDATA[')
-    return render_template('legal.html', content=cdata.text)
-#    return render_template('legal.html', content=content)
+    return render_template('legal.html', content=content)
 
 @portal.route('/about')
 def about():
