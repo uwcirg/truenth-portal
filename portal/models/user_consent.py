4d52c676e041229614d4dbe845259c9d114e3714
Paul Bugni <pbugni@u.washington.edu>
2016-08-29 09:25:19 -0700

    Implementation for #147793313 on the database / API side.  New user_consent table captures when a user or an authority adds a consent for data sharing (or other purposes) to the system.

---
+++ portal/models/user_consent.py
@@ -0,0 +1,63 @@
+"""User Consent module"""
+from datetime import datetime, timedelta
+from validators import url as url_validation
+
+from .audit import Audit
+from ..extensions import db
+from .fhir import FHIR_datetime
+from .organization import Organization
+from .user import User
+
+def default_expires():
+    """5 year from now, in UTC"""
+    return datetime.utcnow() + timedelta(days=365*5)
+
+
+class UserConsent(db.Model):
+    """ORM class for user_consent data
+
+    Capture data when user consents to share data with an organization.
+
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.ForeignKey('users.id'), nullable=False)
+    organization_id = db.Column(
+        db.ForeignKey('organizations.id'), nullable=False)
+    audit_id = db.Column(db.ForeignKey('audit.id'), nullable=False)
+    expires = db.Column(db.DateTime, default=default_expires, nullable=False)
+    agreement_url = db.Column(db.Text, nullable=False)
+
+    audit = db.relationship(Audit, cascade="save-update")
+    organization = db.relationship(Organization, cascade="save-update")
+
+    def __str__(self):
+        return ("user_consent at {0.agreement_url} between {0.user_id} and "
+                "{0.organization_id}".format(self))
+
+    def as_json(self):
+        d = {}
+        d['user_id'] = self.user_id
+        d['organization_id'] = self.organization_id
+        d['signed'] = FHIR_datetime.as_fhir(self.audit.timestamp)
+        d['expires'] = FHIR_datetime.as_fhir(self.expires)
+        d['agreement_url'] = self.agreement_url
+
+        return d
+
+    @classmethod
+    def from_json(cls, data):
+        user = User.query.get(data.get('user_id'))
+        if not user:
+            raise ValueError("required user_id not found")
+        org = Organization.query.get(data.get('organization_id'))
+        if not org:
+            raise ValueError("required organization_id not found")
+        url = data.get('agreement_url')
+        try:
+            url_validation(url)
+        except:
+            raise ValueError("requires a valid agreement_url")
+
+        return cls(user_id=data['user_id'],
+                   organization_id=data['organization_id'],
+                   agreement_url=data['agreement_url'])
---
+++ tests/test_consent.py
@@ -0,0 +1,52 @@
+"""Unit test module for user consent"""
+from flask_webtest import SessionScope
+import json
+
+from portal.extensions import db
+from portal.models.audit import Audit
+from portal.models.organization import Organization
+from portal.models.user_consent import UserConsent
+from tests import TestCase, TEST_USER_ID
+
+
+class TestUserConsent(TestCase):
+    url = 'http://fake.com?arg=critical'
+
+    def test_user_consent(self):
+        org1 = Organization(name='test_1')
+        org2 = Organization(name='test_2')
+
+        audit = Audit(user_id=TEST_USER_ID)
+        uc1 = UserConsent(organization=org1, agreement_url=self.url,
+                          audit=audit)
+        uc2 = UserConsent(organization=org2, agreement_url=self.url,
+                          audit=audit)
+        self.test_user.consents.append(uc1)
+        self.test_user.consents.append(uc2)
+        with SessionScope(db):
+            db.session.commit()
+        self.test_user = db.session.merge(self.test_user)
+        self.login()
+        rv = self.app.get('/api/user/{}/consent'.format(TEST_USER_ID))
+        self.assert200(rv)
+        self.assertEquals(len(rv.json['consent_agreements']), 2)
+
+    def test_post_user_consent(self):
+        org1 = Organization(name='test_1')
+        with SessionScope(db):
+            db.session.add(org1)
+            db.session.commit()
+        self.test_user = db.session.merge(self.test_user)
+        org1 = db.session.merge(org1)
+
+        data = {'user_id': TEST_USER_ID,
+                'organization_id': org1.id,
+                'agreement_url': self.url}
+
+        self.login()
+        rv = self.app.post('/api/user/{}/consent'.format(TEST_USER_ID),
+                          content_type='application/json',
+                          data=json.dumps(data))
+        self.assert200(rv)
+        self.assertEqual(self.test_user.consents.count(), 1)
+        self.assertEqual(self.test_user.consents[0].organization_id, org1.id)
