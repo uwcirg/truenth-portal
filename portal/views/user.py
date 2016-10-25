a00862105a66e92613be9807ad7cb21cad56f05e
Amy Chen <achen2401@gmail.com>
2016-10-25 09:57:04 -0700

    account creation changes without write-only role

--- portal/views/user.py
+++ portal/views/user.py
@@ -180,10 +180,10 @@ def access_url(user_id):
     if not has.isdisjoint(not_allowed):
         abort(400, "Access URL not provided for privileged accounts")
 
-    if not ROLE.WRITE_ONLY in has:
+    #if not ROLE.WRITE_ONLY in has:
         # KEEP this restriction.  Weak authentication (which the
         # returned URL provides) should only be available for WRITE_ONLY users
-        abort(400, "Account {} lacks WRITE_ONLY role".format(user_id))
+    #    abort(400, "Account {} lacks WRITE_ONLY role".format(user_id))
 
     # generate an access token
     token = user_manager.token_manager.generate_token(user_id)
