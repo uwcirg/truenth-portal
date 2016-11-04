508d9b03b25f37515a588c4b9766513e684bab52
Justin McReynolds <mcjustin@uw.edu>
2016-11-04 14:04:50 -0700

    Using random dates (instead of hard-coded strings) in /patients tables' Status and Next Due columns

--- portal/views/patients.py
+++ portal/views/patients.py
@@ -1,6 +1,7 @@
 """Patient view functions (i.e. not part of the API or auth)"""
 import requests
-from datetime import datetime
+import random 
+from datetime import datetime timedelta
 from flask import abort, Blueprint, render_template
 from flask_user import roles_required
 from sqlalchemy import and_
