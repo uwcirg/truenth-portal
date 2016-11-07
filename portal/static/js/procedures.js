30473dbbcecdb00645303be7927f311b14e3be86
Justin McReynolds <mcjustin@uw.edu>
2016-11-07 13:17:37 -0800

    Trying a simpler format for the procedures datepicker (dd/mm/yyyy' instead of 'd/M/yyyy', since the latter doesn't seem to be functional)

--- portal/static/js/procedures.js
+++ portal/static/js/procedures.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
     // Options for datepicker - prevent future dates, no default
     $('.event-element .input-group.date').each(function(){
         $(this).datepicker({
-            format: 'd/M/yyyy',
+            format: 'dd/mm/yyyy',
             endDate: "0d",
             startDate: "-10y",
             autoclose: true
