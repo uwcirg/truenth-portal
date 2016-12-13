998fc6de83e8f0bb6fccf2c16e2ad81eac0b0073
Paul Bugni <pbugni@u.washington.edu>
2016-12-13 14:51:38 -0800

    Extending ToU api to allow users to accept ToU on behalf of others

--- portal/views/tou.py
+++ portal/views/tou.py
@@ -48,9 +48,62 @@ def get_tou(user_id):
     return jsonify(accepted=False)
 
 
+@tou_api.route('/user/<user_id>/tou/accepted', methods=('POST',))
+@oauth.require_oauth()
+def post_user_accepted_tou(user_id):
+    """Accept Terms of Use on behalf of user
+
+    POST simple JSON describing ToU the user accepted for persistence.  This
+    endpoint enables service or other users to set accepted ToU on behalf of
+    any user they have permission to edit.
+
+    ---
+    tags:
+      - Terms Of Use
+    operationId: userAcceptToU
+    produces:
+      - application/json
+    parameters:
+      - name: user_id
+        in: path
+        description: TrueNTH user ID
+        required: true
+        type: integer
+        format: int64
+      - name: body
+        in: body
+        schema:
+          id: acceptedToU
+          description: Details of accepted ToU
+          required:
+            - text
+          properties:
+            text:
+              description: Full text agreed to
+              type: string
+    responses:
+      200:
+        description: message detailing success
+      400:
+        description: if the required JSON is ill formed
+      401:
+        description:
+          if missing valid OAuth token or logged-in user lacks permission
+          to edit requested user
+
+    """
+    authd_user = current_user()
+    authd_user.check_role(permission='edit', other_id=user_id)
+    audit = Audit(user_id = authd_user.id,
+                  comment = "user {} posting accepted ToU for user {}".format(
+                      authd_user.id, user_id))
+    db.session.add(audit)
+    return accept_tou(user_id)
+
+
 @tou_api.route('/tou/accepted', methods=('POST',))
 @oauth.require_oauth()
-def accept_tou():
+def accept_tou(user_id=None):
     """Accept Terms of Use info for authenticated user
 
     POST simple JSON describing ToU the user accepted for persistence.
@@ -81,10 +134,13 @@ def accept_tou():
       401:
         description:
           if missing valid OAuth token or logged-in user lacks permission
-          to view requested patient
+          to edit requested user
 
     """
-    user = current_user()
+    if user_id:
+        user=get_user(user_id)
+    else:
+        user = current_user()
     if not request.json or 'text' not in request.json:
         abort(400, "Requires JSON with the ToU 'text'")
     audit = Audit(user_id = user.id, comment = "ToU accepted")
--- tests/test_tou.py
+++ tests/test_tou.py
@@ -8,6 +8,26 @@ from portal.models.audit import Audit
 from portal.models.tou import ToU
 
 
+tou_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Sed gravida, urna sed faucibus laoreet, turpis sapien euismod turpis, a
+feugiat velit neque sed ligula. Sed magna risus, tincidunt a mauris ut,
+finibus ultricies justo. Phasellus lobortis, dui fringilla dignissim
+ornare, risus augue pharetra odio, ac vulputate lacus leo at odio.
+Donec lobortis pellentesque dapibus. Quisque et convallis elit, in
+placerat nibh. Donec ac lacus eu justo lobortis sollicitudin vel eget
+mi. Morbi placerat egestas elit sit amet tempus. Nam sodales mattis
+nisi gravida dignissim.
+
+Donec sed odio quis justo porttitor auctor. Aliquam nisl enim, faucibus
+eget tristique non, tincidunt eget neque. Suspendisse in ex eu sem
+ultricies euismod vel dapibus lorem. Vivamus tortor metus, laoreet
+vulputate ornare eu, lacinia et enim. Duis sodales, mi scelerisque
+laoreet egestas, dui enim commodo ex, in posuere magna nisi eu lacus.
+Integer id libero in enim volutpat auctor. Proin sed lacinia lacus.
+Nulla facilisi. Nulla facilisi. Integer purus diam, gravida quis neque
+sit amet, laoreet imperdiet risus."""
+
+
 class TestTou(TestCase):
     """Terms Of Use tests"""
 
@@ -18,28 +38,21 @@ class TestTou(TestCase):
         self.assertTrue('Your data is safe' in results)
 
     def test_accept(self):
-        tou_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Sed gravida, urna sed faucibus laoreet, turpis sapien euismod turpis, a
-        feugiat velit neque sed ligula. Sed magna risus, tincidunt a mauris ut,
-        finibus ultricies justo. Phasellus lobortis, dui fringilla dignissim
-        ornare, risus augue pharetra odio, ac vulputate lacus leo at odio.
-        Donec lobortis pellentesque dapibus. Quisque et convallis elit, in
-        placerat nibh. Donec ac lacus eu justo lobortis sollicitudin vel eget
-        mi. Morbi placerat egestas elit sit amet tempus. Nam sodales mattis
-        nisi gravida dignissim.
-
-        Donec sed odio quis justo porttitor auctor. Aliquam nisl enim, faucibus
-        eget tristique non, tincidunt eget neque. Suspendisse in ex eu sem
-        ultricies euismod vel dapibus lorem. Vivamus tortor metus, laoreet
-        vulputate ornare eu, lacinia et enim. Duis sodales, mi scelerisque
-        laoreet egestas, dui enim commodo ex, in posuere magna nisi eu lacus.
-        Integer id libero in enim volutpat auctor. Proin sed lacinia lacus.
-        Nulla facilisi. Nulla facilisi. Integer purus diam, gravida quis neque
-        sit amet, laoreet imperdiet risus."""
-
         self.login()
         data = {'text': tou_text}
         rv = self.app.post('/api/tou/accepted',
+                           content_type='application/json',
+                           data=json.dumps(data))
+        self.assert200(rv)
+        tou = ToU.query.one()
+        self.assertEquals(tou.text, tou_text)
+        self.assertEquals(tou.audit.user_id, TEST_USER_ID)
+
+    def test_service_accept(self):
+        service_user = self.add_service_user()
+        self.login(user_id=service_user.id)
+        data = {'text': tou_text}
+        rv = self.app.post('/api/user/{}/tou/accepted'.format(TEST_USER_ID),
                            content_type='application/json',
                            data=json.dumps(data))
         self.assert200(rv)
