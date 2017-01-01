b011e2775134b0643334b3e8e0b0c7e79ff7de21
Ivan Cvitkovic <ivanc@uw.edu>
2017-01-01 03:12:37 -0800

    Allow CELERY_BROKER_URL to be set from env var

--- portal/config.py
+++ portal/config.py
@@ -6,7 +6,10 @@ from flask_script import Server
 class BaseConfig(object):
     """Base configuration - override in subclasses"""
     ANONYMOUS_USER_ACCOUNT = True
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_BROKER_URL = os.environ.get(
+        'CELERY_BROKER_URL',
+        'redis://localhost:6379/0'
+    )
     CELERY_IMPORTS = ('portal.tasks', )
     CELERY_RESULT_BACKEND = 'redis'
     DEBUG = False
