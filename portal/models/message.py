c65e8ad9043f0a364a7cad1beb95aa7e5f2e5d37
OptimusRhine <adamrhine@gmail.com>
2017-01-12 12:02:36 -0800

    longer line width

--- portal/models/message.py
+++ portal/models/message.py
@@ -32,7 +32,7 @@ class EmailMessage(db.Model):
         message = Message(subject=self.subject,
                 sender=current_app.config['DEFAULT_MAIL_SENDER'],
                 recipients=self.recipients.split())
-        message.html = fill(self.body, width=100)
+        message.html = fill(self.body, width=180)
         mail.send(message)
 
     def __str__(self):
