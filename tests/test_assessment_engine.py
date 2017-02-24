199a9a383a954673905ff66b00a100aa85425a6a
Ivan Cvitkovic <ivanc@uw.edu>
2017-02-24 12:33:13 -0800

    Add QuestionnaireResponse bundle test

--- tests/test_assessment_engine.py
+++ tests/test_assessment_engine.py
@@ -20,3 +20,24 @@ class TestAssessmentEngine(TestCase):
         response = rv.json
         self.assertEquals(response['ok'], True)
         self.assertEquals(response['valid'], True)
+
+    def test_assessments_bundle(self):
+        swagger_spec = swagger(self.app)
+        example_data = swagger_spec['definitions']['QuestionnaireResponse']['example']
+        instrument_id = example_data['questionnaire']['reference'].split('/')[-1]
+
+        self.login()
+        upload = self.client.put(
+            '/api/patient/{}/assessment'.format(TEST_USER_ID),
+            content_type='application/json',
+            data=json.dumps(example_data),
+        )
+
+        rv = self.client.get(
+            '/api/patient/assessment?instrument_id={}'.format(instrument_id),
+            content_type='application/json',
+        )
+        response = rv.json
+
+        self.assertEquals(response['total'], len(response['entry']))
+        self.assertTrue(response['entry'][0]['questionnaire']['reference'].endswith(instrument_id))
