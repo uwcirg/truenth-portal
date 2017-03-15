3ea3586271006dd690bf98585d458faf0025a8b0
Paul Bugni <pbugni@u.washington.edu>
2017-03-14 18:03:54 -0700

    Build SQLALCHEMY_DATABASE_URI from best available environment variables.

--- portal/config.py
+++ portal/config.py
@@ -4,6 +4,24 @@ import redis
 from flask_script import Server
 
 
+def best_sql_url():
+    """Return compliant sql url from available enviornment variables"""
+    env = os.environ
+    if 'PGDATABASE' in env:
+        return (
+            'postgresql://{PGUSER}:{PGPASSWORD}@{PGHOST}/{PGDATABASE}'.format(
+                PGUSER=env.get('PGUSER'), PGPASSWORD=env.get('PGPASSWORD'),
+                PGHOST=env.get('PGHOST', 'localhost'),
+                PGDATABASE=env.get('PGDATABASE')))
+    elif 'SQLALCHEMY_DATABASE_URI' in env:
+        return env.get('SQLALCHEMY_DATABASE_URI')
+    else:
+        return env.get(
+            'DATABASE_URL',
+            'postgresql://test_user:4tests_only@localhost/portal_unit_tests'
+        )
+
+
 class BaseConfig(object):
     """Base configuration - override in subclasses"""
 
@@ -49,13 +67,7 @@ class BaseConfig(object):
     SHOW_PROFILE_MACROS = ['ethnicity', 'race']
     SHOW_WELCOME = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI',
-        os.environ.get(
-            'DATABASE_URL',
-            'postgresql://test_user:4tests_only@localhost/portal_unit_tests'
-        )
-    )
+    SQLALCHEMY_DATABASE_URI = best_sql_url()
     SECRET_KEY = 'override this secret key'
     SESSION_PERMANENT = True
     SESSION_TYPE = 'redis'
