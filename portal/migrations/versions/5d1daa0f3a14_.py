13bc5999598839404d0b050d05fadfc92217e9ab
Ivan Cvitkovic <ivanc@uw.edu>
2017-05-03 16:50:56 -0700

    Merge alembic migration HEADs

---
+++ portal/migrations/versions/5d1daa0f3a14_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 5d1daa0f3a14
+Revises: ('58a9d999ece9', '418982ee27e2')
+Create Date: 2017-05-03 16:50:12.498677
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5d1daa0f3a14'
+down_revision = ('58a9d999ece9', '418982ee27e2')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
