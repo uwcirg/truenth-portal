76a7bf9cbc69ed583d3f26dab34aeb32e0f7abb2
Ivan Cvitkovic <ivanc@uw.edu>
2017-05-08 12:30:55 -0700

    Explicitly set desired app.run() options

--- manage.py
+++ manage.py
@@ -30,8 +30,12 @@ migrate = Migrate(app, db, directory=MIGRATIONS_DIR)
 def runserver():
     # Todo: figure out how to override default host in `flask run`
     # http://click.pocoo.org/5/commands/#overriding-defaults
-    app.run(host='0.0.0.0')
-
+    app.run(
+        host='0.0.0.0',
+        threaded=True,
+        use_debugger=True,
+        use_reloader=True,
+    )
 def _run_alembic_command(args):
     """Helper to manage working directory and run given alembic commands"""
     # Alembic looks for the alembic.ini file in CWD
