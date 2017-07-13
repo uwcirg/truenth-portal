fac3a34d8e49ec7389da6926e89a99476c5f04d6
Paul Bugni <pbugni@u.washington.edu>
2017-07-13 14:47:46 -0700

    Implement SitePersistence on recurring QuestionnaireBanks

--- tests/test_site_persistence.py
+++ tests/test_site_persistence.py
@@ -10,6 +10,8 @@ from datetime import datetime
 from flask_webtest import SessionScope
 import os
 from tests import TestCase, TEST_USER_ID
+from tests.test_assessment_status import  mock_questionnairebanks
+from tempfile import NamedTemporaryFile
 
 from portal.extensions import db
 from portal.site_persistence import SitePersistence
@@ -19,6 +21,10 @@ from portal.models.encounter import Encounter
 from portal.models.fhir import CC
 from portal.models.intervention import INTERVENTION
 from portal.models.organization import Organization
+from portal.models.questionnaire import Questionnaire
+from portal.models.questionnaire_bank import QuestionnaireBank
+from portal.models.questionnaire_bank import QuestionnaireBankQuestionnaire
+from portal.models.recur import Recur
 from portal.models.role import ROLE
 from portal.models.user import get_user
 
@@ -37,6 +43,11 @@ class TestSitePersistence(TestCase):
         self.app.config['PERSISTENCE_FILE'] = known_good_persistence_file
         SitePersistence().import_(
             exclude_interventions=False, keep_unmentioned=False)
+
+    def tearDown(self):
+        if hasattr(self, 'tmpfile') and self.tmpfile:
+            os.remove(self.tmpfile)
+            del self.tmpfile
 
     def testOrgs(self):
         """Confirm persisted organizations came into being"""
@@ -105,3 +116,78 @@ class TestSitePersistence(TestCase):
 
     def test_app_text(self):
         self.assertEquals(app_text('landing title'), 'Welcome to TrueNTH')
+
+    def test_questionnaire_banks_recurs(self):
+        # set up a few recurring instances
+        initial_recur = Recur(
+            days_to_start=90, days_in_cycle=90,
+            days_till_termination=720)
+        every_six_thereafter = Recur(
+            days_to_start=720, days_in_cycle=180)
+
+        metastatic_org = Organization(name='metastatic')
+        questionnaire = Questionnaire(name='test_q')
+        with SessionScope(db):
+            db.session.add(initial_recur)
+            db.session.add(every_six_thereafter)
+            db.session.add(metastatic_org)
+            db.session.add(questionnaire)
+            db.session.commit()
+
+        initial_recur = db.session.merge(initial_recur)
+        every_six_thereafter = db.session.merge(every_six_thereafter)
+        metastatic_org_id = db.session.merge(metastatic_org).id
+
+        # with bits in place, setup a recurring QB
+        mr_qb = QuestionnaireBank(
+            name='metastatic_recurring',
+            classification='recurring',
+            organization_id=metastatic_org_id)
+        questionnaire = db.session.merge(questionnaire)
+        recurs = [initial_recur, every_six_thereafter]
+
+        qbq = QuestionnaireBankQuestionnaire(
+            questionnaire=questionnaire,
+            days_till_due=1, days_till_overdue=30,
+            rank=1, recurs=recurs)
+        mr_qb.questionnaires.append(qbq)
+
+        # confirm persistence of this questionnaire bank includes the bits
+        # added above
+        results = mr_qb.as_json()
+
+        copy = QuestionnaireBank.from_json(results)
+        self.assertEquals(copy.name, mr_qb.name)
+        copy_q = copy.questionnaires[0]
+        self.assertEquals(copy_q.recurs, [initial_recur, every_six_thereafter])
+
+        # now, modify the persisted form, remove one recur and add another
+        new_recur = Recur(
+            days_to_start=900, days_in_cycle=180, days_till_termination=1800)
+        results['questionnaires'][0]['recurs'] = [
+            initial_recur.as_json(), new_recur.as_json()]
+        updated_copy = QuestionnaireBank.from_json(results)
+
+        self.assertEquals(
+            [r.as_json() for r in updated_copy.questionnaires[0].recurs],
+            [r.as_json() for r in (initial_recur, new_recur)])
+
+    def test_questionnaire_banks(self):
+        mock_questionnairebanks()
+
+        def mock_file(read_only=True):
+            '''mock version to create local testfile for site_persistence'''
+            if not hasattr(self, 'tmpfile'):
+                with NamedTemporaryFile(mode='w', delete=False) as tmpfile:
+                    self.tmpfile = tmpfile.name
+            return self.tmpfile
+
+        sp = SitePersistence()
+        sp.persistence_filename = mock_file
+        sp.export()
+        with open(self.tmpfile) as f:
+            data = f.read()
+        self.assertTrue('recur' in data)
+
+        # Pull same data back in
+        sp.import_(exclude_interventions=True, keep_unmentioned=False)
