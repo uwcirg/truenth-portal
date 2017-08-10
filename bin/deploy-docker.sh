1b7b3166745844cd21bb7099684ed27ef6a9b295
Ivan Cvitkovic <ivanc@uw.edu>
2017-08-10 16:49:51 -0700

    Improve comments

--- bin/deploy-docker.sh
+++ bin/deploy-docker.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
-# Abort script at first error
-set -e errexit
+#!/bin/sh -e
+# docker-compose deployment script
+# Build or update a set of containers as defined by a docker-compose.yaml file
+# Environmental variables passed to this script (eg IMAGE_TAG) will be available to the given docker-compose.yaml file
 
 repo_path=$( cd $(dirname $0) ; git rev-parse --show-toplevel )
 
@@ -9,7 +10,7 @@ repo_path=$( cd $(dirname $0) ; git rev-parse --show-toplevel )
 export GIT_WORK_TREE="$repo_path"
 export GIT_DIR="${GIT_WORK_TREE}/.git"
 
-# Set default docker-compose file if COMPOSE_FILE not set
+# Set default docker-compose file if COMPOSE_FILE environmental variable not set
 export COMPOSE_FILE=${COMPOSE_FILE:-"${GIT_WORK_TREE}/docker/docker-compose.yaml"}
 
 docker-compose pull
