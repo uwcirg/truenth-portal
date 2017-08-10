0c4a2ce93a342f1cc77c92b6ea82124ef2ac965c
Ivan Cvitkovic <ivanc@uw.edu>
2017-08-10 11:14:23 -0700

    Add comments

--- bin/deploy-docker.sh
+++ bin/deploy-docker.sh
@@ -3,9 +3,13 @@
 set -e errexit
 
 repo_path=$( cd $(dirname $0) ; git rev-parse --show-toplevel )
+
+# git-specific environmental variables
+# allow git commands outside repo path
 export GIT_WORK_TREE="$repo_path"
 export GIT_DIR="${GIT_WORK_TREE}/.git"
 
+# Set default docker-compose file if COMPOSE_FILE not set
 export COMPOSE_FILE=${COMPOSE_FILE:-"${GIT_WORK_TREE}/docker/docker-compose.yaml"}
 
 docker-compose pull
