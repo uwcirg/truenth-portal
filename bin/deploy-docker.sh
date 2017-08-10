23540411b6ba3828114a19cda56909c38b27065e
Ivan Cvitkovic <ivanc@uw.edu>
2017-08-10 11:14:14 -0700

    Allow docker-compose file to be overridden

--- bin/deploy-docker.sh
+++ bin/deploy-docker.sh
@@ -6,7 +6,7 @@ repo_path=$( cd $(dirname $0) ; git rev-parse --show-toplevel )
 export GIT_WORK_TREE="$repo_path"
 export GIT_DIR="${GIT_WORK_TREE}/.git"
 
-export COMPOSE_FILE="${GIT_WORK_TREE}/docker/docker-compose.yaml"
+export COMPOSE_FILE=${COMPOSE_FILE:-"${GIT_WORK_TREE}/docker/docker-compose.yaml"}
 
 docker-compose pull
 docker-compose up -d
