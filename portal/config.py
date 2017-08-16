747a4accedf8da10f07d92bdf3f0864221a4ce00
Gifford Cheung <giffordcheung@gmail.com>
2017-08-16 16:57:04 -0700

    Updating LifeRay Group Id for default configuration

--- portal/config.py
+++ portal/config.py
@@ -98,7 +98,7 @@ class BaseConfig(object):
     DEFAULT_LOCALE = 'en_US'
     FILE_UPLOAD_DIR = 'uploads'
     LR_ORIGIN = 'https://stg-lr7.us.truenth.org'
-    LR_GROUP = 20139
+    LR_GROUP = 20144
     LR_FOLDER_ST = 32149
 
     SYSTEM_TYPE = 'development'
