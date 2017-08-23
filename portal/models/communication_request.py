acf0d039eaa2b0c8a3359c6a4b1ff18a08e9e25f
Paul Bugni <pbugni@u.washington.edu>
2017-08-23 16:03:56 -0700

    Add tests and complete round one of email reminders.

--- portal/models/communication_request.py
+++ portal/models/communication_request.py
@@ -1,4 +1,5 @@
 """CommunicationRequest model"""
+from flask import current_app
 from datetime import datetime, timedelta
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.dialects.postgresql import ENUM
@@ -8,6 +9,7 @@ from .communication import Communication
 from ..database import db
 from .identifier import Identifier
 from .reference import Reference
+from ..system_uri import TRUENTH_CR_NAME
 
 
 # https://www.hl7.org/fhir/valueset-request-status.html
@@ -58,6 +60,45 @@ class CommunicationRequest(db.Model):
         return (
             'CommunicationRequest for {0.questionnaire_bank}'
             ' on {0.notify_days_after_event}'.format(self))
+
+    @property
+    def name(self):
+        # Specific identifier is used as shorthand for name
+        for id in self.identifiers:
+            if id.system == TRUENTH_CR_NAME:
+                return id.value
+        return None
+
+    @property
+    def lr_url(self):
+        if not self.name:
+            return None
+
+        # Waiting on labels and tags in LR - wire up a cheap lookup
+        # for the time being.
+        lookup = {}
+        lookup["Symptom Tracker | 3 Mo Reminder (1)"] = (
+            "5424efca-9f24-7ff5-cb41-96c1f6546fab")
+        lookup["Symptom Tracker | 3 Mo Reminder (2)"] = (
+            "f1c20a1a-c207-7bfb-92b9-ed51abe73ef1")
+        lookup["Symptom Tracker | 3 Mo Reminder (3)"] = (
+            "b18a5e30-9927-2b33-8db4-e8c2d6e0ad59")
+        lookup["Symptom Tracker | 3 Mo Reminder (4)"] = (
+            "40ee1906-3099-f468-ea7b-69b50df6ee44")
+        lookup["Symptom Tracker | 6 Mo Reminder (1)"] = (
+            "d4b493e0-0c2f-9249-19dc-752d29a542a0")
+        lookup["Symptom Tracker | 6 Mo Reminder (2)"] = (
+            "ed96806d-1d9e-df99-f0e3-7f8372d0ce24")
+        lookup["Symptom Tracker | 6 Mo Reminder (3)"] = (
+            "0d2376e1-5cda-db40-0c0c-5a5ea1cd90bc")
+        lookup["Symptom Tracker | 6 Mo Reminder (4)"] = (
+            "d27fe3af-cf2f-585e-b53c-0397e40065be")
+
+        uuid = lookup.get(self.name)
+        return (
+            "{config[LR_ORIGIN]}/c/portal/truenth/asset/"
+            "mail?version=latest&uuid={uuid}".format(
+                config=current_app.config, uuid=uuid))
 
     @classmethod
     def from_fhir(cls, data):
@@ -160,7 +201,7 @@ def queue_outstanding_messages(user, questionnaire_bank):
         if questionnaire_bank.classification != 'baseline':
             raise NotImplementedError('only baseline communication wired up')
 
-        return overall_assessment_status(user) in (
+        return overall_assessment_status(user.id) in (
             'Due', 'Overdue', 'In Progress')
 
     def queue_communication(user, communication_request):
@@ -173,7 +214,7 @@ def queue_outstanding_messages(user, questionnaire_bank):
         db.session.add(communication)
         db.session.commit()
 
-    def event_start(user):
+    def event_start(user, questionnaire_bank):
         """Lookup event trigger date - if available
 
         Depends on QuestionnaireBank context.  If associated
