eb643066497b7a763940de9d595b07d47d4da7ed
Ivan Cvitkovic <ivanc@uw.edu>
2017-09-28 14:10:01 -0700

    Use standard celery config names

--- portal/factories/celery.py
+++ portal/factories/celery.py
@@ -15,7 +15,7 @@ def create_celery(app):
     celery = Celery(
         app.import_name,
         backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL'],
+        broker=app.config['BROKER_URL'],
     )
     celery.conf.update(app.config)
 
