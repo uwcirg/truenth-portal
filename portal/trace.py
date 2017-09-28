f71b16091b5af034ac27cc3881bfd8f9fd7be44d
Paul Bugni <pbugni@u.washington.edu>
2017-09-28 10:17:47 -0700

    New module to capture a trace of events during a request.

---
+++ portal/trace.py
@@ -0,0 +1,32 @@
+"""Module for capturing trace info during a request
+
+NB this doesn't replace logging, intended to capture
+specific details around tricky tasks.
+
+"""
+from flask import g
+
+
+def establish_trace(opening_line):
+    """Establish the begin of a trace - w/o this trace won't function
+
+    Open up the trace for the request, capturing opening_line
+
+    """
+    if not hasattr(g, 'trace'):
+        g.trace = []
+    g.trace.append(opening_line)
+
+
+def trace(line):
+    """Add given line to trace, if active"""
+    if hasattr(g, 'trace'):
+        g.trace.append(line)
+
+
+def dump_trace(last_line):
+    """Return the active trace, a list of strings"""
+    if hasattr(g, 'trace'):
+        if last_line:
+            g.trace.append(last_line)
+        return g.trace
