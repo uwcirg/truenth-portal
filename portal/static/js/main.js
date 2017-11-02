d042cf5d0ef5d04abb3d270aaba75cbfa3be4e85
Amy Chen <achen2401@gmail.com>
2017-11-02 15:56:34 -0700

    fix sorting issues with id and study id fields in patients list

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -4222,6 +4222,42 @@ var tnthTables = {
         if (isNaN(b_d)) b_d = 0;
 
         return  b_d - a_d;
+    },
+    /***
+     * sorting alpha numeric string
+     */
+     "alphanumericSorter": function (a, b) {
+        /*
+         * ideas came from
+         * http://www.davekoelle.com/files/alphanum.js
+         */
+        function chunkify(t) {
+            var tz = new Array();
+            var x = 0, y = -1, n = 0, i, j;
+
+            while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+              var m = (i == 46 || (i >=48 && i <= 57));
+              if (m !== n) {
+                tz[++y] = "";
+                n = m;
+              }
+              tz[y] += j;
+            }
+            return tz;
+        }
+
+        var aa = chunkify(a);
+        var bb = chunkify(b);
+
+        for (x = 0; aa[x] && bb[x]; x++) {
+            if (aa[x] !== bb[x]) {
+              var c = Number(aa[x]), d = Number(bb[x]);
+              if (c == aa[x] && d == bb[x]) {
+                return c - d;
+              } else return (aa[x] > bb[x]) ? 1 : -1;
+            }
+        };
+        return aa.length - bb.length;
     }
 };
 /**
