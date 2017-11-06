e5a1f45d63ee3fd9097f4928a548047ac2a26b7e
OptimusRhine <adamrhine@gmail.com>
2017-11-06 15:54:17 -0800

    adding ResearchProtocol model

---
+++ portal/models/research_protocol.py
@@ -0,0 +1,33 @@
+"""Research Protocol module"""
+from datetime import datetime
+
+from ..database import db
+
+
+class ResearchProtocol(db.Model):
+    """ResearchProtocol model for tracking QB versions"""
+    __tablename__ = 'research_protocols'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self):
+        self.created_at = datetime.utcnow()
+
+    @classmethod
+    def from_json(cls, data):
+        instance = cls()
+        if 'name' not in data:
+            raise ValueError("missing required name field")
+        rp = ResearchProtocol.query.filter(name=data['name']).first()
+        if not rp:
+            rp = cls()
+            rp.name = data['name']
+        return rp
+
+    def as_json(self):
+        d = {}
+        d['id'] = self.id
+        d['name'] = self.name
+        d['created_at'] = self.created_at
+        return d
