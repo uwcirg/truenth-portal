c113099c73db2c2f89a6d17ffcbac41c34f5cd1e
Amy Chen <achen2401@gmail.com>
2017-11-27 12:43:55 -0800

    IE date check fix

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -4312,7 +4312,8 @@ var tnthDates = {
                var iosDateTest = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
                var d = new Date(dateString);
                var ap, day, month, year, hours, minutes, seconds, nd;
-               if (!this.isDateObj(d)) return "";
+               //note instantiating ios formatted date using Date object resulted in error in IE
+               if (!iosDateTest && !isNaN(d) && !this.isDateObj(d)) return "";
                if (iosDateTest.test(dateString)) {
                    //IOS date, no need to convert again to date object, just parse it as is
                    //issue when passing it into Date object, the output date is inconsistent across from browsers
