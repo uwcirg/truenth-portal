2e689adeca9bac00a92e85f7da9be30cabf9a2f4
Ivan Cvitkovic <ivanc@uw.edu>
2018-01-09 10:54:09 -0800

    Merge alembic heads

---
+++ portal/migrations/versions/e11fc44ad243_.py
@@ -0,0 +1,23 @@
+from alembic import op
+import sqlalchemy as sa
+
+
+"""empty message
+
+Revision ID: e11fc44ad243
+Revises: ('84d8d857c2c6', '8ecdd6381235')
+Create Date: 2018-01-09 10:53:03.400534
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e11fc44ad243'
+down_revision = ('84d8d857c2c6', '8ecdd6381235')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
