eaccb207dd2e9baf32638d0a467523b6148c754d
Ivan Cvitkovic <ivanc@uw.edu>
2018-01-10 14:23:30 -0800

    Add dupe QNR migration for TNUSA

--- portal/migrations/versions/84d8d857c2c6_.py
+++ portal/migrations/versions/84d8d857c2c6_.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from alembic import op
+from flask import current_app
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 from StringIO import StringIO
@@ -29,26 +30,51 @@ def upgrade():
     session = Session(bind=op.get_bind())
 
     def needs_attention():
-        query = (
-            "select subject_id, "
-            " document->'questionnaire'->>'reference' as reference "
-            "from questionnaire_responses "
-            "group by subject_id, questionnaire_bank_id, reference "
-            "having count(*) > 1"
-        )
+        # TNUSA - QNRs do not necessarily have questionnaire_bank_id
+        if current_app.config['GIL']:
+            print("TNUSA detected, ignoring questionnaire_bank_id")
 
-        for subject_id, reference in session.execute(query):
-            yield {
-                'subject_id': subject_id,
-                'reference': reference,
-            }
+            # find QNRs of the same type completed within 1 minute of each other (for the same subject)
+            # https://stackoverflow.com/questions/4342370/27631673#27631673
+            query = (
+                "SELECT string_agg(id::text, ', ') AS agg_qnr_ids, subject_id, "
+                "to_timestamp(floor((extract('epoch' from authored) / 60 )) * 60) "
+                "AT TIME ZONE 'UTC' as interval_alias, document->'questionnaire'->>'reference' as reference "
+                "FROM questionnaire_responses GROUP BY interval_alias, subject_id, reference having count(*) > 1 "
+            )
 
-    def keep_one(subject_id=None, reference=None):
-        items = session.query(QuestionnaireResponse).filter(
-            QuestionnaireResponse.subject_id == subject_id).filter(
-            QuestionnaireResponse.document[
-                ("questionnaire", "reference")
-            ].astext == reference).order_by(QuestionnaireResponse.id)
+            for agg_qnr_ids, subject_id, interval_alias, reference in session.execute(query):
+                yield {'qnr_ids': agg_qnr_ids.split(', ')}
+
+        # ePROMs - there should only be 1 QNR of a given type, for a QB
+        else:
+            print("ePROMs detected, finding dupes by questionnaire_bank_id")
+            query = (
+                "select subject_id, "
+                " document->'questionnaire'->>'reference' as reference "
+                "from questionnaire_responses "
+                "group by subject_id, questionnaire_bank_id, reference "
+                "having count(*) > 1"
+            )
+
+            for subject_id, reference in session.execute(query):
+                yield {
+                    'subject_id': subject_id,
+                    'reference': reference,
+                }
+
+
+    def keep_one(subject_id=None, reference=None, qnr_ids=None):
+        if qnr_ids is not None:
+            items = session.query(QuestionnaireResponse).filter(
+                QuestionnaireResponse.id.in_(qnr_ids)
+            ).order_by(QuestionnaireResponse.id)
+        else:
+            items = session.query(QuestionnaireResponse).filter(
+                QuestionnaireResponse.subject_id == subject_id).filter(
+                QuestionnaireResponse.document[
+                    ("questionnaire", "reference")
+                ].astext == reference).order_by(QuestionnaireResponse.id)
 
         keeper = None
         for i in items:
