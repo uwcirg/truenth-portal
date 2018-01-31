9617b26f36628faeef559ebb06e4d970fe2c87ed
OptimusRhine <adamrhine@gmail.com>
2018-01-31 12:30:52 -0800

    merging db heads

---
+++ portal/migrations/versions/4ec18346a368_.py
@@ -0,0 +1,23 @@
+from alembic import op
+import sqlalchemy as sa
+
+
+"""empty message
+
+Revision ID: 4ec18346a368
+Revises: ('124cffb0fc6f', '76ca6e6d0301')
+Create Date: 2018-01-31 12:30:27.226259
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4ec18346a368'
+down_revision = ('124cffb0fc6f', '76ca6e6d0301')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
