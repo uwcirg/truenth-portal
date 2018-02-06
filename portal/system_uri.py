ec1d5fed376e2d9a71404c7f23702ce5a45bec3c
Paul Bugni <pbugni@u.washington.edu>
2018-02-06 13:11:28 -0800

    US_NPI should be in constants - added.

--- portal/system_uri.py
+++ portal/system_uri.py
@@ -3,6 +3,7 @@
 SNOMED = 'http://snomed.info/sct'
 ICHOM = 'http://www.ichom.org/medical-conditions/localized-prostate-cancer/'
 IETF_LANGUAGE_TAG = 'urn:ietf:bcp:47'
+US_NPI = 'http://hl7.org/fhir/sid/us-npi'
 
 # Our common, unique namespace
 TRUENTH_NAMESPACE = 'http://us.truenth.org'
