159fdb74e86e1be998811cc7c88698e5299775b8
Paul Bugni <pbugni@u.washington.edu>
2018-03-14 10:32:39 -0700

    Module to encode `next_step` logic - used to name a dynamic redirect target after validation

---
+++ portal/models/next_step.py
@@ -0,0 +1,33 @@
+"""Module for next_step logic"""
+from werkzeug.exceptions import BadRequest
+from .intervention import Intervention
+
+
+class NextStep(object):
+    """NextStep determination
+
+    Extend the `next` logic encoded in a request path, to lookup the
+    contextual meaning of the named step.
+
+    """
+    @staticmethod
+    def validate(step):
+        if hasattr(NextStep, step):
+            return True
+        raise BadRequest("{} not a valid next step".format(step))
+
+    @staticmethod
+    def decision_support(user):
+        """Returns link to decision support applicable to user"""
+        # Using same determination used for display of cards,
+        # check each intervention with the `decision_support_*`
+        # name pattern
+        for option in Intervention.query.filter(
+                Intervention.name.like("decision_support%")):
+            if option.quick_access_check(user=user):
+                return option.link_url
+
+        # Still here implies user doesn't meet requirements for any
+        raise BadRequest("No decision support applicable to {}".format(user))
+
+
