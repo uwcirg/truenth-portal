e35c8e3473d204af1c6ad53771f76dd1dc26a91c
Paul Bugni <pbugni@u.washington.edu>
2018-03-21 13:01:54 -0700

    Questionnaire.name refactored to FHIR compliant identifier.

--- portal/models/questionnaire.py
+++ portal/models/questionnaire.py
@@ -1,49 +1,98 @@
 """Questionnaire module"""
 from flask import url_for
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.dialects.postgresql import JSONB
 
 from ..database import db
 from ..date_tools import FHIR_datetime
+from .identifier import Identifier
 from ..system_uri import TRUENTH_QUESTIONNAIRE_CODE_SYSTEM
 
 
 class Questionnaire(db.Model):
     __tablename__ = 'questionnaires'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False, unique=True)
+    identifiers = db.relationship(
+        'Identifier', lazy='dynamic', secondary="questionnaire_identifiers")
     document = db.Column(JSONB)
 
     def __str__(self):
         """Print friendly format for logging, etc."""
         return "Questionnaire {0.id} {0.name}".format(self)
 
+    @property
+    def name(self):
+        # Specific identifier is used as shorthand for name
+        for i in self.identifiers:
+            if i.system == TRUENTH_QUESTIONNAIRE_CODE_SYSTEM:
+                return i.value
+        return None
+
+    @classmethod
+    def find_by_name(cls, name):
+        """Shortcut to fetch by named identifier with common system"""
+        identifier = Identifier(
+            _value=name, system=TRUENTH_QUESTIONNAIRE_CODE_SYSTEM).add_if_not_found()
+        return cls.find_by_identifier(identifier)
+
+    @classmethod
+    def find_by_identifier(cls, identifier):
+        """Query method to lookup by identifier"""
+        q_ids = QuestionnaireIdentifier.query.filter(
+            QuestionnaireIdentifier.identifier_id == identifier.id
+        )
+        if q_ids.count() > 1:
+            raise ValueError(
+                "Multiple Questionnaires mapped to {}".format(
+                    identifier
+                ))
+        elif q_ids.count():
+            first = q_ids.first()
+            return cls.query.get(first.questionnaire_id)
+
     @classmethod
     def from_fhir(cls, data):
         instance = cls()
-        assert data['resourceType'] == 'Questionnaire'
-        instance.name = data['identifier'][0]['value']
-        return instance
+        return instance.update_from_fhir(data)
+
+    def update_from_fhir(self, data):
+        if 'identifier' in data:
+            # track current identifiers - must remove any not requested
+            remove_if_not_requested = [i for i in self.identifiers]
+            for id in data['identifier']:
+                identifier = Identifier.from_fhir(id).add_if_not_found()
+                if identifier not in self.identifiers.all():
+                    self.identifiers.append(identifier)
+                else:
+                    remove_if_not_requested.remove(identifier)
+            for obsolete in remove_if_not_requested:
+                self.identifiers.remove(obsolete)
+        self = self.add_if_not_found(commit_immediately=True)
+        return self
 
     def as_fhir(self):
-        d = {}
-        d['resourceType'] = 'Questionnaire'
-        d['identifier'] = [{
-            "system": TRUENTH_QUESTIONNAIRE_CODE_SYSTEM,
-            "value": self.name,
-        }]
+        d = {'resourceType': 'Questionnaire'}
+        if self.identifiers.count():
+            d['identifier'] = []
+        for i in self.identifiers:
+            d['identifier'].append(i.as_fhir())
         return d
 
     def add_if_not_found(self, commit_immediately=False):
         """Add self to database, or return existing
 
-        Queries for similar, matching on **name** alone.
-        Note the database unique constraint to match.
+        Queries for similar, matching on **name** alone (which is
+        maintained as an identifier with the matching system value)
 
         @return: the new or matched Questionnaire
 
         """
         assert self.name
-        existing = Questionnaire.query.filter_by(name=self.name).first()
+        i = Identifier(
+            _value=self.name,
+            system=TRUENTH_QUESTIONNAIRE_CODE_SYSTEM).add_if_not_found(
+            commit_immediately=commit_immediately)
+        existing = Questionnaire.find_by_identifier(i)
         if not existing:
             db.session.add(self)
             if commit_immediately:
@@ -74,9 +123,23 @@ class Questionnaire(db.Model):
             'type': 'searchset',
             'link': {
                 'rel': 'self',
-                'href': url_for('assessment_engine_api.questionnaire_list',
+                'href': url_for('questionnaire_api.questionnaire_list',
                                 _external=True),
             },
             'entry': objs,
         }
         return bundle
+
+
+class QuestionnaireIdentifier(db.Model):
+    """link table for questionnaire : n identifiers"""
+    __tablename__ = 'questionnaire_identifiers'
+    id = db.Column(db.Integer, primary_key=True)
+    questionnaire_id = db.Column(db.ForeignKey(
+        'questionnaires.id', ondelete='cascade'), nullable=False)
+    identifier_id = db.Column(db.ForeignKey(
+        'identifiers.id', ondelete='cascade'), nullable=False)
+
+    __table_args__ = (UniqueConstraint(
+        'questionnaire_id', 'identifier_id',
+        name='_questionnaire_identifier'),)
