db0430aeccf4893fdbcd81f17165437bb3384db9
Amy Chen <achen2401@gmail.com>
2018-04-18 17:39:14 -0700

    remove white space

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -425,7 +425,7 @@ var ConsentUIHelper = function(consentItems, userId) {
             /*
              * note terms of use text is hidden/unhidden using respective project's stylesheet
              */
-            touContent += "<td><span class='text-success small-text eproms-tou-table-text'><a href='" + item.agreement_url + "' target='_blank'>" + i18next.t("Agreed to {document name}").replace("{document name}", capitalize(item.display_type)) + "</a></span><span class='text-success small-text truenth-tou-table-text'>" + i18next.t("Agreed to terms") + "</span></td>";
+            touContent += "<td><span class='text-success small-text eproms-tou-table-text'><a href='" + item.agreement_url + "' target='_blank'>" + i18next.t("Agreed to " + capitalize(item.display_type)) + "</a></span><span class='text-success small-text truenth-tou-table-text'>" + i18next.t("Agreed to terms") + "</span></td>";
             touContent += "<td><span class='eproms-tou-table-text text-capitalize'><a href='" + item.agreement_url + "' target='_blank'>" + i18next.t(item.display_type) + "</a></span><span class='truenth-tou-table-text'>" + (i18next.t("{project name} Terms of Use").replace("{project name}", "TrueNTH USA")) + "</span> <span class='agreement'>&nbsp;<a href='" + item.agreement_url + "' target='_blank'><em>" + i18next.t("View") + "</em></a></span></td>";
             touContent += "<td>" + item.accepted + "</td></tr>";
         });
@@ -6879,6 +6879,4 @@ __i18next.init({
 
     }, 150);
 });
-
-
 
