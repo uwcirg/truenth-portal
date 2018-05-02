de0181bf867f90fe6cc03065c5a90f514e9dde4d
Ivan Cvitkovic <ivanc@uw.edu>
2018-05-02 16:22:47 -0700

    Add migration merge

---
+++ portal/migrations/versions/3da0caa42c62_.py
@@ -0,0 +1,23 @@
+from alembic import op
+import sqlalchemy as sa
+
+
+"""empty message
+
+Revision ID: 3da0caa42c62
+Revises: ('a33cb61fff11', '2b127d65388b')
+Create Date: 2018-05-02 16:22:33.562649
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3da0caa42c62'
+down_revision = ('a33cb61fff11', '2b127d65388b')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
