7619d53734860d9606b6f651f911dc7ceefd10b9
Paul Bugni <pbugni@u.washington.edu>
2018-05-23 13:06:03 -0700

    TN-1030 plug in custom text for forgot password message.

--- portal/factories/app.py
+++ portal/factories/app.py
@@ -172,10 +172,12 @@ def configure_extensions(app):
     ## manage the flask-user business logic
 
     from flask_user.views import login, register
-    from ..views.patch_flask_user import patch_make_safe_url
+    from ..views.patch_flask_user import (
+        patch_make_safe_url, patch_forgot_password)
 
     user_manager.init_app(
         app,
+        forgot_password_view_function=patch_forgot_password,
         make_safe_url_function=patch_make_safe_url,
         reset_password_view_function=reset_password_view_function,
         register_view_function=capture_next_view_function(register),
