31a76dcdda55d944b771c1ad5c6bf06c289e99e8
Paul Bugni <pbugni@u.washington.edu>
2018-06-04 11:46:10 -0700

    For staging exclusions, capture intervention.client_id in export/import.

--- portal/models/intervention.py
+++ portal/models/intervention.py
@@ -92,15 +92,16 @@ class Intervention(db.Model):
         The id is also independent - return the rest of the not null
         fields as a simple json dict.
 
-        NB for staging exclusions to function, link_url is now included.
-        Take care to remove it from persistence files where it is NOT
-        portable.
+        NB for staging exclusions to function, link_url and client_id
+        are now included. Take care to remove it from persistence files
+        where it is NOT portable, for example, when generating persistence
+        files programmatically.
 
         """
         d = {'resourceType': 'Intervention'}
         for attr in ('name', 'description', 'card_html', 'link_label',
                      'status_text', 'public_access', 'display_rank',
-                     'subscribed_events', 'link_url'):
+                     'subscribed_events', 'link_url', 'client_id'):
             if getattr(self, attr, None) is not None:
                 d[attr] = getattr(self, attr)
 
@@ -121,7 +122,7 @@ class Intervention(db.Model):
             if attr in data:
                 setattr(self, attr, data.get(attr))
 
-        # link_url is special - generally we don't pull links
+        # link_url and client_id are special - generally we don't pull
         # from persisted format as each instance is configured to
         # communicate with distinct interventions.  As it is used
         # for prod -> staging, warn if seen on any other system
@@ -131,6 +132,12 @@ class Intervention(db.Model):
                     "IMPORTING non-portable intervention({}) link_url: '{}'"
                     "".format(self.name, data['link_url']))
             self.link_url = data['link_url']
+        if 'client_id' in data and self.client_id != data['client_id']:
+            if current_app.config.get("SYSTEMT_TYPE", '').lower() != 'staging':
+                current_app.logger.warn(
+                    "IMPORTING non-portable intervention({}) client_id: '{}'"
+                    "".format(self.name, data['client_id']))
+            self.client_id = data['client_id']
 
         return self
 
