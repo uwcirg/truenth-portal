aefd7a1d353adbd0fa561b7b20ba17af5cab12d8
John Randolph <Johnr17@lakesideschool.org>
2018-06-05 15:15:42 -0700

    Added test cases; moved dict_tools.py to unicode in one case

--- tests/test_dict_tools.py
+++ tests/test_dict_tools.py
@@ -23,3 +23,21 @@ def test_dict_match_matches():
     d1 = {'one': 1, 'two': False, 'three': {'nested zero': 0}}
     d2 = {'one': 1, 'two': False, 'three': {'nested zero': 0}}
     assert dict_match(d1, d2, io.StringIO())
+
+
+def test_dict_match_not_match():
+    d1 = {'one': 1, 'two': True, 'three': {'nested zero': 0}}
+    d2 = {'one': 1, 'two': False, 'three': {'nested zero': 0}}
+    assert not dict_match(d1, d2, io.StringIO())
+
+
+def test_dict_compare_same():
+    d1 = {'one': 1, 'two': False, 'three': {'nested zero': 0}}
+    d2 = {'one': 1, 'two': False, 'three': {'nested zero': 0}}
+    assert dict_compare(d1,d2) == (set(),set(),{},{'one','two','three'})
+
+
+def test_dict_compare_different():
+    d1 = {'one': 1, 'two': False, 'three': {'nested zero': 0}}
+    d2 = {'one': 1, 'three': {'nested zero': 1},'four': False}
+    assert dict_compare(d1,d2) == ({'two'}, {'four'}, {'three': ({'nested zero': 0}, {'nested zero': 1})}, {'one'})
