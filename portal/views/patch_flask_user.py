c6dd69d15e443e3e26517853b21682532ac0e442
Ivan Cvitkovic <ivanc@uw.edu>
2018-06-08 15:00:52 -0700

    add newline

--- portal/views/patch_flask_user.py
+++ portal/views/patch_flask_user.py
@@ -1,6 +1,7 @@
 """workarounds to flask_user problems"""
 from future import standard_library
 standard_library.install_aliases()
+
 from urllib.parse import urlsplit, urlunsplit
 
 from flask import current_app, flash, redirect, request, url_for
