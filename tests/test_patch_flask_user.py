3e47e664a37582f09de143ecc9d3c4ec1bfa2143
Paul Bugni <pbugni@u.washington.edu>
2018-06-11 11:57:01 -0700

    Tests need to use unicode URL strings as the framework expects for updated urlunsplit to function.

--- tests/test_patch_flask_user.py
+++ tests/test_patch_flask_user.py
@@ -6,32 +6,32 @@ from tests import TestCase
 class TestPathFlaskUser(TestCase):
 
     def test_no_path(self):
-        url = 'http://google.com'
+        url = u'http://google.com'
         safe_url = patch_make_safe_url(url)
         self.assertEqual('', safe_url)
 
     def test_no_qs(self):
-        url = 'https://google.com/'
+        url = u'https://google.com/'
         safe_url = patch_make_safe_url(url)
         self.assertEqual('/', safe_url)
 
     def test_w_qs(self):
-        url = 'https://google.com/search?q=testing'
+        url = u'https://google.com/search?q=testing'
         safe_url = patch_make_safe_url(url)
         self.assertEqual('/search?q=testing', safe_url)
 
     def test_wo_host_scheme(self):
-        url = '/search?q=testing&safe=on'
+        url = u'/search?q=testing&safe=on'
         safe_url = patch_make_safe_url(url)
         self.assertEqual('/search?q=testing&safe=on', safe_url)
 
     def test_fragment_wo_host(self):
-        url = '/search?q=testing&safe=on#row=4'
+        url = u'/search?q=testing&safe=on#row=4'
         safe_url = patch_make_safe_url(url)
         self.assertEqual(url, safe_url)
 
     def test_qs_and_fragment(self):
-        url = 'https://google.com:443/search?q=testing&safe=on#row=4'
+        url = u'https://google.com:443/search?q=testing&safe=on#row=4'
         safe_url = patch_make_safe_url(url)
         index = url.find('/search')
         self.assertEqual(url[index:], safe_url)
