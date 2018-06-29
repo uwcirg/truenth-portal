74ba6e5a6a8ee065f122fa07b00380e43be35151
Ivan Cvitkovic <ivanc@uw.edu>
2018-06-29 13:35:15 -0700

    Move setuptools-scm config to setup.cfg

--- setup.py
+++ setup.py
@@ -7,27 +7,16 @@ to install:
     python setup.py install
 
 """
-import datetime
 import os
 
 from setuptools import setup
 
-setup_kwargs = dict(
-    test_suite="tests",
-)
-
-# Use setuptools-scm to determine version if available
+# Use setuptools-scm to determine version if available (see setup.cfg)
 # Todo: refactor into function for setuptools_scm.parse_scm_fallback entrypoint
-version_kwargs = dict(
-    use_scm_version=True,
-    setup_requires=('setuptools_scm'),
-)
-
 # Detect Heroku build environment and override version generation (setuptools-scm)
 BUILD_DIR = os.environ.get("BUILD_DIR")
 if BUILD_DIR:
     build_version = BUILD_DIR.split("-")[-1]
-    version_kwargs = {"version": "0+ng"+build_version}
-
-setup_kwargs.update(version_kwargs)
-setup(**setup_kwargs)
+    setup(version="0+ng"+build_version)
+else:
+    setup()
