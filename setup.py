2f610fa0075b0607f8848136f26ec9bbfc92a8d6
Ivan Cvitkovic <icvitkovic@gmail.com>
2018-07-02 12:14:07 -0700

    Cleanup setup.py (#2377)
    * Fix indentation
    
    * Move scripts section to setup.cfg
    
    * Move setuptools-scm config to setup.cfg
    
    * Add comment
    
    * Rewrite comments

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 """ setup script for "portal" package
 
+see setup.cfg for configuration parameters
 for development:
     python setup.py develop
 
@@ -7,32 +8,16 @@ to install:
     python setup.py install
 
 """
-import datetime
 import os
 
 from setuptools import setup
 
-setup_kwargs = dict(
-    scripts=[
-        "manage.py",
-        "wsgi.py",
-        os.path.join('docker', 'remap_envvars.py'),
-    ],
-    test_suite="tests",
-)
-
-# Use setuptools-scm to determine version if available
-# Todo: refactor into function for setuptools_scm.parse_scm_fallback entrypoint
-version_kwargs = dict(
-    use_scm_version=True,
-    setup_requires=('setuptools_scm'),
-)
-
-# Detect Heroku build environment and override version generation (setuptools-scm)
+# Detect Heroku build environment and override version generation
+# Todo: refactor into setuptools_scm.parse_scm_fallback entrypoint
 BUILD_DIR = os.environ.get("BUILD_DIR")
 if BUILD_DIR:
     build_version = BUILD_DIR.split("-")[-1]
-    version_kwargs = {"version": "0+ng"+build_version}
-
-setup_kwargs.update(version_kwargs)
-setup(**setup_kwargs)
+    setup(version="0+ng"+build_version)
+else:
+    # default git-based version generation (setuptools-scm), see setup.cfg
+    setup()
