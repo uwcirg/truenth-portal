01e5d42b084e1014ca9da3ada02b0aba52b5e493
John Randolph <Johnr17@lakesideschool.org>
2018-07-03 13:49:38 -0700

    update test_exclusion_persistence for py2/py3 compatability, cleanup

--- tests/test_exclusion_persistence.py
+++ tests/test_exclusion_persistence.py
@@ -72,8 +72,8 @@ class TestExclusionPersistence(TestCase):
         tc1 = Client.query.filter(Client.client_id == '123_abc').one()
         tc2 = Client.query.filter(Client.client_id == '234_bcd').one()
 
-        self.assertEqual(tc1.callback_url, 'http://callback.one')
-        self.assertEqual(tc2._redirect_uris, 'http://test_two.org')
+        assert tc1.callback_url == 'http://callback.one'
+        assert tc2._redirect_uris == 'http://test_two.org'
 
     def testIntervention(self):
         t1 = Intervention(
@@ -109,8 +109,8 @@ class TestExclusionPersistence(TestCase):
 
         result = Intervention.query.filter(Intervention.name == 'alvin').one()
 
-        self.assertEqual(result.link_url, 'http://retain.this')
-        self.assertEqual(result.description, 'prod description')
+        assert result.link_url == 'http://retain.this'
+        assert result.description == 'prod description'
 
     def test_connected_user(self):
         """User and service tokens connected with intervention should survive"""
@@ -155,7 +155,7 @@ class TestExclusionPersistence(TestCase):
         expected = client_users_filter().count()
         with open(path_join(self.tmpdir, 'User.json')) as f:
             serial_form = json.loads(f.read())
-        self.assertEqual(expected, len(serial_form['entry']))
+        assert expected == len(serial_form['entry'])
 
         # Modify/delete some internal db values and confirm reapplication of
         # persistence restores desired values
@@ -164,21 +164,21 @@ class TestExclusionPersistence(TestCase):
 
         # just expecting the one service token.  purge it and the
         # owner (the service user) and the owner's auth_provider
-        self.assertEqual(Token.query.count(), 1)
+        assert Token.query.count() == 1
         service_user_id = Token.query.one().user_id
         b4 = User.query.count()
-        self.assertEqual(UserRelationship.query.count(), 1)
-        self.assertEqual(AuthProvider.query.count(), 1)
+        assert UserRelationship.query.count() == 1
+        assert AuthProvider.query.count() == 1
         with SessionScope(db):
             AuthProvider.query.delete()
             Token.query.delete()
             UserRelationship.query.delete()
             User.query.filter_by(id=service_user_id).delete()
             db.session.commit()
-        self.assertEqual(AuthProvider.query.count(), 0)
-        self.assertEqual(Token.query.count(), 0)
-        self.assertEqual(UserRelationship.query.count(), 0)
-        self.assertEqual(b4 - 1, User.query.count())
+        assert AuthProvider.query.count() == 0
+        assert Token.query.count() == 0
+        assert UserRelationship.query.count() == 0
+        assert User.query.count() == b4 - 1
 
         for model in staging_exclusions:
             ep = ExclusionPersistence(
@@ -191,10 +191,10 @@ class TestExclusionPersistence(TestCase):
             ep.import_(keep_unmentioned=True)
 
         result = User.query.get(owner_id)
-        self.assertEqual(result.email, 'sm-owner@gmail.com')
-        self.assertEqual(AuthProvider.query.count(), 1)
-        self.assertEqual(Token.query.count(), 1)
-        self.assertEqual(UserRelationship.query.count(), 1)
+        assert result.email == 'sm-owner@gmail.com'
+        assert AuthProvider.query.count() == 1
+        assert Token.query.count() == 1
+        assert UserRelationship.query.count() == 1
 
     def test_preflight_valid(self):
         # setup pre-flight conditions expected to pass
@@ -217,21 +217,21 @@ class TestExclusionPersistence(TestCase):
         sp = SitePersistence(target_dir=self.tmpdir)
         sp.export(staging_exclusion=True)
 
-        self.assertEqual(Token.query.count(), 1)
+        assert Token.query.count() == 1
 
         # Delete service account, expect it to return
         with SessionScope(db):
             db.session.delete(service)
             db.session.commit()
 
-        self.assertEqual(User.query.count(), 1)
-        self.assertEqual(Token.query.count(), 0)
+        assert User.query.count() == 1
+        assert Token.query.count() == 0
 
         # Import
         sp.import_(keep_unmentioned=True, staging_exclusion=True)
 
-        self.assertEqual(Token.query.count(), 1)
-        self.assertEqual(User.query.count(), 2)
+        assert Token.query.count() == 1
+        assert User.query.count() == 2
 
     def test_preflight_invalid_service_user(self):
         # setup pre-flight conditions expected to fail
@@ -255,8 +255,8 @@ class TestExclusionPersistence(TestCase):
         sp = SitePersistence(target_dir=self.tmpdir)
         sp.export(staging_exclusion=True)
 
-        self.assertEqual(Token.query.count(), 1)
-        self.assertEqual(UserRelationship.query.count(), 1)
+        assert Token.query.count() == 1
+        assert UserRelationship.query.count() == 1
 
     def test_preflight_valid(self):
         # setup pre-flight conditions expected to pass
@@ -279,21 +279,21 @@ class TestExclusionPersistence(TestCase):
         sp = SitePersistence(target_dir=self.tmpdir)
         sp.export(staging_exclusion=True)
 
-        self.assertEqual(Token.query.count(), 1)
+        assert Token.query.count() == 1
 
         # Delete service account, expect it to return
         with SessionScope(db):
             db.session.delete(service)
             db.session.commit()
 
-        self.assertEqual(User.query.count(), 1)
-        self.assertEqual(Token.query.count(), 0)
+        assert User.query.count() == 1
+        assert Token.query.count() == 0
 
         # Import
         sp.import_(keep_unmentioned=True, staging_exclusion=True)
 
-        self.assertEquals(Token.query.count(), 1)
-        self.assertEquals(User.query.count(), 2)
+        assert Token.query.count() == 1
+        assert User.query.count() == 2
 
     def test_preflight_invalid_service_user(self):
         # setup pre-flight conditions expected to fail
@@ -317,15 +317,15 @@ class TestExclusionPersistence(TestCase):
         sp = SitePersistence(target_dir=self.tmpdir)
         sp.export(staging_exclusion=True)
 
-        self.assertEqual(Token.query.count(), 1)
+        assert Token.query.count() == 1
 
         # Delete service account, and put fake patient in its place
         with SessionScope(db):
             db.session.delete(service)
             db.session.commit()
 
-        self.assertEqual(User.query.count(), 1)
-        self.assertEqual(Token.query.count(), 0)
+        assert User.query.count() == 1
+        assert Token.query.count() == 0
 
         patient_role_id = Role.query.filter_by(
             name=ROLE.PATIENT.value).one().id
@@ -344,4 +344,4 @@ class TestExclusionPersistence(TestCase):
         with self.assertRaises(ValueError) as context:
             sp.import_(keep_unmentioned=True, staging_exclusion=True)
 
-        self.assertTrue('intheway@here.com' in context.exception.message)
+        assert 'intheway@here.com' in str(context.exception)
