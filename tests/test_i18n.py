ff6fdd3842edab9744ad63f59ecf14ccc8cd1cd8
John Randolph <Johnr17@lakesideschool.org>
2018-07-05 11:51:39 -0700

    cleanup of test_i18n.py

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,22 +1,18 @@
 """Unit test module for internationalization logic"""
-import sys
+from __future__ import unicode_literals  # isort:skip
 
 from flask import current_app
 from flask_login import login_user
-import pytest
 
 from portal.models.i18n import get_locale
 from portal.models.user import User
 from tests import TEST_USER_ID, TestCase
 
-if sys.version_info.major > 2:
-    pytest.skip(msg="not yet ported to python3", allow_module_level=True)
 class TestI18n(TestCase):
     """I18n tests"""
 
     def test_get_locale(self):
-        self.assertEqual(
-            get_locale(), current_app.config.get("DEFAULT_LOCALE"))
+        assert get_locale() == current_app.config.get("DEFAULT_LOCALE")
 
         language = 'en_AU'
         language_name = "Australian English"
@@ -25,4 +21,4 @@ class TestI18n(TestCase):
         test_user.locale = (language, language_name)
 
         login_user(test_user)
-        self.assertEqual(get_locale(), language)
+        assert get_locale() == language
