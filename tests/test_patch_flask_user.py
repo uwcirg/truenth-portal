59a716f1a5ff50c7056a0bae18876afb828e04cf
John Randolph <Johnr17@lakesideschool.org>
2018-07-06 19:13:34 -0700

    cleaned up test_patch_flask_user.py

--- tests/test_patch_flask_user.py
+++ tests/test_patch_flask_user.py
@@ -1,42 +1,38 @@
 """Unit test module for patch_flask_user"""
-import sys
-
-import pytest
+from __future__ import unicode_literals  # isort:skip
 
 from portal.views.patch_flask_user import patch_make_safe_url
 from tests import TestCase
 
-if sys.version_info.major > 2:
-    pytest.skip(msg="not yet ported to python3", allow_module_level=True)
 class TestPathFlaskUser(TestCase):
 
     def test_no_path(self):
         url = u'http://google.com'
         safe_url = patch_make_safe_url(url)
-        self.assertEqual('', safe_url)
+        assert '' == safe_url
 
     def test_no_qs(self):
         url = u'https://google.com/'
         safe_url = patch_make_safe_url(url)
-        self.assertEqual('/', safe_url)
+        assert '/' == safe_url
 
     def test_w_qs(self):
         url = u'https://google.com/search?q=testing'
         safe_url = patch_make_safe_url(url)
-        self.assertEqual('/search?q=testing', safe_url)
+        assert '/search?q=testing' == safe_url
 
     def test_wo_host_scheme(self):
         url = u'/search?q=testing&safe=on'
         safe_url = patch_make_safe_url(url)
-        self.assertEqual('/search?q=testing&safe=on', safe_url)
+        assert '/search?q=testing&safe=on' == safe_url
 
     def test_fragment_wo_host(self):
         url = u'/search?q=testing&safe=on#row=4'
         safe_url = patch_make_safe_url(url)
-        self.assertEqual(url, safe_url)
+        assert url == safe_url
 
     def test_qs_and_fragment(self):
         url = u'https://google.com:443/search?q=testing&safe=on#row=4'
         safe_url = patch_make_safe_url(url)
         index = url.find('/search')
-        self.assertEqual(url[index:], safe_url)
+        assert url[index:] == safe_url
