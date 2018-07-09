71ad68561cb12ca972c674612d554f1314e73936
John Randolph <Johnr17@lakesideschool.org>
2018-07-09 13:29:26 -0700

    cleanup of test_truenth.py

--- tests/test_truenth.py
+++ tests/test_truenth.py
@@ -1,27 +1,23 @@
-import sys
-
-import pytest
+from __future__ import unicode_literals  # isort:skip
 
 from tests import FIRST_NAME, LAST_NAME, TestCase
 
-if sys.version_info.major > 2:
-    pytest.skip(msg="not yet ported to python3", allow_module_level=True)
 class TestTrueNTH(TestCase):
 
     def test_portal_wrapper_html(self):
         self.login()
-        rv = self.client.get('/api/portal-wrapper-html/')
+        response = self.client.get('/api/portal-wrapper-html/')
 
-        results = unicode(rv.data, 'utf-8')
-        self.assertTrue(FIRST_NAME in results)
-        self.assertTrue(LAST_NAME in results)
+        results = response.get_data(as_text=True)
+        assert FIRST_NAME in results
+        assert LAST_NAME in results
 
     def test_portal_wrapper_wo_name(self):
         "w/o a users first, last name, username should appear"
         username = 'test2@example.com'
         user = self.add_user(username=username, first_name=None)
         self.login(user_id=user.id)
-        rv = self.client.get('/api/portal-wrapper-html/')
+        response = self.client.get('/api/portal-wrapper-html/')
 
-        self.assertEqual(rv.status_code, 200)
-        self.assertTrue(username in rv.data)
+        assert response.status_code == 200
+        assert username in response.get_data(as_text=True)
