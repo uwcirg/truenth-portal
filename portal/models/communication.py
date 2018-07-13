8e8f29dae9406041ce419e2b0cecae4c8af39c3c
Amy Chen <achen2401@gmail.com>
2018-07-13 10:15:26 -0700

    pep8 fix

--- portal/models/communication.py
+++ portal/models/communication.py
@@ -57,7 +57,6 @@ def load_template_args(user, questionnaire_bank_id=None):
         `_lookup_first_name` -> `first_name`
 
     """
-    
     def ae_link():
         return url_for('assessment_engine_api.present_needed', _external=True)
 
