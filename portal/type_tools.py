62c5b226370d27c81da213d8df00b63c8113ec8c
Paul Bugni <pbugni@u.washington.edu>
2018-07-19 15:11:14 -0700

    Remove unused variable.

--- portal/type_tools.py
+++ portal/type_tools.py
@@ -6,5 +6,5 @@ from flask import abort
 def check_int(i):
     try:
         return int(i)
-    except ValueError, e:
+    except ValueError:
         abort(400, "invalid input '{}' - must be an integer".format(i))
