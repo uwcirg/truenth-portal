f9d8d1c45a9356a5306caa47cbccdeda4c37dee4
John Randolph <Johnr17@lakesideschool.org>
2018-07-20 15:22:31 -0700

    TN-351 formatted test_assessment_status.py and test_clinical.py

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals  # isort:skip
 
 from tests import TestCase
 
+
 class TestCelery(TestCase):
     """Portal view tests"""
 
@@ -12,6 +13,6 @@ class TestCelery(TestCase):
         y = 99
         response = (self.client.get(
             '/celery-test?x={x}&y={y}&redirect-to-result=True'.
-            format(x=x, y=y), follow_redirects=True))
+                format(x=x, y=y), follow_redirects=True))
         assert response.status_code == 200
         assert response.get_data(as_text=True) == str(x + y)
