ec6a0b861c421e300f7da07932f91b4917d03d22
John0Randolph <35846969+John0Randolph@users.noreply.github.com>
2018-07-23 11:58:33 -0700

    TN-351 formatting test_site_persistence.py through test_tou.py (#2514)

--- tests/test_telecom.py
+++ tests/test_telecom.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals  # isort:skip
 from portal.models.telecom import ContactPoint, Telecom
 from tests import TestCase
 
+
 class TestTelecom(TestCase):
     """Telecom model tests"""
 
