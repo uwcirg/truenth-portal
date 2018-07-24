da3a5ed21820e31a5141f3fe4ab353537073bb15
pbugni <pbugni@u.washington.edu>
2018-07-24 14:22:39 -0700

    TN-1369 Handle duplicate email check in demographics for users w/o em… (#2521)
    * TN-1369 Handle duplicate email check in demographics for users w/o email.
    
    * Correct failing py3 test.
    
    * pep8 whitespace

--- portal/models/user.py
+++ portal/models/user.py
@@ -1320,7 +1320,7 @@ class User(db.Model, UserMixin):
         if 'telecom' in fhir:
             telecom = Telecom.from_fhir(fhir['telecom'])
             if telecom.email:
-                if self.email and ((telecom.email != self.email) and
+                if self._email and ((telecom.email != self._email) and
                         (User.query.filter_by(email=telecom.email).count() > 0)):
                     abort(400, "email address already in use")
                 self.email = telecom.email
