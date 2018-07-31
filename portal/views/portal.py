124e3492d0f64475099a5dd539378e9fc6579d67
Amy Chen <achen2401@gmail.com>
2018-07-31 12:26:43 -0700

    remove / and extra space

--- portal/views/portal.py
+++ portal/views/portal.py
@@ -672,8 +672,8 @@ def patient_reminder_email(user_id):
         if qbd and qbd.questionnaire_bank:
             questionnaire_bank_id = qbd.questionnaire_bank.id
         # pass in questionnaire bank id to get at the questionnaire due date
-        args = load_template_args(user=user, questionnaire_bank_id=\
-                                  questionnaire_bank_id)
+        args = load_template_args(user=user,
+                                  questionnaire_bank_id=questionnaire_bank_id)
         item = MailResource(
             app_text(name_key), locale_code=user.locale_code, variables=args)
     except UndefinedAppText:
