e81d6cab71c783a43fda106844eaa417215f7de4
Amy Chen <achen2401@gmail.com>
2018-08-10 13:53:54 -0700

    fix call to ajax stop, refactor sendrequest

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -485,9 +485,10 @@ var tnthAjax = {
     },
     "sendRequest": function(url, method, userId, params, callback) {
         if (!url) { return false; }
-        var defaultParams = {attempts: 0, max_attempts: 3, contentType: "application/json; charset=utf-8", dataType: "json", sync: false, timeout: 5000, data: null, useWorker: false, async: true};
+        var defaultParams = {type: method ? method : "GET", url: url, attempts: 0, max_attempts: 3, contentType: "application/json; charset=utf-8", dataType: "json", sync: false, timeout: 5000, data: null, useWorker: false, async: true};
         params = params || defaultParams;
         params = $.extend({}, defaultParams, params);
+        params.async = params.sync ? false: params.async;
         var self = this;
         var fieldHelper = this.FieldLoaderHelper, targetField = params.targetField || null;
         callback = callback || function() {};
@@ -523,16 +524,7 @@ var tnthAjax = {
                 "pragma": "no-cache"
             };
         }
-        $.ajax({
-            type: method ? method : "GET",
-            url: url,
-            contentType: params.contentType,
-            dataType: params.dataType,
-            cache: params.cache,
-            async: params.sync ? false: params.async,
-            data: params.data,
-            timeout: params.timeout //set default timeout to 5 seconds
-        }).done(function(data) {
+        $.ajax(params).done(function(data) {
             params.attempts = 0;
             if (data) {
                 callback(data);
--- portal/static/js/profile.js
+++ portal/static/js/profile.js
@@ -35,13 +35,13 @@
             var self = this;
             VueErrorHandling(); /*global VueErrorHandling */
             this.registerDependencies();
-            this.onBeforeSectionsLoad();
             this.getOrgTool();
             this.setUserSettings();
+            this.onBeforeSectionsLoad();
             this.setCurrentUserOrgs();
             this.initStartTime = new Date();
             this.initChecks.push({done: false});
-            this.setDemoData({useWorker: true}, function() {
+            this.setDemoData({beforeSend: self.setSectionLoaders}, function() { //this will initialize section loaders, only if ajax request is sent
                 self.onInitChecksDone();
             });
             if (this.currentUserId) { //get user roles - note using the current user Id - so we can determine: if user is an admin, if he/she can edit the consent, etc.
@@ -512,10 +512,16 @@
             getShowInMacro: function() {
                 return this.settings.SHOW_PROFILE_MACROS || [];
             },
+            setSectionLoaders: function() {
+                $("#profileForm .section-loader").removeClass("tnth-hide");
+            },
+            clearSectionLoaders: function() {
+                $("#profileForm .section-loader").addClass("tnth-hide");
+                $("#profileForm .profile-item-edit-btn").addClass("active");
+            },
             onBeforeSectionsLoad: function() {
-                if ($("#profileForm").length > 0) {
+                if (this.mode === "profile") {
                     $("#mainDiv").addClass("profile");
-                    $("#profileForm .section-loader").removeClass("tnth-hide");
                 }
             },
             onSectionsDidLoad: function() {
@@ -528,9 +534,8 @@
                         self.fillViews = self.setView();
                         self.initEditButtons();
                     }, 50);
-                    $(document).ajaxStop(function() { //show edit button after all ajax calls are done, NOTE this also accounts for failed/cancelled requests
-                        $("#profileForm .section-loader").addClass("tnth-hide");
-                        $("#profileForm .profile-item-edit-btn").addClass("active");
+                    $(document).ajaxStop(function() {
+                        self.clearSectionLoaders(); //clear section loaders after ajax calls completed, note this will account for failed/cancelled requests as well
                     });
                 }
             },
