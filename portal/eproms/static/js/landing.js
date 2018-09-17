daa610714f83d8a51f9936bfc44f417f95e03b3b
Amy Chen <achen2401@gmail.com>
2018-09-17 12:08:09 -0700

    TN-1484 eproms landing page design changes 1st iteration

---
+++ portal/eproms/static/js/landing.js
@@ -0,0 +1,22 @@
+$("document").ready(function() {
+    DELAY_LOADING = true; /* global DELAY_LOADING */
+    setTimeout(function() {
+        DELAY_LOADING = false;
+        showMain(); /* global showMain */
+        hideLoader(true); /* global hideLoader */
+    }, 150);
+    if (typeof sessionStorage !== "undefined") {
+        sessionStorage.clear();
+    }
+    $("#email").on("focus", function() {
+    	$(this).removeAttr("readonly");
+    });
+
+    $("#email, #password").on("change", function() {
+    	if ($(this).val()) {
+    		$("#btnLogin").attr("disabled", false).removeClass("disabled");
+    		return;
+    	}
+    	$("#btnLogin").attr("disabled", true).addClass("disabled");
+    });
+});
