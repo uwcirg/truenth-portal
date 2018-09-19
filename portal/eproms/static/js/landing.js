d43caba429d98fc3bd43c4500748df1206052fdd
Amy Chen <achen2401@gmail.com>
2018-09-19 12:16:15 -0700

    TN-1484 follow up fix - remove code that prevent auto completion

--- portal/eproms/static/js/landing.js
+++ portal/eproms/static/js/landing.js
@@ -8,9 +8,6 @@ $("document").ready(function() {
     if (typeof sessionStorage !== "undefined") {
         sessionStorage.clear();
     }
-    $("#email").on("focus touchstart", function() {
-        $(this).removeAttr("readonly");
-    });
     $("#email, #password").on("change", function() {
         if ($(this).val()) {
             $("#btnLogin").attr("disabled", false).removeClass("disabled");
