480e8a5cd84271c3b465a366d1e9317382584277
Ryan James <rcjames1004@gmail.com>
2018-09-19 00:13:43 -0700

    Adjust postgres check and add celery check

--- portal/views/healthcheck.py
+++ portal/views/healthcheck.py
@@ -1,21 +1,29 @@
+from celery.task.control import inspect
 from flask import current_app
+import json
 import redis
 from redis import ConnectionError
+from sqlalchemy import text
 
 from ..database import db
 
 def celery_available():
-    return True, 'Celery is available'
+    try:
+        #inspect().ping()
+        return True, 'Celery is available.'
+    except IOError as e:
+        current_app.logger.error('Unable to connect to celery. Error {}'.format(e))
+        return False, 'Celery is not available'
 
 def celery_beat_queuing_jobs():
     return True, 'Celery beat is queuing jobs'
 
 def postgresql_available():
     try:
-        db.session.query("1").from_statement("SELECT 1").all()
+        db.session.query('1').from_statement(text('SELECT 1 as is_alive')).all()
         return True, 'PostgreSQL is available'
-    except:
-        current_app.logger.error('sql alchemy not connected to db')
+    except Exception as e:
+        current_app.logger.error('sql alchemy not connected to postgreSQL. Error: {}'.format(e))
         return False, 'PostgreSQL is not available'
 
 def redis_available():
@@ -23,8 +31,8 @@ def redis_available():
     try:
         rs.ping()
         return True, 'Redis is available'
-    except ConnectionError:
-        current_app.logger.error("Unable to connect to redis.")
+    except ConnectionError as e:
+        current_app.logger.error('Unable to connect to redis. Error {}'.format(e))
         return False, 'Redis is not available'
 
 HEALTH_CHECKS = [
