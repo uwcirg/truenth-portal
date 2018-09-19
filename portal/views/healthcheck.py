9d5a4c89ac435beb9a6936564f2f154b6ca12ceb
Ryan James <rcjames1004@gmail.com>
2018-09-18 23:40:05 -0700

    Add PostgreSQL health check

--- portal/views/healthcheck.py
+++ portal/views/healthcheck.py
@@ -2,14 +2,21 @@ from flask import current_app
 import redis
 from redis import ConnectionError
 
+from ..database import db
+
 def celery_available():
     return True, 'Celery is available'
 
 def celery_beat_queuing_jobs():
     return True, 'Celery beat is queuing jobs'
 
-def postgres_available():
-    return True, 'Postgres is available'
+def postgresql_available():
+    try:
+        db.session.query("1").from_statement("SELECT 1").all()
+        return True, 'PostgreSQL is available'
+    except:
+        current_app.logger.error('sql alchemy not connected to db')
+        return False, 'PostgreSQL is not available'
 
 def redis_available():
     rs = redis.from_url(current_app.config["REDIS_URL"])
@@ -23,6 +30,6 @@ def redis_available():
 HEALTH_CHECKS = [
     celery_available,
     celery_beat_queuing_jobs,
-    postgres_available,
+    postgresql_available,
     redis_available,
 ]
