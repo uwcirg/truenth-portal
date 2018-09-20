afdfbe3ef738b5098b610edd4801a87c37ee7c28
Amy Chen <achen2401@gmail.com>
2018-09-20 10:39:09 -0700

    Feature/eproms landing autocomplete fix (#2657)
    * TN-1484 follow up fix - remove code that prevent auto completion
    
    * clear fields on initial load
    
    * remove unnecessary code

--- portal/eproms/static/js/landing.js
+++ portal/eproms/static/js/landing.js
@@ -1,4 +1,4 @@
-$("document").ready(function() {
+$(document).ready(function() {
     DELAY_LOADING = true; /* global DELAY_LOADING */
     setTimeout(function() {
         DELAY_LOADING = false;
@@ -8,15 +8,5 @@ $("document").ready(function() {
     if (typeof sessionStorage !== "undefined") {
         sessionStorage.clear();
     }
-    $("#email").on("focus touchstart", function() {
-        $(this).removeAttr("readonly");
-    });
-    $("#email, #password").on("change", function() {
-        if ($(this).val()) {
-            $("#btnLogin").attr("disabled", false).removeClass("disabled");
-            return;
-        }
-        $("#btnLogin").attr("disabled", true).addClass("disabled");
-    });
 });
 
