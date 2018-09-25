e28642fe7fd7780c273e699364dcd24fc664433d
Ryan James <rcjames1004@gmail.com>
2018-09-25 11:54:07 -0700

    Update logs and clean up

--- portal/views/healthcheck.py
+++ portal/views/healthcheck.py
@@ -1,15 +1,16 @@
 from datetime import datetime, timedelta
 from flask import Blueprint, current_app
+import os
 import redis
 from redis import ConnectionError
 from sqlalchemy import text
-from subprocess import call
+import subprocess
 
 from ..database import db
 
 
 healthcheck_blueprint = Blueprint('healthcheck', __name__)
-last_celery_beat_health_check = datetime.now()
+last_celery_beat_ping = None
 
 @healthcheck_blueprint.route('/celery_beat_ping')
 def celery_beat_ping():
@@ -18,8 +19,8 @@ def celery_beat_ping():
     Updates the last time we recieved a call to this API.
     This allows us to monitor whether celery beat tasks are running
     """
-    global last_celery_beat_health_check
-    last_celery_beat_health_check = datetime.now()
+    global last_celery_beat_ping
+    last_celery_beat_ping = datetime.now()
     return 'PONG'
 
 
@@ -28,36 +29,44 @@ def celery_beat_ping():
 ##############################
 
 def celery_available():
-    code = call([
-        'celery',
-        '-A', 'portal.celery_worker.celery',
-        'inspect', 'ping'
-    ])
+    FNULL = open(os.devnull, 'w')
+    code = subprocess.call([
+            'celery',
+            '-A', 'portal.celery_worker.celery',
+            'inspect', 'ping'
+        ],
+        stdout=FNULL, # Don't output to console
+        stderr=subprocess.STDOUT
+    )
     if code == 0:
         return True, 'Celery is available.'
     else:
-        current_app.logger.error('Unable to connect to celery.')
+        current_app.logger.error(
+            'Unable to connect to celery. Exit code {}'.format(code)
+        )
         return False, 'Celery is not available'
 
 def celery_beat_available():
-    if last_celery_beat_health_check:
+    if last_celery_beat_ping:
         threshold = timedelta(
             seconds=current_app.config['CELERY_BEAT_HEALTH_CHECK_INTERVAL_SECONDS'] * 2
         )
         time_since_last_beat = \
-            datetime.now() - last_celery_beat_health_check
+            datetime.now() - last_celery_beat_ping
 
         if time_since_last_beat <= threshold:
-            return True, 'Celery beat is available. Last check: {}'.format(last_celery_beat_health_check)
+            return True, 'Celery beat is available. Last check: {}'.format(last_celery_beat_ping)
     
-    return False, "Celery beat is not running jobs. Last check: {}".format(last_celery_beat_health_check)
+    return False, "Celery beat is not running jobs. Last check: {}".format(last_celery_beat_ping)
 
 def postgresql_available():
     try:
         db.engine.execute(text('SELECT 1'))
         return True, 'PostgreSQL is available'
     except Exception as e:
-        current_app.logger.error('sql alchemy not connected to postgreSQL. Error: {}'.format(e))
+        current_app.logger.error(
+            'sql alchemy not connected to postgreSQL. Error: {}'.format(e)
+        )
         return False, 'PostgreSQL is not available'
 
 def redis_available():
@@ -66,7 +75,9 @@ def redis_available():
         rs.ping()
         return True, 'Redis is available'
     except ConnectionError as e:
-        current_app.logger.error('Unable to connect to redis. Error {}'.format(e))
+        current_app.logger.error(
+            'Unable to connect to redis. Error {}'.format(e)
+        )
         return False, 'Redis is not available'
 
 HEALTH_CHECKS = [
