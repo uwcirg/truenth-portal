720c73a0e5e3e606371a32b2b1416515a22dd1df
Amy Chen <achen2401@gmail.com>
2018-10-02 15:36:58 -0700

    add missing information check before submitting terms

--- portal/static/js/initialQueries.js
+++ portal/static/js/initialQueries.js
@@ -726,9 +726,11 @@
                         }
                         if (hasValue(userOrgId) && parseInt(userOrgId) !== 0 && !isNaN(parseInt(userOrgId))) {
                             var topOrg = orgTool.getTopLevelParentOrg(userOrgId);
-                            if (hasValue(topOrg)) {
-                                theTerms["organization_id"] = topOrg;
-                            }
+                            theTerms["organization_id"] = topOrg || userOrgId;
+                        }
+                        if (!theTerms["organization_id"] || !theTerms["agreement_url"]) {
+                            $("#topTerms .post-tou-error").html(i18next.t("Missing information for organization and consent agreement.  Unable to complete request."));
+                            return;
                         }
                         tnthAjax.postTerms(theTerms, $("#topTerms")); // Post terms agreement via API
                     });
