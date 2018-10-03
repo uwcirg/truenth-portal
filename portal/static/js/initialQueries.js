e77d110b932126794407483e05afcf14633464d9
Amy Chen <achen2401@gmail.com>
2018-10-03 14:55:21 -0700

    fix comment

--- portal/static/js/initialQueries.js
+++ portal/static/js/initialQueries.js
@@ -728,7 +728,7 @@
                             var topOrg = orgTool.getTopLevelParentOrg(userOrgId);
                             theTerms["organization_id"] = topOrg || userOrgId;
                         }
-                        if (!theTerms["agreement_url"]) { //this will display error to user if information is missing - not can't check for org id as user might not belong to an org just yet
+                        if (!theTerms["agreement_url"]) { //this will display error to user if information is missing - can't check for org id as user might not belong to an org just yet
                             $("#topTerms .post-tou-error").html(i18next.t("Missing information for consent agreement.  Unable to complete request."));
                             return;
                         }
