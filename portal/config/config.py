a3910fa2d0484bd99adace6e502121b5e35d7898
Ivan Cvitkovic <icvitkovic@gmail.com>
2018-10-18 11:38:43 -0700

    Allow configuration of Piwik values by environment variable (#2734)
    * Allow configuration of Piwik values by environment variable
    
    * Allow multiple domains, comma-delimited

--- portal/config/config.py
+++ portal/config/config.py
@@ -102,8 +102,9 @@ class BaseConfig(object):
     SEXUAL_RECOVERY_TIMEOUT = 60 * 60  # SR users get 1 hour
 
     PERSISTENCE_EXCLUSIONS_DIR = os.environ.get('PERSISTENCE_EXCLUSIONS_DIR')
-    PIWIK_DOMAINS = ""
-    PIWIK_SITEID = 0
+    PIWIK_DOMAINS = os.environ['PIWIK_DOMAINS'].split(',') \
+        if os.environ.get('PIWIK_DOMAINS') else None
+    PIWIK_SITEID = os.environ.get('PIWIK_SITEID')
     PORTAL_STYLESHEET = 'css/portal.css'
     PRE_REGISTERED_ROLES = [
         'access_on_verify', 'write_only', 'promote_without_identity_challenge']
