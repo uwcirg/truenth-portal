b322fafd846da2f8a05cd2780da92bf76ade9b2c
John Howe (UW-CIRG) <johnhowe@uw.edu>
2018-10-29 13:58:54 -0700

    20181029 jhowe Alternate method to check celerybeat, standardize outputs for parser

--- portal/views/healthcheck.py
+++ portal/views/healthcheck.py
@@ -1,4 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import pytz
+import shelve
 
 from flask import Blueprint, current_app
 import redis
@@ -39,30 +42,27 @@ def celery_available():
     if result:
         return True, 'Celery is available.'
     else:
-        return False, 'Celery is not available'
+        return False, 'Celery is not available.'
 
 
 def celery_beat_available():
     """Determines whether celery beat is available"""
-    rs = redis.from_url(current_app.config['REDIS_URL'])
+    # https://github.com/celery/celery/issues/3694
+    # HealthCheck from https://steemit.com/code/@qrul/celery-beat-and-workers-healthchecks
 
-    # When celery beat is running it pings
-    # our service periodically which sets
-    # 'celery_beat_available' in redis. If
-    # that variable expires it means
-    # we have not received a ping from celery beat
-    # within the allowed window and we must assume
-    # celery beat is not available
-    last_celery_beat_ping = rs.get('last_celery_beat_ping')
-    if last_celery_beat_ping:
-        return (
-            True,
-            'Celery beat is available. Last check: {}'.format(
-                last_celery_beat_ping
-            )
-        )
+    now = datetime.now(tz=pytz.utc)
 
-    return False, 'Celery beat is not running jobs'
+    # Name of the file used by PersistentScheduler to store the last run times of periodic tasks.
+    file_data = shelve.open('celerybeat-schedule')
+
+    for task_name, task in file_data['entries'].items():
+        try:
+            assert now  < task.last_run_at + task.schedule.run_every
+        except AttributeError:
+            assert timedelta() < task.schedule.remaining_estimate(task.last_run_at)
+            return False, 'Celery beat is not available.'
+
+    return True, 'Celery beat is available.'
 
 
 def postgresql_available():
@@ -72,12 +72,12 @@ def postgresql_available():
     # If it fails we assume psotgresql is not available.
     try:
         db.engine.execute(text('SELECT 1'))
-        return True, 'PostgreSQL is available'
+        return True, 'PostgreSQL is available.'
     except Exception as e:
         current_app.logger.error(
             'sql alchemy not connected to postgreSQL. Error: {}'.format(e)
         )
-        return False, 'PostgreSQL is not available'
+        return False, 'PostgreSQL is not available.'
 
 
 def redis_available():
@@ -88,12 +88,12 @@ def redis_available():
     rs = redis.from_url(current_app.config["REDIS_URL"])
     try:
         rs.ping()
-        return True, 'Redis is available'
+        return True, 'Redis is available.'
     except Exception as e:
         current_app.logger.error(
             'Unable to connect to redis. Error {}'.format(e)
         )
-        return False, 'Redis is not available'
+        return False, 'Redis is not available.'
 
 
 # The checks that determine the health
