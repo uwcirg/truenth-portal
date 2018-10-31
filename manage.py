72e2eee1b31234d99d86ea93fe0b217775bc56f9
Ryan C James <rcjames1004@gmail.com>
2018-10-31 11:09:43 -0700

    TN-1642 Delay celery beat healthcheck warnings/errors (#2766)
    * Delay celery beat healthcheck warnings/errors until after celery beat starts
    
    * Add new line
    
    * Convert underscores to dashes in flask command

--- manage.py
+++ manage.py
@@ -30,6 +30,7 @@ from portal.models.user import (
     permanently_delete_user,
     validate_email,
 )
+from portal.tasks import celery_beat_health_check
 
 app = create_app()
 
@@ -306,3 +307,8 @@ def config(config_key):
         {k: v for k, v in app.config.items() if isinstance(v, basestring)},
         indent=2,
     ))
+
+
+@app.cli.command()
+def set_celery_beat_healthy():
+    return celery_beat_health_check()
