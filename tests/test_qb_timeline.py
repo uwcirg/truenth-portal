ec4bd92869901490d73e05f9aeadd4049188b8b4
Paul Bugni <pbugni@u.washington.edu>
2018-11-05 12:59:52 -0800

    delay QB switch by RP if user submitted results on old RP

--- tests/test_qb_timeline.py
+++ tests/test_qb_timeline.py
@@ -5,16 +5,15 @@ import pytest
 
 from portal.database import db
 from portal.models.qb_timeline import ordered_qbs, second_null_safe_datetime
-from portal.models.questionnaire_bank import visit_name
+from portal.models.questionnaire_bank import QuestionnaireBank, visit_name
 from portal.views.user import withdraw_consent
 from tests import associative_backdate
+from tests.test_assessment_status import mock_qr
 from tests.test_questionnaire_bank import TestQuestionnaireBank
 
 """Additional Test cases needed:
 
 For ordered_qbs generator:
- - if RP changes during active w/ submitted, do the get the orig RP on active, then new
- - if submission during RP change, does it work
  - on initial submission - clear results till next qb
  - on completed submission - correct status
  - on partial submission - correct status before and after overdue
@@ -136,6 +135,78 @@ class TestQbTimeline(TestQuestionnaireBank):
         assert visit_name(expect_baseline) == 'Baseline'
         assert (
             expect_baseline.questionnaire_bank.research_protocol.name == 'v3')
+        for n in (3, 6, 9, 15, 18, 21, 30):
+            qbd = next(gen)
+            assert visit_name(qbd) == 'Month {}'.format(n)
+            assert qbd.questionnaire_bank.research_protocol.name == 'v3'
+
+        with pytest.raises(StopIteration):
+            next(gen)
+
+    def test_change_midstream_results_rp(self):
+        now = datetime.utcnow()
+        back7, nowish = associative_backdate(
+            now=now, backdate=relativedelta(months=7))
+        back14, nowish = associative_backdate(
+            now=now, backdate=relativedelta(months=14))
+        org = self.setup_qbs(rp_name='v2', retired_as_of=back7)
+        org_id = org.id
+        self.setup_qbs(org=org, rp_name='v3')
+        self.consent_with_org(org_id=org_id, setdate=back14)
+
+        # submit a mock response for 9 month QB on old RP
+        # which should result in v2 for up to 9 month and v3 thereafter
+        qb_name = "CRV_recurring_3mo_period v2"
+        nineMo = QuestionnaireBank.query.filter(
+            QuestionnaireBank.name == qb_name).one()
+        mock_qr('epic_26_v2', qb=nineMo, iteration=1)
+
+        user = db.session.merge(self.test_user)
+        gen = ordered_qbs(user)
+
+        # expect baseline and 3 month in v2, rest in v3
+        expect_baseline = next(gen)
+        assert visit_name(expect_baseline) == 'Baseline'
+        assert (
+            expect_baseline.questionnaire_bank.research_protocol.name == 'v2')
+        for n in (3, 6, 9):
+            qbd = next(gen)
+            assert visit_name(qbd) == 'Month {}'.format(n)
+            assert qbd.questionnaire_bank.research_protocol.name == 'v2'
+        for n in (15, 18, 21, 30):
+            qbd = next(gen)
+            assert visit_name(qbd) == 'Month {}'.format(n)
+            assert qbd.questionnaire_bank.research_protocol.name == 'v3'
+
+        with pytest.raises(StopIteration):
+            next(gen)
+
+    def test_change_before_start_rp_w_result(self):
+        now = datetime.utcnow()
+        back7, nowish = associative_backdate(
+            now=now, backdate=relativedelta(months=7))
+        back14, nowish = associative_backdate(
+            now=now, backdate=relativedelta(months=14))
+        org = self.setup_qbs(rp_name='v2', retired_as_of=back14)
+        org_id = org.id
+        self.setup_qbs(org=org, rp_name='v3')
+        self.consent_with_org(org_id=org_id, setdate=back7)
+
+        # submit a mock response for baseline QB on old RP
+        # which should result in v2 for baseline and v3 thereafter
+        qb_name = "CRV Baseline v2"
+        baseline = QuestionnaireBank.query.filter(
+            QuestionnaireBank.name == qb_name).one()
+        mock_qr('epic_26_v2', qb=baseline, iteration=None)
+
+        user = db.session.merge(self.test_user)
+        gen = ordered_qbs(user)
+
+        # expect everything in v3 post baseline
+        expect_baseline = next(gen)
+        assert visit_name(expect_baseline) == 'Baseline'
+        assert (
+            expect_baseline.questionnaire_bank.research_protocol.name == 'v2')
         for n in (3, 6, 9, 15, 18, 21, 30):
             qbd = next(gen)
             assert visit_name(qbd) == 'Month {}'.format(n)
