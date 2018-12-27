84e57d26fe4adb9ad00539f7ef8618c5a398a793
Ivan Cvitkovic <icvitkovic@gmail.com>
2018-12-27 11:34:15 -0800

    TN-1762 Refactor front-end wrappers (#2863)
    * Create python venv if necessary
    
    * Add comments
    
    * Reorder logging
    
    * Add gulp node_env wrapper
    
    * Use gulp wrapper to call commands
    
    * Fixup paths
    
    * Use gulp-wrapper to extract front-end strings
    
    * Allow gulpfile to be overidden
    
    * Add logging
    
    * Use i18n gulpfile explicitly
    
    * Update comments
    
    * Move default path into function
    
    * Move default node venv path into function
    
    * Improve logging
    
    * Exit early if virtual environment directory exists
    
    * Fixup

--- bin/build-frontend-translations.sh
+++ bin/build-frontend-translations.sh
@@ -21,34 +21,11 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     exit 0
 fi
 
-setup_node_venv() {
-    # Use existing python virtual environment to install nodeenv module
-    . "${repo_root}/env/bin/activate"
-    pip install nodeenv
 
-    nodeenv "$node_venv"
-    deactivate
-}
-
-node_venv="${repo_root}/node_env"
-
-if [ ! -d "${node_venv}" ]; then
-    echo "Creating new virtual environment for NodeJS..."
-    setup_node_venv
-fi
-
-echo "Activating NodeJS virtual environment..."
-. "${node_venv}/bin/activate"
-
-echo "Installing NodeJS dependencies..."
-npm --prefix "${repo_root}/portal" install
-
-PATH="${PATH}:${repo_root}/portal/node_modules/gulp/bin"
-GULPFILE="${repo_root}/portal/i18next_gulpfile.js"
-echo "Running tasks..."
+PATH="${PATH}:${repo_root}/bin"
 
 echo "Converting translations from PO to JSON format"
-gulp.js --gulpfile "$GULPFILE" i18nextConvertPOToJSON
+gulp-wrapper.sh -g "${repo_root}/portal/i18next_gulpfile.js" i18nextConvertPOToJSON
 
 echo "Combining front-end and back-end translation files"
-gulp.js --gulpfile "$GULPFILE" combineTranslationJsons
+gulp-wrapper.sh -g "${repo_root}/portal/i18next_gulpfile.js" combineTranslationJsons
