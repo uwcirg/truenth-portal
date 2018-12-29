e37c5c65abf698d6f64b35c179db4873a2baba40
Ivan Cvitkovic <ivanc@uw.edu>
2018-12-29 13:00:18 -0800

    Fixup logging

--- bin/build-frontend-files.sh
+++ bin/build-frontend-files.sh
@@ -24,7 +24,7 @@ fi
 PATH="${PATH}:${repo_root}/bin"
 GULPFILE="${repo_root}/portal/development_gulpfile.js"
 
-echo "Compiling LESS files"
+echo "Compiling LESS files..."
 gulp-wrapper.sh -g "${GULPFILE}" epromsLess
 gulp-wrapper.sh -g "${GULPFILE}" gilLess
 gulp-wrapper.sh -g "${GULPFILE}" portalLess
