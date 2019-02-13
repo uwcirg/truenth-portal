5dbe13ec768f53039d3022d3f99ded9619c691e3
Amy Chen <achen2401@gmail.com>
2019-02-13 10:58:06 -0800

    TN-1883 add UI changes to prevent multiple clicks of submission button during registration

--- portal/static/js/flask_user/base.js
+++ portal/static/js/flask_user/base.js
@@ -2,5 +2,14 @@ $(document).on("ready", function() {
     $("input[type='text']").on("blur paste", function() {
         $(this).val($.trim($(this).val()));
     });
+    var SUBMIT_CLASS_CONTAINER_NAME = ".form___submit_container";
+    if ($(SUBMIT_CLASS_CONTAINER_NAME).length) {
+        $(SUBMIT_CLASS_CONTAINER_NAME)
+        .css("width", $(SUBMIT_CLASS_CONTAINER_NAME+ " .btn").outerWidth() || 250)
+        .append("<i class='fa fa-spinner fa-spin fa-2x icon'></i>");
+        $(SUBMIT_CLASS_CONTAINER_NAME+" [type='submit']").on("click", function() {
+            $(this).closest(SUBMIT_CLASS_CONTAINER_NAME).addClass("active");
+        });
+    }
 });
 
