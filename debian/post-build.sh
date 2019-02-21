438c339e5ed6dc7ab16f103df8ac090c516e2598
Ivan Cvitkovic <icvitkovic@gmail.com>
2019-02-21 12:59:45 -0800

    Debian-ize; remove build dependency (#3003)
    * Use debian tools to generate changelog
    * Remove build dependencies package after installation
    * Move pre and post-build steps to helper scripts
      * Add comments/logging
    ---
    * Use debian tooling to generate changelog
    
    * Delete build dependency package after done
    
    * Always attempt to build docker image for debian package creation
    
    * Update comment
    
    * Move changelog-generation steps to custom script
    
    * Move repo-preparing steps to custom script
    
    * Fixup whitespace; add comments/logging
    
    * Fix whitespace; improve comments
    
    * Do not apt-get clean; already done by image
    
    See
    https://github.com/debuerreotype/debuerreotype/blob/master/scripts/debuerreotype-minimizing-config
    
    * Fixup whitespace; comment
    
    * Move artifact directory creation into script
    
    * Add pytest

---
+++ debian/post-build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+cmdname="$(basename "$0")"
+bin_path="$(cd "$(dirname "$0")" && pwd)"
+root_path="${bin_path}/.."
+
+
+usage() {
+   cat << USAGE >&2
+Usage:
+   $cmdname [-h] [--help]
+
+   -h
+   --help
+          Show this help message
+
+    Post-build helper script
+
+    Runs steps necessary after package building
+
+USAGE
+   exit 1
+}
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage
+fi
+
+if [ ! -d "${ARTIFACT_DIR}" ]; then
+    echo "Creating artifact directory..."
+    mkdir --parents "${ARTIFACT_DIR}"
+fi
+
+echo "Moving debian package file from working directory to artifact directory..."
+mv --verbose "${root_path}"/../portal_*.deb "${ARTIFACT_DIR}"
+
+echo "Building package list for local repo..."
+cd "${ARTIFACT_DIR}"
+dpkg-scanpackages . | gzip > "${ARTIFACT_DIR}/Packages.gz"
+
+chown --verbose nobody:nogroup "${ARTIFACT_DIR}/"*
