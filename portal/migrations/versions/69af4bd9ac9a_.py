7a684771c38a3a78963152a0dc38432bfb68525e
Paul Bugni <pbugni@u.washington.edu>
2019-02-25 11:35:43 -0800

    apply review suggestions

--- portal/migrations/versions/69af4bd9ac9a_.py
+++ portal/migrations/versions/69af4bd9ac9a_.py
@@ -9,7 +9,7 @@ import copy
 from flask import current_app
 from flask_swagger import swagger
 import jsonschema
-import sys
+import logging
 
 from portal.database import db
 from portal.system_uri import DECISION_SUPPORT_GROUP
@@ -20,6 +20,9 @@ from portal.models.questionnaire_response import QuestionnaireResponse
 # revision identifiers, used by Alembic.
 revision = '69af4bd9ac9a'
 down_revision = 'a1de3ab4d050'
+
+log = logging.getLogger("alembic")
+log.setLevel(logging.INFO)
 
 
 def validate_closure():
@@ -42,7 +45,7 @@ def validate_closure():
             return True
         except jsonschema.ValidationError as e:
             if display_stack:
-                sys.stderr.write(str(e))
+                log.error(str(e))
             return False
 
     return validate
@@ -64,6 +67,7 @@ def correct_invalid_document(qnr_id, validate_fn, sys_id):
     Commits fix and audit row to db
 
     """
+    details = []
     qnr = QuestionnaireResponse.query.get(qnr_id)
     document = copy.deepcopy(qnr.document)
     answer_list = document['group']
@@ -76,36 +80,48 @@ def correct_invalid_document(qnr_id, validate_fn, sys_id):
         the_answer = a['answer']
 
         # remove bogus 'system' at wrong level
-        the_answer.pop('system', None)
+        bogus_system = the_answer.pop('system', None)
+        if bogus_system:
+            msg = "removed 'system' at wrong level w/i 'answer'"
+            if msg not in details:
+                details.append(msg)
 
         # correct string version of numbers
         if 'valueCoding' in the_answer and 'extension' in the_answer[
                 'valueCoding']:
+            msg = "cast valueDecimal to integer"
+            if msg not in details:
+                details.append(msg)
             the_answer['valueCoding']['extension']['valueDecimal'] = int(
                 the_answer['valueCoding']['extension']['valueDecimal'])
 
         # put single answer item in list as required
         if not isinstance(the_answer, list):
+            msg = "force single answer into list"
+            if msg not in details:
+                details.append(msg)
             a['answer'] = [the_answer]
         else:
             a['answer'] = the_answer
 
     # put the answer list under the required dict key 'question'
+    details.append("nest answers under 'question' key")
     document['group'] = {'question': answer_list}
 
     # fix broken identifiers - requires system
     if 'identifier' in document and 'system' not in document['identifier']:
+        details.append("correct identifier missing 'system'")
         system = lookup_system(document['identifier']['label'])
         document['identifier']['system'] = system
 
     # confirm it now validates
     if not validate_fn(document, display_stack=True):
-        sys.stderr.write("ERROR - {} is still invalid".format(qnr_id))
+        log.error("ERROR - {} is still invalid".format(qnr_id))
 
     # create audit trail
     comment = (
-        "Migration: corrected questionnaire_response.id {} ; "
-        "from ill formed document {}".format(qnr_id, qnr.document))
+        "Correct ill formed document from QNR {}: [{}] ".format(
+            qnr_id, ', '.join(details)))
     audit = Audit(
         user_id=sys_id, subject_id=qnr.subject_id, _context='assessment',
         comment=comment)
@@ -120,24 +136,20 @@ def upgrade():
 
     ids_to_patch = []
     validate = validate_closure()
-    sys.stderr.write("invalid QNR.ids: ")
     for qnr in QuestionnaireResponse.query.all():
         if not validate(qnr.document):
             if 'question' in qnr.document['group']:
-                sys.stderr.write(
+                log.error(
                     "question already in group, can't fix {}".format(qnr.id))
                 continue
             ids_to_patch.append(qnr.id)
-            sys.stderr.write(" {}".format(qnr.id))
-        else:
-            sys.stdout.write('.')
-            sys.stdout.flush()
+            log.info("patch QNR {}".format(qnr.id))
 
     sys_user = User.query.filter_by(email='__system__').first()
     sys_id = sys_user.id
 
-    for id in ids_to_patch:
-        correct_invalid_document(id, validate, sys_id)
+    for qnr_id in ids_to_patch:
+        correct_invalid_document(qnr_id, validate, sys_id)
 
     db.session.commit()
 
