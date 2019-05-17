d5d3b55fa14cf8e25f14a8651ccfca8b6baa9eb9
Ivan Cvitkovic <ivanc@uw.edu>
2019-05-17 13:08:39 -0700

    TN-1988 Move unit/integration tests to Gitlab CI (#3212)
    * Move Dockerfile to repo root
    
    * Add Gitlab CI yaml stub
    
    * Add image-based testing
    
    * Do not reinstall pip
    
    * Install testing dependencies before testing
    
    * Separate production-only dependencies
    
    * Restrict tox environments
    
    * Add debugging
    
    * Revert "Add debugging"
    
    This reverts commit e44752873f22839940380caa4fdc6e4f3e334284.
    
    * Fix script block
    
    * Set required env vars
    
    * Add services; configs
    
    * add debug
    
    * debug- exit after first failure
    
    * Fixup service hostnames/configs
    
    * Do not use PGHOST; also read by postgres
    
    * Hard-code psql hostname
    
    * debug- redis url
    
    * Allow redis config by env var
    
    * Start celery if installed
    
    * Add DB URL
    
    * Allow all SQLA env vars to pass through tox
    
    * Allow SQLA DB URL to be overridden
    
    * Remove debugging
    
    * Remove TravisCI python3 job
    
    * Revert "Move Dockerfile to repo root"
    
    This reverts commit 374088609ad276d2d00d21ecd095425943b8f607.
    
    * Revert "debug- exit after first failure"
    
    This reverts commit 720af1accf6e1e39a753e9e285838065867b4820.
    
    * Add python2 job
    
    * Change job names; remove whitespace
    
    * Remove python2 unit test job from TravisCI
    
    * DRY with YAML anchors
    
    * Isolate unit-test job config
    
    * Move docgen testing to GitlabCI
    
    * Fixup YAML error
    
    * debug
    
    * Install deps before starting celery; remove debugging
    
    * Rearrange sections; improve comments
    
    * Do not use normal DB during testing
    
    * Allow REDIS_URL to be overridden during testing with env var
    
    * pep8
    
    * pep8 again
    
    * Set TESTING env var with tox
    
    * Remove individual env vars; add comment
    
    * Add env var postgres container uses to configure DB
    
    * debug; try setting TESTING env var from Gitlab
    
    * fix YAML issue
    
    * Run celery inside tox
    
    * only set TESTING through tox
    
    * Remove debugging
    
    * Only pass testing DB URL through tox
    
    * Use python 3.5 to match debian stretch (3.7 in buster)
    
    * Split lines for readability
    
    * Add todo
    
    * Fixup syntax error

--- portal/config/config.py
+++ portal/config/config.py
@@ -51,18 +51,17 @@ class BaseConfig(object):
         'localhost'
     )
 
-    # Allow Heroku env vars to override most defaults
+    # Allow Heroku environment variables to override service defaults
     # NB: The value of REDIS_URL may change at any point
 
     # We override REDIS_URL when testing now to avoid needing to
     # also reset the other variables using it as a default below
-    if os.environ.get('TESTING', 'false').lower() == 'true':
-        REDIS_URL = 'redis://localhost:6379/5'
-    else:
-        REDIS_URL = os.environ.get(
-            'REDIS_URL',
-            'redis://localhost:6379/0'
-        )
+    REDIS_URL = os.environ.get(
+        'REDIS_URL',
+        'redis://localhost:6379/5'
+        if os.environ.get('TESTING', 'false').lower() == 'true'
+        else 'redis://localhost:6379/0',
+    )
 
     ANONYMOUS_USER_ACCOUNT = True
     BROKER_URL = os.environ.get(
@@ -137,7 +136,10 @@ class BaseConfig(object):
     SHOW_PUBLIC_TERMS = True
     SHOW_WELCOME = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = best_sql_url()
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'SQLALCHEMY_DATABASE_URI',
+        best_sql_url()
+    )
     SESSION_PERMANENT = True
     SESSION_TYPE = 'redis'
 
