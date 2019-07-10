31e00be9882cb8556a964ab45616eb49a4535ef0
Ivan Cvitkovic <ivanc@uw.edu>
2019-07-10 14:29:31 -0700

    Update sphinx docs ref to python3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -317,6 +317,6 @@ texinfo_documents = [(
 # texinfo_no_detailmenu = False
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/2.7', None),
+    'python': ('https://docs.python.org/3', None),
     'werkzeug': ('http://werkzeug.pocoo.org/docs/latest', None),
 }
