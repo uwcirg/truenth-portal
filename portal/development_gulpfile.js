47bbb81d14d1ebabe81a3741325080c0fdd04882
Amy Chen <achen2401@gmail.com>
2019-08-23 17:29:11 -0700

    fix path, accordion, table fixes

--- portal/development_gulpfile.js
+++ portal/development_gulpfile.js
@@ -208,7 +208,7 @@ const exerciseDietLess = function(callback) {
             plugins: [cleancss]
         }))
         .pipe(postCSS())
-        .pipe(sourcemaps.write("../../../"+mapPath)) /* note to write external source map files, pass a path relative to the destination */
+        .pipe(sourcemaps.write("../../../../"+mapPath)) /* note to write external source map files, pass a path relative to the destination */
         .pipe(dest(EDPath + cssPath))
         .on("end", function() {
             replaceStd(ED + ".css.map");
