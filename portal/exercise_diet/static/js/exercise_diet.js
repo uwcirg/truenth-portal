074b902b37cd779eb7ff422d84fe6e0ff233ae1e
Amy Chen <achen2401@gmail.com>
2019-08-26 17:36:56 -0700

    clean up js, fixes based on feedback

--- portal/exercise_diet/static/js/exercise_diet.js
+++ portal/exercise_diet/static/js/exercise_diet.js
@@ -1,36 +1,47 @@
-$(document).ready(function(){
-    var fullpath = $(location).attr('pathname').split("/");
-    var pathname = "." + fullpath[fullpath.length-1];
-    $('#mainNavbar .nav '+pathname).addClass('active');
-});
-
 function show_diet(new_item, current_item) {
-    $('#'+new_item).on('shown.bs.modal', function(e) {
-        $('body').addClass('modal-open');
+    $("#"+new_item).on("shown.bs.modal", function(e) {
+        $("body").addClass("modal-open");
     });
-    $('#'+new_item).on('hide.bs.modal', function(e) {
-        $('body').removeClass('modal-open');
+    $("#"+new_item).on("hide.bs.modal", function(e) {
+        $("body").removeClass("modal-open");
     });
-    $('#'+current_item).modal('hide');
-    $('#'+new_item).modal('show');
+    $("#"+current_item).modal("hide");
+    $("#"+new_item).modal("show");
 }
 
-$(function(){
-
-    setTimeout(function() {
-        $("main").addClass("active");
-    }, 750);
-    
-    $('[data-toggle=modal]').on('click touchend', function(e) {
-        var target;
-        e.preventDefault();
-        e.stopPropagation();
-        target = $(this).attr('data-target');
-        $(target).appendTo('body');
-        return setTimeout(function() {
-            $(target).modal('show');
-        }, 50);
+function show_exercise(new_item, current_item) {
+    $("#"+new_item).on("shown.bs.modal", function(e) {
+        $("body").addClass("modal-open");
     });
+
+    $("#"+new_item).on("hide.bs.modal", function(e) {
+        $("body").removeClass("modal-open");
+    });
+
+    $("#"+current_item).modal("hide");
+    $("#"+new_item).modal("show");
+}
+function show_recipe(heading, item, recipe_type) {
+    if (recipe_type === "tip") {
+        $("#recipe-modal").addClass("tip");
+    }
+    else {
+        $("#recipe-modal").removeClass("tip");
+    }
+    $("#recipe-modal").modal("show");
+    $(".modal-body").addClass("load");
+    $(".modal-body" ).load( "recipe/" + heading + "/" + item, function() {
+        setTimeout(function() {
+            $(".modal-body").removeClass("load");
+        }, 500);
+    });
+}
+function setActiveMenuLink() {
+    var fullpath = $(location).attr("pathname").split("/");
+    var pathname = "." + fullpath[fullpath.length-1];
+    $("#mainNavbar .nav "+pathname).addClass("active");
+}
+function setVideoLinkEvent() {
     $(".watch-button-video").on("click", function(e) {
         e.stopPropagation();
         var src = $(".video-module").data("iframe-src");
@@ -39,89 +50,61 @@ $(function(){
         }
         $(this).fadeOut();
     });
+}
+function setAccordionElementsEvent() {
     $("#Recipe-Accordions [role='tabpanel']").on("shown.bs.collapse", function() {
         $(this).addClass("visited");
     });
     $("#Recipe-Accordions [role='tabpanel']").on("hidden.bs.collapse", function() {
         $(this).removeClass("visited");
     });
-});
-
-function show_exercise(new_item, current_item) {
-    $('#'+new_item).on('shown.bs.modal', function(e) {
-        $('body').addClass('modal-open');
-    });
-
-    $('#'+new_item).on('hide.bs.modal', function(e) {
-        $('body').removeClass('modal-open');
-    });
-
-    $('#'+current_item).modal('hide');
-    $('#'+new_item).modal('show');
 }
-
-$(function(){
+function setResourcesSubmitEvent() {
     $("#Resources input[type='submit']").on("click",function() {
         var zipcode = $("input[name='zipcode']").val();
         var url = "https://www.livestrong.org/ymca-search?distance[postal_code]="+zipcode+"&distance[search_distance]=20&distance[search_units]=mile";
-        window.open(url, '_blank');
+        window.open(url, "_blank");
     });
-});
-
-function show_recipe(heading, item, recipe_type) {
-    if (recipe_type === "tip") {
-        $("#recipe-modal").addClass("tip");
-    }
-    else {
-        $("#recipe-modal").removeClass("tip");
-    }
-    $('#recipe-modal').modal('show');
-    $(".modal-body").addClass("load");
-    $(".modal-body" ).load( "recipe/" + heading + "/" + item, function() {
-        setTimeout(function() {
-            $(".modal-body").removeClass("load");
-        }, 500);
-    });
-    // $("#recipe-modal").on("show.bs.modal", function() {
-    //     $( ".modal-body" ).load( "recipe/" + heading + "/" + item, function() {
-    //         setTimeout(function() {
-    //             $(".modal-body").removeClass("load");
-    //         }, 500);
-    //     });
-    // });
-    // $(this).removeClass("load");
-    // $("#recipe-modal").on("shown.bs.modal", function() {
-    //     setTimeout(function() {
-    //         $(".modal-body").removeClass("load");
-    //     }, 150);
-    // });
-   
-    
 }
-
-// $(function(){
-
-//     $("td').on('mouseenter',function(){
-//         $(this).addClass('cell-hover');
-//         return false;
-//     });
-
-//     $('td').on('mouseleave',function(){
-//         $(this).removeClass('cell-hover');
-//         return false;
-//     });
-
-// });
-
-$(document).ready(function() {
-    if ($("#Exercise-Diet-Portal").length > 0) {
-        //setTimeout(function() {
-        $("body").addClass("page-exercise-diet-portal");
-        //}, 100);
-    }
+function setScrollToLinkEvent() {
     $(".js-scroll-to").attr("style", "").on("click", function() {
         $("html, body").animate({
             scrollTop: ($(".anchor").offset().top - 80)
         },1000);
     });
+}
+function setInitVis() {
+    if ($("#Exercise-Diet-Portal").length > 0) {
+        $("body").addClass("page-exercise-diet-portal");
+    }
+    setTimeout(function() {
+        $("main").addClass("active");
+    }, 750);
+}
+function setModalElementsEvent() {
+    $("[data-toggle=modal]").on("click touchend", function(e) {
+        var target;
+        e.preventDefault();
+        e.stopPropagation();
+        target = $(this).attr("data-target");
+        $(target).appendTo("body");
+        return setTimeout(function() {
+            $(target).modal("show");
+        }, 50);
+    });
+}
+
+$(function(){
+    //set active tab
+    setActiveMenuLink();
+    //bind modal event
+    setModalElementsEvent();
+    //bind video link event - video link that plays video
+    setVideoLinkEvent();
+    //bind accordion element event - element as seen in the recipies tab
+    setAccordionElementsEvent();
+    //Resources 
+    setResourcesSubmitEvent();
+    setScrollToLinkEvent();
+    setInitVis();
 });
