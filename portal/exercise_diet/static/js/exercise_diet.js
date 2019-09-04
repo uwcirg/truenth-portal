50e55374e8dfc03ead32a0f4cc857c6c2a341237
Amy Chen <achen2401@gmail.com>
2019-09-04 12:55:18 -0700

    Test/ed content change (#3450)
    * TN-2181 re-styling addition
    
    * a few changes based on feedback
    
    * more fixes based on feedback
    
    * responsive fixes
    
    * responsive fixes
    
    * more responsive fixes
    
    * correct css rules
    
    * overlay, minibox styling fixes
    
    * IE css fix
    
    * remove unwanted file
    
    * add overlay for intro page
    
    * responsive fixes
    
    * fix modal font size
    
    * fix minibox background color
    
    * fix recipe and diet pages stylings
    
    * fix path, accordion, table fixes
    
    * try new background color
    
    * add lighter variation theme color
    
    * fix loader z index
    
    * fix loader position
    
    * fix diet top margin
    
    * fix recipe background
    
    * fix intro section padding
    
    * fix intro line height
    
    * margin/padding updates
    
    * fix diet table styling
    
    * fix navbar margin
    
    * fix cell header background color
    
    * navbar margin at desktop view
    
    * add button for mobile view
    
    * fix mobile view button styling
    
    * fix bold text in intro
    
    * fix accordion element margin
    
    * fix h2 element line height
    
    * fix accordion element click event
    
    * fix link styling
    
    * fix accordion element table element styling
    
    * more accordion element styling fix
    
    * clean up js, fixes based on feedback
    
    * fix table cell background
    
    * fix modal table cell background style
    
    * feedback fixes
    
    * navbar fix
    
    * collapsible element styling
    
    * fix anchor link hover styling
    
    * fix modal bottom styling collapsible header styling
    
    * fix typo
    
    * remove commented out code
    
    * fix highlight color
    
    * responsive layout fixes
    
    * feedback changes
    
    * feedback fixes
    
    * hide navbar on scroll
    
    * fix animation
    
    * fix overlay background for exercise tab
    
    * modal element css fixes
    
    * fix big image css in modal
    
    * more feedback changes
    
    * fix resources page
    
    * fix button size
    
    * use dynamic width for minibox
    
    * resources layout fix
    
    * feedback changes
    
    * adjust hero image title font size
    
    * modal table styling fixes
    
    * responsive fixes
    
    * fix scrolling event on modal
    
    * fix recipe table background
    
    * fix modal button
    
    * fix exercise diet modal image location
    
    * fix nav link hover color
    
    * fix mini box layout
    
    * fix mini box width
    
    * more minibox width fix
    
    * responsive fixes related to banner presence
    
    * fix top margin
    
    * responsive fix
    
    * responsive fixes
    
    * fix indentation in view file
    
    * add responsive fix
    
    * acknowledgement fix
    
    * ack
    
    * cleanup code
    
    * remove commented out code
    
    * pep8 fixes
    
    * remove trailing space
    
    * fixes based on feedback
    
    * remove unwanted file
    
    * fix path
    
    * remove file
    
    * fix nav typo

--- portal/exercise_diet/static/js/exercise_diet.js
+++ portal/exercise_diet/static/js/exercise_diet.js
@@ -1,52 +1,48 @@
-$(document).ready(function(){
-    var fullpath = $(location).attr('pathname').split("/");
-    var pathname = "." + fullpath[fullpath.length-1];
-    $('#mainNavbar .nav '+pathname).addClass('active');
-});
-
 function show_diet(new_item, current_item) {
-    $('#'+new_item).on('shown.bs.modal', function(e) {
-        $('body').addClass('modal-open');
+    $("#"+new_item).on("shown.bs.modal", function(e) {
+        $("body").addClass("modal-open");
     });
-    $('#'+new_item).on('hide.bs.modal', function(e) {
-        $('body').removeClass('modal-open');
+    $("#"+new_item).on("hide.bs.modal", function(e) {
+        $("body").removeClass("modal-open");
     });
-    $('#'+current_item).modal('hide');
-    $('#'+new_item).modal('show');
+    $("#"+current_item).modal("hide");
+    $("#"+new_item).modal("show");
 }
 
-$(function(){
-
-    $('#diet-table tr').on('mouseenter',function(){
-        $(this).addClass('row-hover');
-        return false;
+function show_exercise(new_item, current_item) {
+    $("#"+new_item).on("shown.bs.modal", function(e) {
+        $("body").addClass("modal-open");
     });
 
-    $('#diet-table tr').on('mouseleave',function(){
-        $(this).removeClass('row-hover');
-        return false;
+    $("#"+new_item).on("hide.bs.modal", function(e) {
+        $("body").removeClass("modal-open");
     });
 
-    $('#avoid-foods-table td').on('mouseenter',function(){
-        $(this).addClass('cell-hover');
-        return false;
+    $("#"+current_item).modal("hide");
+    $("#"+new_item).modal("show");
+}
+function show_recipe(heading, item, recipe_type) {
+    if (recipe_type === "tip") {
+        $("#recipe-modal").addClass("tip");
+    }
+    else {
+        $("#recipe-modal").removeClass("tip");
+    }
+    $("#recipe-modal").modal("show");
+    $(".modal-body").addClass("load");
+    $(".modal-body" ).load( "recipe/" + heading + "/" + item, function() {
+        setTimeout(function() {
+            $(".modal-body").removeClass("load");
+            $("#recipe-modal").animate({ scrollTop: 0 }, "slow");
+        }, 750);
     });
-
-    $('#avoid-foods-table td').on('mouseleave',function(){
-        $(this).removeClass('cell-hover');
-        return false;
-    });
-
-    $('[data-toggle=modal]').on('click touchend', function(e) {
-      var target;
-      e.preventDefault();
-      e.stopPropagation();
-      target = $(this).attr('data-target');
-      $(target).appendTo('body');
-      return setTimeout(function() {
-        $(target).modal('show');
-      }, 150);
-    });
+}
+function setActiveMenuLink() {
+    var fullpath = $(location).attr("pathname").split("/");
+    var pathname = "." + fullpath[fullpath.length-1];
+    $("#mainNavbar .nav "+pathname).addClass("active");
+}
+function setVideoLinkEvent() {
     $(".watch-button-video").on("click", function(e) {
         e.stopPropagation();
         var src = $(".video-module").data("iframe-src");
@@ -55,64 +51,73 @@ $(function(){
         }
         $(this).fadeOut();
     });
+}
+function setAccordionElementsEvent() {
     $("#Recipe-Accordions [role='tabpanel']").on("shown.bs.collapse", function() {
         $(this).addClass("visited");
     });
     $("#Recipe-Accordions [role='tabpanel']").on("hidden.bs.collapse", function() {
         $(this).removeClass("visited");
     });
-});
-
-function show_exercise(new_item, current_item) {
-    $('#'+new_item).on('shown.bs.modal', function(e) {
-        $('body').addClass('modal-open');
-    });
-
-    $('#'+new_item).on('hide.bs.modal', function(e) {
-        $('body').removeClass('modal-open');
-    });
-
-    $('#'+current_item).modal('hide');
-    $('#'+new_item).modal('show');
 }
-
-$(function(){
+function setResourcesSubmitEvent() {
     $("#Resources input[type='submit']").on("click",function() {
         var zipcode = $("input[name='zipcode']").val();
         var url = "https://www.livestrong.org/ymca-search?distance[postal_code]="+zipcode+"&distance[search_distance]=20&distance[search_units]=mile";
-        window.open(url, '_blank');
+        window.open(url, "_blank");
     });
-});
-
-function show_recipe(heading, item, recipe_type) {
-    if (recipe_type === 'tip') {
-        $('#recipe-modal').addClass('tip');
+}
+function setScrollToLinkEvent() {
+    $(".js-scroll-to").attr("style", "").on("click", function() {
+        $("html, body").animate({
+            scrollTop: ($(".anchor").offset().top - 80)
+        },1000);
+    });
+}
+function setInitVis() {
+    if ($("#Exercise-Diet-Portal").length > 0) {
+        $("body").addClass("page-exercise-diet-portal");
     }
-    else {
-        $('#recipe-modal').removeClass('tip');
+    if (!$("#upperBanner").length) {
+        $("html").addClass("is-upper-banner-closed");
     }
-    $('#recipe-modal').modal('show');
-    $( ".modal-body" ).load( "recipe/" + heading + "/" + item );
+    setTimeout(function() {
+        $("main").addClass("active");
+    }, 750);
+}
+function setModalElementsEvent() {
+    $("[data-toggle=modal]").on("click touchend", function(e) {
+        var target;
+        e.preventDefault();
+        e.stopPropagation();
+        target = $(this).attr("data-target");
+        $(target).appendTo("body");
+        return setTimeout(function() {
+            $(target).modal("show");
+        }, 50);
+    });
+    //exercise modal nav link
+    $(".exercise-navlink").on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        show_exercise($(this).attr("data-newItem"), $(this).attr("data-currentItem"));
+        return false;
+    });
 }
 
 $(function(){
-
-    $('td').on('mouseenter',function(){
-        $(this).addClass('cell-hover');
-        return false;
-    });
-
-    $('td').on('mouseleave',function(){
-        $(this).removeClass('cell-hover');
-        return false;
-    });
-
-});
-
-$(document).ready(function() {
-    if ($("#Exercise-Diet-Portal").length > 0) {
-        setTimeout(function() {
-            $("body").addClass("page-exercise-diet-portal");
-        }, 100);
-    }
+    //set active tab
+    setActiveMenuLink();
+    //bind modal event
+    setModalElementsEvent();
+    //bind video link event - video link that plays video
+    setVideoLinkEvent();
+    //bind accordion element event - element as seen in the recipies tab
+    setAccordionElementsEvent();
+    //Resources 
+    setResourcesSubmitEvent();
+    //down arrow link event - element seen on top of Hero image
+    setScrollToLinkEvent();
+    //allow content to load before showing content
+    setInitVis();
 });
