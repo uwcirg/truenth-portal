2e4ead3652ab106639c4df4b0795ef11d9d7c0d2
Paul Bugni <pbugni@u.washington.edu>
2019-09-23 16:24:22 -0700

    Support standard access to check if an intervention is defined in application.

--- portal/models/intervention.py
+++ portal/models/intervention.py
@@ -3,6 +3,7 @@ from flask import current_app
 from sqlalchemy import and_
 from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.exceptions import BadRequest
 
 from ..database import db
@@ -392,10 +393,12 @@ class _NamedInterventions(object):
         # (i.e. not from static list)
         try:
             value = self.__dict__[attr.lower()].__call__(self)
+        except NoResultFound:
+            raise AttributeError("Intervetion {} not fount".format(attr))
         except KeyError:
             query = Intervention.query.filter_by(name=attr)
             if not query.count():
-                raise ValueError(
+                raise AttributeError(
                     "Intervention {} not found".format(attr))
             value = query.one()
         return value
@@ -404,7 +407,18 @@ class _NamedInterventions(object):
         for attr in dir(self):
             if attr.startswith('_'):
                 continue
-            yield getattr(self, attr)
+            try:
+                yield getattr(self, attr)
+            except AttributeError:
+                # Intervention from static list not found in db, skip
+                continue
+
+    def __contains__(self, item):
+        try:
+            self.__getattribute__(item)
+            return True
+        except AttributeError:
+            return False
 
 
 """INTERVENTION behaves like a static accessor for all interventions.
