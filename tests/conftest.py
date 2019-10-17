848747a85f7becc39c7000680d95a4a5114c6f99
Paul Bugni <pbugni@u.washington.edu>
2019-10-17 16:14:24 -0700

    Added pytest fixtures for client (app) and celery tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 # test plugin
 # https://docs.pytest.org/en/latest/writing_plugins.html#conftest-py-plugins
 import pytest
+from portal.factories.app import create_app
+from portal.factories.celery import create_celery
 
 
 def pytest_addoption(parser):
@@ -10,3 +12,42 @@ def pytest_addoption(parser):
         default=False,
         help="run selenium ui tests",
     )
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    app_ = create_app()
+    ctx = app_.app_context()
+    ctx.push()
+
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
+    return app_
+
+
+@pytest.fixture(scope='session')
+def celery_worker_parameters():
+    # type: () -> Mapping[str, Any]
+    """Redefined like fixture from celery.contrib.pytest.py as instructed
+
+    Specifically, we need to extend the default queues so the celery worker
+    will process tasks given to either queue.
+
+    The dict returned by your fixture will then be used
+    as parameters when instantiating :class:`~celery.worker.WorkController`.
+    """
+    return {
+        'queues': ('celery', 'low_priority'),
+        'perform_ping_check': False}
+
+
+@pytest.fixture(scope='session')
+def celery_app(app):
+    # change default queue to low
+    #app.config['CELERY_TASK_DEFAULT_QUEUE'] = 'low_priority'
+    celery = create_celery(app)
+    # for use celery_worker fixture
+    from celery.contrib.testing import tasks  # NOQA
+    return celery
