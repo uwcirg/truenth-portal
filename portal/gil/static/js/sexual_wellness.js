f2f2948b0807c94c43678bf6973a7a6b3c34d1e7
Amy Chen <achen2401@gmail.com>
2019-10-24 11:33:02 -0700

    fix mobile styling

--- portal/gil/static/js/sexual_wellness.js
+++ portal/gil/static/js/sexual_wellness.js
@@ -2,11 +2,15 @@ $(function() {
     $(".item__link").on("click", function() {
         $(".item__link").removeClass("active");
         $(this).addClass("active");
-        $(".content__item").hide().removeClass("active");
-        $(".content__item[data-group="+ $(this).attr("data-group") + "]").fadeIn().addClass("active");
+        $(".content__item").removeClass("active");
+        $(".content__item[data-group="+ $(this).attr("data-group") + "]").addClass("active");
     });
     var activeItemLinkGroup = $(".item__link.active").attr("data-group");
     if (activeItemLinkGroup) {
-        $(".content__item[data-group="+activeItemLinkGroup+"]").fadeIn().addClass("active");
+        $(".content__item[data-group="+activeItemLinkGroup+"]").addClass("active");
     }
+    $(".content__item .title").on("click", function() {
+        $(".content__item").removeClass("active");
+        $(this).closest(".content__item").toggleClass("active");
+    });
 });
