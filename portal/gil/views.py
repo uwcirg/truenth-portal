0bb1f0b54600c642577a1efbf8a97f3496691280
Amy Chen <achen2401@gmail.com>
2019-10-25 10:03:29 -0700

    serve link content from liferay

--- portal/gil/views.py
+++ portal/gil/views.py
@@ -30,6 +30,7 @@ from ..models.user import current_user, get_user_or_abort
 from ..system_uri import SHORTCUT_ALIAS
 from ..views.auth import next_after_login
 from ..views.crossdomain import crossdomain
+from ..views.portal import get_any_tag_data, get_asset
 
 gil = Blueprint(
     'gil', __name__, template_folder='templates', static_folder='static',
@@ -299,7 +300,10 @@ def lived_experience():
 
 @gil.route('/sexual-wellbeing')
 def sexual_wellbeing():
-    return render_template('gil/sexual_wellbeing.html', user=current_user())
+    data = get_any_tag_data('sexualwellbeing_links_content')
+    asset = get_asset(data['results'][0]['uuid'])
+    return render_template('gil/sexual_wellbeing.html',
+                            user=current_user(), asset=asset)
 
 
 @gil.route('/stories/<string:page_name>')
