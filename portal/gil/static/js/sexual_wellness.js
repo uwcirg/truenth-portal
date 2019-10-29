1da892897fe6fafd90d2f7dc7221f239cdf0bd0c
Amy Chen <achen2401@gmail.com>
2019-10-29 12:50:31 -0700

    clean up JS, add comment

--- portal/gil/static/js/sexual_wellness.js
+++ portal/gil/static/js/sexual_wellness.js
@@ -9,34 +9,36 @@ $(function() {
         }).done(function(data) {
             requestAttempt++;
             $("#sexualWellbeingMainContent").html(data).removeClass("error-message");
-            $(".item__link").on("click", function() {
-                $(".item__link").removeClass("active");
-                $(this).addClass("active");
-                $(".content__item").removeClass("active");
-                $(".content__item[data-group="+ $(this).attr("data-group") + "]").addClass("active");
-            });
-            var activeItemLinkGroup = $(".item__link.active").attr("data-group");
-            if (activeItemLinkGroup) {
-                $(".content__item[data-group="+activeItemLinkGroup+"]").addClass("active");
-            }
-            $(".content__item .title").on("click", function() {
-                $(".content__item").removeClass("active");
-                $(this).closest(".content__item").toggleClass("active");
-            });
-            $(".content__item--links li").on("click", function() {
-                $(this).find("a")[0].click();
-            });
+            initLinksEvent();
             $(".pre-loader").hide();
     
         }).fail(function(xhr, status, error) {
             if (requestAttempt <= 3) {
-                console.log("attempt: ", requestAttempt);
                 setTimeout(function() { getContent;}, 3000);
             } else {
                 $("#sexualWellbeingMainContent").html(error).addClass("error-message");
                 $(".pre-loader").hide();
                 requestAttempt = 0;
             }
+        });
+    };
+    var initLinksEvent = function() {
+        $(".item__link").on("click", function() {
+            $(".item__link").removeClass("active");
+            $(this).addClass("active");
+            $(".content__item").removeClass("active");
+            $(".content__item[data-group="+ $(this).attr("data-group") + "]").addClass("active");
+        });
+        var activeItemLinkGroup = $(".item__link.active").attr("data-group");
+        if (activeItemLinkGroup) {
+            $(".content__item[data-group="+activeItemLinkGroup+"]").addClass("active");
+        }
+        $(".content__item .title").on("click", function() {
+            $(".content__item").removeClass("active");
+            $(this).closest(".content__item").toggleClass("active");
+        });
+        $(".content__item--links li").on("click", function() {
+            $(this).find("a")[0].click();
         });
     };
     var initStartTime = new Date();
