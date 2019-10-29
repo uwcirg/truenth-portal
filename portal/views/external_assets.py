5b384aa4829d40fc2da818e71dc202d6f9ed71cd
Paul Bugni <pbugni@u.washington.edu>
2019-10-29 12:24:13 -0700

    new module for ajax queries to portal cached (not done yet) liferay assets

---
+++ portal/views/external_assets.py
@@ -0,0 +1,23 @@
+"""Views used to obtain external assets
+
+Primarily LifeRay content access - via exposed APIs for portal side caching
+"""
+from flask import Blueprint, current_app
+import requests
+
+external_assets = Blueprint('external_assets', __name__)
+
+
+@external_assets.route('/api/external/tag/<tag>', methods=('GET',))
+def by_tag(tag):
+    url = "{}/c/portal/truenth/asset/query".format(
+        current_app.config["LR_ORIGIN"])
+    return requests.get(
+        url, params={'allTags': tag, 'returnFirst': True}).json()['asset']
+
+
+@external_assets.route('/api/external/uuid/<uuid>', methods=('GET',))
+def by_uuid(uuid):
+    url = "{}/c/portal/truenth/asset/detailed".format(
+        current_app.config["LR_ORIGIN"])
+    return requests.get(url, params={'uuid': uuid}).json()['asset']
