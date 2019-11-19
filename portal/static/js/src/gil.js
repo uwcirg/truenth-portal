33973a57cb2c67d782100abd8095d8ae544a3db2
Amy Chen <achen2401@gmail.com>
2019-11-19 11:13:50 -0800

    Feature/refactor footer api (#3546)
    * TN-2285 refector GIL footer content to use API
    
    * fix url for
    
    * fix comment
    
    * refactors
    
    * remove unused code
    
    * add comment
    
    * remove unneeded code, use common ajax call
    
    * remove unwanted code, add comment
    
    * fix param
    
    * fix broken links
    
    * styling fixes, add comment
    
    * remove white space, disabled link styling fix
    
    * fix comment
    
    * add comment

--- portal/static/js/src/gil.js
+++ portal/static/js/src/gil.js
@@ -1,4 +1,5 @@
 import Utility from "./modules/Utility.js";
+import tnthAjax from "./modules/TnthAjax.js";
 import __i18next from "./modules/I18nextConfig.js";
 $.ajaxSetup({
   beforeSend: function(xhr, settings) {
@@ -1354,7 +1355,6 @@ module.exports = utilObj = (function() {
       var results = regex.exec(location.search);
       return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     };
-
     this.initSessionMonitor = function() {
       /*
        * get portal wrapper nav content
@@ -1392,6 +1392,13 @@ module.exports = utilObj = (function() {
       });
     };
 
+    this.setFooter = function(currentUserId) {
+      tnthAjax.getPortalFooter("", false,  false, function(data) {
+        $("footer").html(data);
+        window.app.menuObj.filterMenu(currentUserId);
+      });
+    };
+
     this.handleLoginAsUser = function() {
       var __LOGIN_AS_PATIENT = (typeof sessionStorage !== "undefined") ? sessionStorage.getItem("loginAsPatient") : null;
       if (__LOGIN_AS_PATIENT) {
@@ -1425,6 +1432,7 @@ $(document).ready(function(){
   } else {
     window.app.utilObj.handlePostLogout();
   }
+  window.app.utilObj.setFooter(currentUserId); 
   window.app.utilObj.initSessionMonitor();
 
   $(".button--login--register").on("click", function () {
