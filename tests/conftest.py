1c158c4bf5b9952ebaa870fd163d0a781068c02c
Wayne Lai <wayne128@uw.edu>
2019-12-16 13:27:33 -0800

    Fix pep8 errors in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -93,14 +93,14 @@ def celery_app(app):
     return celery
 
 
-#@pytest.fixture(scope="session")
-#def setUp():
-#    add_static_concepts(only_quick=True)
-#    add_static_interventions()
-#    add_static_organization()
-#    add_static_relationships()
-#    add_static_roles()
-#    db.session.commit()
+# @pytest.fixture(scope="session")
+# def setUp():
+#     add_static_concepts(only_quick=True)
+#     add_static_interventions()
+#     add_static_organization()
+#     add_static_relationships()
+#     add_static_roles()
+#     db.session.commit()
 
 
 @pytest.fixture(scope="session")
@@ -116,10 +116,10 @@ def test_user(app):
 
     test_user = User(
         username=TEST_USERNAME, first_name=FIRST_NAME,
-        last_name=LAST_NAME, image_url=IMAGE_URL, 
+        last_name=LAST_NAME, image_url=IMAGE_URL,
         password=DEFAULT_PASSWORD)
     with SessionScope(db):
-        #User.query.filter_by(username=TEST_USERNAME).delete()
+        # User.query.filter_by(username=TEST_USERNAME).delete()
         db.session.add(test_user)
         db.session.commit()
     test_user = db.session.merge(test_user)
@@ -131,7 +131,7 @@ def test_user(app):
 @pytest.fixture
 def add_user(app):
     def add_user(
-            username, first_name="", last_name="", 
+            username, first_name="", last_name="",
             image_url=None, password="fakePa$$", email=None):
         db.drop_all()
         db.create_all()
@@ -141,7 +141,7 @@ def add_user(app):
         password = app.user_manager.hash_password(password)
 
         test_user = User(
-            username=username, first_name=first_name, 
+            username=username, first_name=first_name,
             last_name=last_name, image_url=image_url, password=password)
         if email is not None:
             test_user.email = email
@@ -173,7 +173,7 @@ def add_service_user(test_user):
         with SessionScope(db):
             db.session.add(service_user)
             db.session.commit()
-    
+
         return db.session.merge(service_user)
     return add_service_user
 
@@ -184,9 +184,9 @@ def login(app, client):
         follow_redirects = True
 
         app.config.from_object(TestConfig)
-        
+
         oauth_info = {'user_id': user_id}
-    
+
         return client.get(
                 'test/oauth',
                 query_string=oauth_info,
@@ -207,7 +207,7 @@ def promote_user(test_user):
         add_static_relationships()
         add_static_roles()
         db.session.commit()
-    
+
         """Bless a user with role needed for a test"""
         if not user:
             user = test_user
@@ -240,50 +240,53 @@ def assert_redirects(app):
             expected_location = urljoin("http://%s" % server_name, location)
 
         valid_status_codes = (301, 302, 303, 305, 307)
-        valid_status_code_str = ', '.join(str(code) for code in valid_status_codes)
-        not_redirect = "HTTP Status %s expected but got %d" % (valid_status_code_str, response.status_code)
-        assert(response.status_code in valid_status_codes, message or not_redirect)
+        valid_status_code_str = ', '.join(
+                str(code) for code in valid_status_codes)
+        not_redirect = "HTTP Status %s expected but got %d" % (valid_status_code_str,
+                response.status_code)
+        assert(response.status_code in valid_status_codes,
+                message or not_redirect)
         assert(response.location == expected_location), message
     return assertRedirects
 
-#@pytest.fixture(scope="session")
-#def add_client(promote_user):
-#    client_id = 'test_client'
-#    client = Client(
-#        client_id=client_id, _redirect_uris='http://localhost',
-#        client_secret='tc_secret', user_id=TEST_USER_ID)
-#    with SessionScope(db):
-#        db.session.add(client)
-#        db.session.commit()
-#    return db.session.merge(client)
-#
-#
-#@pytest.fixture(scope="session")
-#def login():
-#    TEST_USER_ID = 1
-#    oauth_info = None
-#    follow_redirects = True
-#    
-#    if not oauth_info:
-#        oauth_info = {'user_id': TEST_USER_ID}
-#
-#    return client.get(
-#            'test/oauth',
-#            query_string=oauth_info,
-#            follow_redirects=follow_redirects
-#    )
-#
-#
-#@pytest.fixture(scope="session")
-#def add_service_user():
-#    sponser = None
-#
-#    if not sponsor:
-#        sponsor = self.test_user
-#    if sponsor not in db.session:
-#        sponsor = db.session.merge(sponsor)
-#    service_user = sponsor.add_service_account()
-#    with SessionScope(db):
-#        db.session.add(service_user)
-#        db.session.commit()
-#    return db.session.merge(service_user)
+# @pytest.fixture(scope="session")
+# def add_client(promote_user):
+#     client_id = 'test_client'
+#     client = Client(
+#         client_id=client_id, _redirect_uris='http://localhost',
+#         client_secret='tc_secret', user_id=TEST_USER_ID)
+#     with SessionScope(db):
+#         db.session.add(client)
+#         db.session.commit()
+#     return db.session.merge(client)
+# 
+# 
+# @pytest.fixture(scope="session")
+# def login():
+#     TEST_USER_ID = 1
+#     oauth_info = None
+#     follow_redirects = True
+# 
+#     if not oauth_info:
+#         oauth_info = {'user_id': TEST_USER_ID}
+# 
+#     return client.get(
+#             'test/oauth',
+#             query_string=oauth_info,
+#             follow_redirects=follow_redirects
+#     )
+# 
+# 
+# @pytest.fixture(scope="session")
+# def add_service_user():
+#     sponser = None
+# 
+#     if not sponsor:
+#         sponsor = self.test_user
+#     if sponsor not in db.session:
+#         sponsor = db.session.merge(sponsor)
+#     service_user = sponsor.add_service_account()
+#     with SessionScope(db):
+#         db.session.add(service_user)
+#         db.session.commit()
+#     return db.session.merge(service_user)
