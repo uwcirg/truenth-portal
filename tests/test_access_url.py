b1cfce8e3e8f7c111d304c0b1770e49dfaffdbd2
Wayne Lai <wayne128@uw.edu>
2019-12-16 14:42:11 -0800

    Replace the unittest framework with pytest fixture Login still does not work Executing the first and second tests at the same time causes pytest to hang

--- tests/test_access_url.py
+++ tests/test_access_url.py
@@ -7,48 +7,46 @@ from portal.models.role import ROLE
 from tests import TestCase
 
 
-class TestAccessUrl(TestCase):
+#def test_create_access_url(client, add_user, promote_user, login):
+#    onetime = add_user('one@time.com')
+#    promote_user(user=onetime, role_name=ROLE.WRITE_ONLY.value)
+#
+#    promote_user(role_name=ROLE.ADMIN.value)
+#    login()
+#    onetime = db.session.merge(onetime)
+#    response = client.get('/api/user/{}/access_url'.format(
+#        onetime.id))
+#    assert response.status_code == 200
+#
+#    # confirm we obtained a valid token
+#    access_url = response.json['access_url']
+#    token = access_url.split('/')[-1]
+#    is_valid, has_expired, id = user_manager.token_manager.verify_token(
+#        token, 10)
+#    assert is_valid
+#    assert not has_expired
+#    assert id == onetime.id
 
-    def test_create_access_url(self):
-        onetime = self.add_user('one@time.com')
-        self.promote_user(user=onetime, role_name=ROLE.WRITE_ONLY.value)
+def test_use_access_url(client, add_user, promote_user, assert_redirects):
+    """The current flow forces access to the challenge page"""
+    onetime = add_user(
+        'one@time.com', first_name='first', last_name='last')
+    onetime.birthdate = '01-31-1969'  # verify requires DOB
+    promote_user(user=onetime, role_name=ROLE.WRITE_ONLY.value)
+    onetime = db.session.merge(onetime)
 
-        self.promote_user(role_name=ROLE.ADMIN.value)
-        self.login()
-        onetime = db.session.merge(onetime)
-        response = self.client.get('/api/user/{}/access_url'.format(
-            onetime.id))
-        assert response.status_code == 200
+    token = user_manager.token_manager.generate_token(onetime.id)
+    access_url = url_for(
+        'portal.access_via_token', token=token, _external=True)
 
-        # confirm we obtained a valid token
-        access_url = response.json['access_url']
-        token = access_url.split('/')[-1]
-        is_valid, has_expired, id = user_manager.token_manager.verify_token(
-            token, 10)
-        assert is_valid
-        assert not has_expired
-        assert id == onetime.id
+    response = client.get(access_url)
+    assert_redirects(
+        response,
+        url_for('portal.challenge_identity', request_path=access_url))
 
-    def test_use_access_url(self):
-        """The current flow forces access to the challenge page"""
-        onetime = self.add_user(
-            'one@time.com', first_name='first', last_name='last')
-        onetime.birthdate = '01-31-1969'  # verify requires DOB
-        self.promote_user(user=onetime, role_name=ROLE.WRITE_ONLY.value)
-        onetime = db.session.merge(onetime)
+def test_bad_token(client):
+    token = 'TBKSYw7iHndUT3DfaED9tw.DHZMrQ.Wwr8SPM7ylABWf0mQHhGHHwttYk'
+    access_url = url_for('portal.access_via_token', token=token)
 
-        token = user_manager.token_manager.generate_token(onetime.id)
-        access_url = url_for(
-            'portal.access_via_token', token=token, _external=True)
-
-        response = self.client.get(access_url)
-        self.assert_redirects(
-            response,
-            url_for('portal.challenge_identity', request_path=access_url))
-
-    def test_bad_token(self):
-        token = 'TBKSYw7iHndUT3DfaED9tw.DHZMrQ.Wwr8SPM7ylABWf0mQHhGHHwttYk'
-        access_url = url_for('portal.access_via_token', token=token)
-
-        response = self.client.get(access_url)
-        assert response.status_code == 404
+    response = client.get(access_url)
+    assert response.status_code == 404
