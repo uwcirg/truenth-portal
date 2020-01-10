f2935fdb496f4ca683b92853cb22b97216647698
Paul Bugni <pbugni@u.washington.edu>
2020-01-10 09:18:05 -0800

    cache slow view /organization results, add support function to build cache key including parameters

--- portal/cache.py
+++ portal/cache.py
@@ -1,9 +1,22 @@
+from flask import request, url_for
 from flask_caching import Cache
-
 
 # Simple init, replaced during app configuration
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 
-
 # Human readable constants, values in seconds, for cache timeouts
 TWO_HOURS = 2*60*60
+FIVE_MINS = 5*60
+
+
+def request_args_in_key():
+    """Set as `key_prefix=request_args_in_key` for safe view caching
+
+    By default, cache.cached() uses only the request path as the key.
+    For any views returning different results with query string parameters,
+    need to include those parameters in the cache key
+
+    :return: key for caching
+
+    """
+    return url_for(request.endpoint, **request.args)
