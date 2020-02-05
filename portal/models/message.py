edaffb625ac88303d2b8ed73eb0849e6446fd2a3
Paul Bugni <pbugni@u.washington.edu>
2020-02-05 10:41:37 -0800

    Force str(Message) in logging to eliminate bogus error log text.

--- portal/models/message.py
+++ portal/models/message.py
@@ -180,7 +180,8 @@ class EmailMessage(db.Model):
         else:
             # This should never happen, alert if it does
             current_app.logger.error(
-                "Unable to generate audit log for email {}".format(Message))
+                "Unable to generate audit log for email {}".format(
+                    str(Message)))
         # If an exception was raised when attempting the send, re-raise now
         # for clients to manage
         if exc:
