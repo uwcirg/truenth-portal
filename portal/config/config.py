d90346e7e780b3ed395a592dba0e31217a1faf43
Amy Chen <achen2401@gmail.com>
2020-02-14 11:01:46 -0800

    pep8 fixes

--- portal/config/config.py
+++ portal/config/config.py
@@ -230,9 +230,9 @@ class BaseConfig(object):
     ]
 
     BANNER_LOGOS = {
-        'desktop': os.environ.get('PERSISTENCE_DIR', 'gil') + \
+        'desktop': os.environ.get('PERSISTENCE_DIR', 'gil') +
             '/static/img/TrueNTH_Banner_Logo.svg',
-        'mobile': os.environ.get('PERSISTENCE_DIR', 'gil') + \
+        'mobile': os.environ.get('PERSISTENCE_DIR', 'gil') +
             '/static/img/TrueNTH_Banner_Logo_Mobile.svg'
     }
 
