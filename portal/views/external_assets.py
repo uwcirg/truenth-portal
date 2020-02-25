1f96d5cb2bb54cd8e103b5f410eb67d82301648f
pbugni <pbugni@u.washington.edu>
2020-02-25 10:24:06 -0800

    refactoring liferay content pulling functions & views to common module. (#3651)
    * refactoring liferay content pulling functions & views to common module.
    
    * refactoring liferay content pulling functions & views to common module.

--- portal/views/external_assets.py
+++ portal/views/external_assets.py
@@ -26,11 +26,19 @@ def get_request(url, params=None):
     return response
 
 
+def lr_query_url():
+    return '/'.join((
+        current_app.config["LR_ORIGIN"], 'c/portal/truenth/asset/query'))
+
+
+def lr_detailed_url():
+    return '/'.join((
+        current_app.config["LR_ORIGIN"], 'c/portal/truenth/asset/detailed'))
+
+
 @asset_api.route('/api/asset/tag/<tag>', methods=('GET',))
-def by_tag(tag):
-    url = "{}/c/portal/truenth/asset/query".format(
-        current_app.config["LR_ORIGIN"])
-    url = localize_url(url, request.args.get('locale_code', 'en'))
+def asset_by_tag(tag):
+    url = localize_url(lr_query_url(), request.args.get('locale_code', 'en'))
     params = {
         'allTags': tag,
         'index': request.args.get('index', 0),
@@ -43,10 +51,9 @@ def by_tag(tag):
 
 
 @asset_api.route('/api/asset/uuid/<uuid>', methods=('GET',))
-def by_uuid(uuid):
-    url = "{}/c/portal/truenth/asset/detailed".format(
-        current_app.config["LR_ORIGIN"])
-    url = localize_url(url, request.args.get('locale_code', 'en'))
+def asset_by_uuid(uuid):
+    url = localize_url(
+        lr_detailed_url(), request.args.get('locale_code', 'en'))
     params = {
         'uuid': uuid,
         'version': 'latest',
@@ -54,3 +61,40 @@ def by_uuid(uuid):
     response = get_request(url, params)
     # This will fail noisily if no matching key
     return response['asset']
+
+
+@asset_api.route('/api/asset/tag/any', methods=('GET',))
+def get_any_tag_data(*anyTags):
+    """ query LR based on any tags
+
+    this is an OR condition; will match any tag specified
+
+    :param anyTag: a variable number of tags to be queried,
+         e.g., 'tag1', 'tag2'
+
+    """
+    # NOTE: need to convert tags to format: anyTags=tag1&anyTags=tag2...
+    liferay_qs_params = {
+        'anyTags': anyTags,
+        'sort': 'true',
+        'sortType': 'DESC'
+    }
+    return get_request(lr_query_url(), params=liferay_qs_params)
+
+
+def get_all_tag_data(*allTags):
+    """ query LR based on all required tags
+
+    this is an AND condition; all required tags must be present
+
+    :param allTags: variable number of tags to be queried,
+        e.g., 'tag1', 'tag2'
+
+    """
+    # NOTE: need to convert tags to format: allTags=tag1&allTags=tag2...
+    liferay_qs_params = {
+        'allTags': allTags,
+        'sort': 'true',
+        'sortType': 'DESC'
+    }
+    return get_request(lr_query_url(), params=liferay_qs_params)
