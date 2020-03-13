955f9ff23a9254cab66942400a7c2ab19d004cf2
Ivan Cvitkovic <ivanc@uw.edu>
2020-03-12 17:17:09 -0700

    Restrict updates to only affected questions

--- portal/migrations/versions/894bbf6a8aa5_.py
+++ portal/migrations/versions/894bbf6a8aa5_.py
@@ -20,29 +20,87 @@ down_revision = 'd4517f61aaed'
 Session = sessionmaker()
 
 
+# questions that allow multiple answers
+checkbox_questions = {
+    # comorbidities
+    'comorb.1',
+
+    # tobacco
+    'irondemog.13',
+    'irondemog_v3.13',
+
+    # living arrangement
+    'irondemog.15',
+    'irondemog_v3.15',
+
+    # supplements
+    'irondemog.25',
+    'irondemog_v3.25',
+
+    # ethnic group
+    'irondemog.26',
+    'irondemog_v3.26',
+
+    # colorectal cancer
+    'irondemog.28',
+    'irondemog_v3.29',
+
+    # prostate cancer
+    'irondemog.32',
+    'irondemog_v3.36',
+
+    # breast cancer
+    'irondemog.39',
+    'irondemog_v3.43',
+
+    # ovarian cancer
+    'irondemog.42',
+    'irondemog_v3.48',
+
+    # pancreatic cancer
+    'irondemog_v3.53',
+
+    # melanoma
+    'irondemog_v3.60',
+}
+
+
 def fixup_qnr(questionnaire_response_json, questionnaire_option_map):
     """Replace valueString text with corresponding valueCoding"""
-    # exit early if preconditions not met
-    if len(questionnaire_response_json['group']['question'][0]['answer']) == 0:
-        return questionnaire_response_json
 
     # JSONB mutation detection work around
     # See https://bugs.launchpad.net/fuel/+bug/1482658
     qnr_json_copy = copy.deepcopy(questionnaire_response_json)
 
-    target_question = qnr_json_copy['group']['question'][0]
-    coded_answers = []
-    for answer in target_question['answer']:
-        if 'valueCoding' in answer:
-            return questionnaire_response_json
-        coded_answer = {'valueCoding': {
-            # lookup code from valueString
-            'code': questionnaire_option_map[answer['valueString']],
-            'system': 'https://eproms.truenth.org/api/codings/assessment',
-        }}
-        coded_answers.append(coded_answer)
+    for question in qnr_json_copy['group']['question']:
+        # skip unaffected questions
+        if question['linkId'] not in checkbox_questions:
+            continue
 
-    target_question['answer'] = coded_answers
+        fixed_answers = []
+        for answer in question['answer']:
+            if 'valueCoding' in answer:
+                seen_codes = {a['valueCoding']['code'] for a in fixed_answers if 'valueCoding' in a}
+
+                # check for duplicate codes
+                if answer['valueCoding']['code'] not in seen_codes:
+                    fixed_answers.append(answer)
+                    continue
+
+            # encode strings as codes
+            if 'valueString' in answer:
+                coded_answer = {'valueCoding': {
+                    # lookup code from valueString
+                    'code': questionnaire_option_map[answer['valueString']],
+                    'system': 'https://eproms.truenth.org/api/codings/assessment',
+                }}
+                fixed_answers.append(coded_answer)
+                continue
+
+            # catch any other answer types
+            fixed_answers.append(answer)
+
+        question['answer'] = fixed_answers
     return qnr_json_copy
 
 
