d561e9edccf11d17a294a6eb31abfc0dfa9d300e
Wayne Lai <waynelai01011231@gmail.com>
2020-03-15 14:25:42 -0700

    Add write_only_user fixture and admin_user fixture

--- tests/test_access_url.py
+++ tests/test_access_url.py
@@ -2,20 +2,30 @@
 
 from flask import url_for
 from flask_webtest import SessionScope
+import pytest
 
 from portal.extensions import db, user_manager
 from portal.models.organization import Organization
 from portal.models.role import ROLE
 
 
-def test_create_access_url(
-        app, client, add_user, promote_user, login, teardown_db):
-    onetime = add_user('one@time.com')
-    promote_user(user=onetime, role_name=ROLE.WRITE_ONLY.value)
+@pytest.fixture
+def write_only_user(add_user, promote_user):
+    write_only = add_user('one@time.com', first_name='first', last_name='last')
+    promote_user(user=write_only, role_name=ROLE.WRITE_ONLY.value)
+    return write_only
 
-    promote_user(role_name=ROLE.ADMIN.value)
+
+@pytest.fixture
+def admin_user(write_only_user, promote_user):
+    promote_user(user=write_only_user, role_name=ROLE.ADMIN.value)
+    return write_only_user
+
+
+def test_create_access_url(
+        app, client, admin_user, login, teardown_db):
     login()
-    onetime = db.session.merge(onetime)
+    onetime = db.session.merge(admin_user)
     response = client.get('/api/user/{}/access_url'.format(
         onetime.id))
     assert response.status_code == 200
@@ -31,13 +41,10 @@ def test_create_access_url(
 
 
 def test_use_access_url(
-        client, add_user, promote_user, assert_redirects, teardown_db):
+        client, write_only_user, assert_redirects, teardown_db):
     """The current flow forces access to the challenge page"""
-    onetime = add_user(
-        'one@time.com', first_name='first', last_name='last')
+    onetime = db.session.merge(write_only_user)
     onetime.birthdate = '01-31-1969'  # verify requires DOB
-    promote_user(user=onetime, role_name=ROLE.WRITE_ONLY.value)
-    onetime = db.session.merge(onetime)
 
     token = user_manager.token_manager.generate_token(onetime.id)
     access_url = url_for(
