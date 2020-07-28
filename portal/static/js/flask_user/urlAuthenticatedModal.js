614f44e2f5a56c01fe2d66e560eb5a3585402c40
Amy Chen <clone@cesium.cirg.washington.edu>
2020-07-28 09:24:04 -0700

    refactor, fix comments

--- portal/static/js/flask_user/urlAuthenticatedModal.js
+++ portal/static/js/flask_user/urlAuthenticatedModal.js
@@ -1,24 +1,33 @@
-var URLAuthenticatedModalObj = function() {
-    /*
-     * initialized modal
-     */
-    this.initURLAuthenticatedModal = function() {
-        $("#urlAuthenticatedModal").modal({
-            "show":false,
-            "backdrop": "static",
-            "focus": true,
-            "keyboard": false
-        });
-    }
-    /*
-     * set UI element with matching CSS class identifier to trigger modal
-     */
-    this.setURLAuthenticatedUI = function(data) {
+/* 
+ * see urlAuthenticatedLoginModal macro in /templates/flask_user/_macros for modal HTML
+ */
+function URLAuthenticatedModalObj() {
+    this.URL_AUTH_METHOD_IDENTIFIER = "url_authenticated";
+    this.PROMOTE_ENCOUNTER_URL = "/promote-encounter";
+    this.MODAL_ELEMENT_IDENTIFIER = "urlAuthenticatedModal";
+};
+
+/*
+ * initialized login prompt modal
+ */
+URLAuthenticatedModalObj.prototype.initURLAuthenticatedModal = function() {
+    $("#"+this.MODAL_ELEMENT_IDENTIFIER).modal({
+        "show":false,
+        "backdrop": "static",
+        "focus": true,
+        "keyboard": false
+    });
+};
+
+/*
+ * set UI element with matching CSS class identifier to trigger the login modal
+ */
+URLAuthenticatedModalObj.prototype.setURLAuthenticatedUI = function() {
+    var self = this;
+    this.getCurrentUser(function(data) {
         if (!data || !data.id) {
             return;
         }
-        var URL_AUTH_METHOD_IDENTIFIER = "url_authenticated";
-        var PROMOTE_ENCOUNTER_URL = "/promote-encounter";
         //call to check if the current user is authenticated via url authenticated method
         $.ajax({
             type: "GET",
@@ -27,42 +36,54 @@ var URLAuthenticatedModalObj = function() {
             if (!data || !data.auth_method) {
                 return;
             }
-            if (String(data.auth_method).toLowerCase() === URL_AUTH_METHOD_IDENTIFIER) {
-                //links needing to redirect to login page 
-                $(".portal-weak-auth-disabled").each(function() {
-                    $(this).on("click", function(e) {
-                        e.preventDefault();
-                        var originalHref = $(this).attr("href");
-                        var redirectHref = PROMOTE_ENCOUNTER_URL+"?next="+originalHref;
-                        $(this).attr("href", redirectHref);
-                        $("#btnUrlAuthenticatedContinue").attr("href", redirectHref);
-                        $("#urlAuthenticatedModal").modal("show");
-                    });
-                });
+            if (String(data.auth_method).toLowerCase() === self.URL_AUTH_METHOD_IDENTIFIER) {
+                //links needing to redirect to login page
+                $("body").delegate(".portal-weak-auth-disabled", "click", function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    var originalHref = $(this).attr("href");
+                    var redirectHref = self.PROMOTE_ENCOUNTER_URL+"?next="+originalHref;
+                    $(this).attr("href", redirectHref);
+                    $("#btnUrlAuthenticatedContinue").attr("href", redirectHref);
+                    $("#"+self.MODAL_ELEMENT_IDENTIFIER).modal("show");
+                })
                 //elements needing to be hidden
-                $(".portal-weak-auth-hide").each(function() {
-                    $(this).hide();
-                });
+                $(".portal-weak-auth-hide").each(
+                    function() {
+                        $(this).hide();
+                    }
+                );
             }
         });
-    };
-    this.getCurrentUser = function (callback) {
-        callback = callback || function() {};
-        $.ajax({
-            type: "GET",
-            url: "/api/me"
-        }).done(function(data) {
-            if (!data || !data.id) {
-                callback({error: true});
-                return;
-            }
-            callback(data);
-        }).fail(function() {
+    });
+};
+
+/*
+ * get current user information
+ * @param callback function to execute when data is returned from API
+ */
+URLAuthenticatedModalObj.prototype.getCurrentUser = function(callback) {
+    callback = callback || function() {};
+    callback.bind(this);
+    $.ajax({
+        type: "GET",
+        url: "/api/me"
+    }).done(function(data) {
+        if (!data || !data.id) {
             callback({error: true});
-        });
-    };
-    this.init = function() {
-        this.initURLAuthenticatedModal();
-        this.getCurrentUser(this.setURLAuthenticatedUI);
-    };
+            return;
+        }
+        callback(data);
+    }).fail(function() {
+        callback({error: true});
+    });
+};
+
+/*
+ * initializing object
+ * this will initialize the login modal and set UI states/events appropiately
+ */
+URLAuthenticatedModalObj.prototype.init = function() {
+    this.initURLAuthenticatedModal();
+    this.setURLAuthenticatedUI();
 };
