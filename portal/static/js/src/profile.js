d1f17b21c815412f8b2114ceb1d997a552db44e6
Amy Chen <clone@cesium.cirg.washington.edu>
2020-08-27 14:38:47 -0700

    remove commented out code, re-order code

--- portal/static/js/src/profile.js
+++ portal/static/js/src/profile.js
@@ -2041,18 +2041,6 @@ export default (function() {
                      * alphabetize role list by the name property of each item in the array
                      * for ease of viewing and selection
                      */
-                    // let sortedData = (data.roles).sort((a, b) => {
-                    //     var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-                    //     var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-                    //     if (nameA < nameB) {
-                    //       return -1;
-                    //     }
-                    //     if (nameA > nameB) {
-                    //       return 1;
-                    //     }
-                    //     // names must be equal
-                    //     return 0;
-                    // });
                     self.roles.data = sortArrayByField(data.roles, "name");
                 });
                 self.initUserRoles();
