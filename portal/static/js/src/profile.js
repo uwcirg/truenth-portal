75db97f8b716d7714803fd989f83fc36dfd0c024
Amy Chen <achen2401@gmail.com>
2020-09-03 14:55:07 -0700

    Feature/substudy consent ui update (#3747)
    * TN-2634 get substudy consent entry in UI
    
    * move substudy variable references to separate export file
    
    * add end of line
    
    * fix reference to research study id
    
    * Update consts.js
    
    Fix placeholder name
    
    Co-authored-by: Amy Chen <clone@cesium.cirg.washington.edu>

--- portal/static/js/src/profile.js
+++ portal/static/js/src/profile.js
@@ -6,6 +6,7 @@ import ProcApp from "./modules/Procedures.js";
 import Utility from "./modules/Utility.js";
 import ClinicalQuestions from "./modules/ClinicalQuestions.js";
 import Consent from "./modules/Consent.js";
+import {EPROMS_SUBSTUDY_ID, EPROMS_SUBSTUDY_TITLE} from "./data/common/consts.js";
 
 /*
  * helper Object for initializing profile sections  TODO streamline this more
@@ -2168,8 +2169,17 @@ export default (function() {
                 content += "</tr>";
                 return content;
             },
+            isSubStudyConsent: function(item) {
+                if (!item) {
+                    return false;
+                }
+                return parseInt(item.research_study_id) === EPROMS_SUBSTUDY_ID;
+            },
             getConsentOrgDisplayName: function(item) {
                 if (!item) {return "";}
+                if (this.isSubStudyConsent(item)) {
+                    return EPROMS_SUBSTUDY_TITLE;
+                }
                 var orgId = item.organization_id, OT = this.getOrgTool(), currentOrg = OT.orgsList[orgId], orgName = currentOrg ? currentOrg.name : item.organization_id;
                 if (!this.isConsentWithTopLevelOrg()) {
                     var topOrgID = OT.getTopLevelParentOrg(orgId), topOrg = OT.orgsList[topOrgID];
