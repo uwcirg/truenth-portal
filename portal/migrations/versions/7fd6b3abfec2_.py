ff23ac5276ee3d80b1bb8d7f04e72289bac3ae3d
Paul Bugni <pbugni@u.washington.edu>
2020-10-26 16:56:06 -0700

    pep8

--- portal/migrations/versions/7fd6b3abfec2_.py
+++ portal/migrations/versions/7fd6b3abfec2_.py
@@ -19,25 +19,50 @@ def upgrade():
     if current_app.config.get('GIL'):
         return
     op.create_table('trigger_states',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('user_id', sa.Integer(), nullable=False),
-    sa.Column('state', postgresql.ENUM('unstarted', 'due', 'processed', name='trigger_state_type'), nullable=False),
-    sa.Column('timestamp', sa.DateTime(), nullable=False),
-    sa.Column('triggers', postgresql.JSONB(astext_type=sa.Text()), nullable=True),
-    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
-    sa.PrimaryKeyConstraint('id')
-    )
-    op.create_index(op.f('ix_trigger_states_state'), 'trigger_states', ['state'], unique=False)
-    op.create_index(op.f('ix_trigger_states_timestamp'), 'trigger_states', ['timestamp'], unique=False)
-    op.create_index(op.f('ix_trigger_states_user_id'), 'trigger_states', ['user_id'], unique=False)
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('user_id', sa.Integer(), nullable=False),
+        sa.Column(
+            'state',
+            postgresql.ENUM(
+                'unstarted',
+                'due',
+                'processed',
+                name='trigger_state_type'),
+            nullable=False),
+        sa.Column('timestamp', sa.DateTime(), nullable=False),
+        sa.Column(
+            'triggers',
+            postgresql.JSONB(astext_type=sa.Text()),
+            nullable=True),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
+        sa.PrimaryKeyConstraint('id')
+        )
+    op.create_index(
+        op.f('ix_trigger_states_state'),
+        'trigger_states',
+        ['state'],
+        unique=False)
+    op.create_index(
+        op.f('ix_trigger_states_timestamp'),
+        'trigger_states',
+        ['timestamp'],
+        unique=False)
+    op.create_index(
+        op.f('ix_trigger_states_user_id'),
+        'trigger_states',
+        ['user_id'],
+        unique=False)
     # ### end Alembic commands ###
 
 
 def downgrade():
     if current_app.config.get('GIL'):
         return
-    op.drop_index(op.f('ix_trigger_states_user_id'), table_name='trigger_states')
-    op.drop_index(op.f('ix_trigger_states_timestamp'), table_name='trigger_states')
-    op.drop_index(op.f('ix_trigger_states_state'), table_name='trigger_states')
+    op.drop_index(
+        op.f('ix_trigger_states_user_id'), table_name='trigger_states')
+    op.drop_index(
+        op.f('ix_trigger_states_timestamp'), table_name='trigger_states')
+    op.drop_index(
+        op.f('ix_trigger_states_state'), table_name='trigger_states')
     op.drop_table('trigger_states')
     # ### end Alembic commands ###
