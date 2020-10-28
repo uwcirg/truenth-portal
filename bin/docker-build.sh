8dadb9d62618c8b02a2f503f3cae98bfc8c2939e
Ivan Cvitkovic <icvitkovic@gmail.com>
2020-10-27 21:30:39 -0700

    Substudy Docker Build (#3718)
    * Substudy build WIP
    
    * Fixup trailing slash
    
    * Add frontend file wip
    
    * Add dockerignore, removing script functionality
    
    * Copy each substudy directory- docker COPY does not copy directories (only directory contents)

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -21,31 +21,10 @@ USAGE
    exit 1
 }
 
-cleanup_generated_dockerignore() {
-    local file_copied="$1"
-    if [ -n "$file_copied" ]; then
-        rm "${repo_path}/.dockerignore"
-        echo "Deleted generated .dockerignore"
-    fi
-}
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     usage
 fi
-
-
-# Use .gitignore as .dockerignore during build only
-# not worth the effort to maintain both, for now
-copy_output="$(
-    cp \
-        --no-clobber \
-        --verbose \
-        "${repo_path}/.gitignore" \
-        "${repo_path}/.dockerignore"
-)"
-
-# "->" will appear in `cp` output if file is sucessfully copied
-file_copied="$(echo "$copy_output" | grep "\->" || true)"
 
 # docker-compose commands must be run in the same directory as docker-compose.yaml
 docker_compose_directory="${repo_path}/docker"
@@ -57,11 +36,5 @@ default_portal_env="${PORTAL_ENV_FILE:-portal.env}"
 # Create required env_file if it doesn't exist
 cp --no-clobber portal.env.default "$default_portal_env"
 
-
-# use trap to cleanup generated .dockerignore on early exit
-trap 'cleanup_generated_dockerignore "$file_copied"; exit' INT TERM EXIT
 echo "Building portal docker image..."
 docker-compose build web
-trap - INT TERM EXIT
-
-cleanup_generated_dockerignore "$file_copied"
