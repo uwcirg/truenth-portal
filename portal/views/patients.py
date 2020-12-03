e98c095c8eda80a27f2e869f473401acd8d03385
Paul Bugni <pbugni@u.washington.edu>
2020-12-03 14:32:33 -0800

    index error fix.

--- portal/views/patients.py
+++ portal/views/patients.py
@@ -205,7 +205,7 @@ def patient_profile(patient_id):
         study for study in patient_research_study_status(patient) if
         study['research_study_id'] == EMPRO_RS_ID]
     substudy_assessment_is_ready = (
-        substudy_status and substudy_status['ready'])
+        substudy_status and substudy_status[0]['ready'])
 
     return render_template(
         'profile/patient_profile.html', user=patient,
