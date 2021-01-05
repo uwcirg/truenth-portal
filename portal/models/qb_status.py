41c6e3d6403c68f4c6bc68fc0307db34a28db66c
pbugni <pbugni@u.washington.edu>
2021-01-05 15:09:09 -0800

    Withdrawn sanity check should log error, not halt process (#3916)
    * the "withdrawn sanity check" didn't include an edge case, and has outlived it's utility.
    
    * retain withdrawn check, logging as error not halting process.
    
    * Update portal/models/qb_status.py
    
    use logging friendly string formatting
    
    Co-authored-by: Ivan Cvitkovic <ivanc@uw.edu>
    
    * pep8
    
    Co-authored-by: Ivan Cvitkovic <ivanc@uw.edu>

--- portal/models/qb_status.py
+++ portal/models/qb_status.py
@@ -141,9 +141,10 @@ class QB_Status(object):
         # Withdrawn sanity check
         if self.withdrawn_by(self.as_of_date) and (
                 self.overall_status != OverallStatus.withdrawn):
-            raise RuntimeError(
-                "Unexpected state {}, should be withdrawn".format(
-                    self.overall_status))
+            from flask import current_app
+            current_app.logger.error(
+                "Unexpected state %s, user %d should be withdrawn",
+                self.overall_status, self.user.id)
 
     def older_qbds(self, last_known):
         """Generator to return QBDs and status prior to last known
