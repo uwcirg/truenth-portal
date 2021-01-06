a71aa49833030795cfe6b187c5497cb5d67ae263
Paul Bugni <pbugni@u.washington.edu>
2021-01-06 12:53:46 -0800

    Add merge heads needed migration

---
+++ portal/migrations/versions/c3d394d9dfd1_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: c3d394d9dfd1
+Revises: ('3c81fc76164c', 'c19bff0f70ab')
+Create Date: 2021-01-06 12:53:14.809274
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c3d394d9dfd1'
+down_revision = ('3c81fc76164c', 'c19bff0f70ab')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
