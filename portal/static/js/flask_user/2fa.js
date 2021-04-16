97c47b2127391cd360f26399d25c3f487ee2696f
Amy Chen <clone@cesium.cirg.washington.edu>
2021-04-15 17:14:44 -0700

    frontend code to respond to return key

--- portal/static/js/flask_user/2fa.js
+++ portal/static/js/flask_user/2fa.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-    $("#submitValidationCode").on("click", function(e) {
-        e.stopPropagation();
+    var submitForm = function() {
         if (!$("#faInput").val()) return;
         $(".buttons-container").addClass("loading");
         setTimeout(function() {
             $("#TwoFAForm").submit();
         }, 50);
-
+    };
+    $("#submitValidationCode").on("click", function(e) {
+        e.stopPropagation();
+        submitForm();
+    });
+    $(document).keypress(function(event) {
+        if (event.which === 13) {
+           submitForm();
+           return false;
+        }
+        return true;
     });
 });
