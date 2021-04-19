70cfdb20c84cb70e82cca177c4ede43302a848f7
pbugni <pbugni@u.washington.edu>
2021-04-19 13:37:01 -0700

    TN-2989 correctly manage 'next' in 2FA (#4055)
    * as the only available hook into the flask-user flow, insert the configured `USER_AFTER_LOGIN_ENDPOINT` as the `safe_url` to pass user on to as the `next` parameter in an unauthenticated, needing login, request.
    
    * consider `next` as a request param as well as a session variable.
    
    * replace term `Token` with `Code` as per review suggestion.
    
    * pep8
    
    * Apply suggestions from code review
    
    Co-authored-by: Ivan Cvitkovic <ivanc@uw.edu>
    
    * TN-2988 frontend loading indication addition, ui layout updates
    
    * use 6 digits for 2FA code
    prepend with zeros for codes that begin with a zero
    minor wording changes
    
    * frontend code to respond to return key
    
    * fix template verbiage based on feedback
    
    * fix based on feedback
    
    Co-authored-by: Ivan Cvitkovic <ivanc@uw.edu>
    Co-authored-by: Amy Chen <clone@cesium.cirg.washington.edu>

---
+++ portal/static/js/flask_user/2fa.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var submitForm = function() {
+        if (!$("#faInput").val()) return;
+        $(".buttons-container").addClass("loading");
+        setTimeout(function() {
+            $("#TwoFAForm").submit();
+        }, 50);
+    };
+    $("#submitValidationCode").on("click", function(e) {
+        e.stopPropagation();
+        submitForm();
+    });
+    $(document).keypress(function(event) {
+        if (event.which === 13) {
+           submitForm();
+           return false;
+        }
+        return true;
+    });
+});
