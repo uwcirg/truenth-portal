334566786f43d026578b77d26cda672d9c345a80
pbugni <pbugni@u.washington.edu>
2021-08-24 17:38:01 -0700

    Update portal/config/config.py
    Co-authored-by: Ivan Cvitkovic <ivanc@uw.edu>

--- portal/config/config.py
+++ portal/config/config.py
@@ -82,8 +82,8 @@ class BaseConfig(object):
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false').lower == "true"
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'false').lower == "true"
     MAIL_SUPPRESS_SEND = os.environ.get(
         'MAIL_SUPPRESS_SEND',
         str(TESTING)).lower() == 'true'
