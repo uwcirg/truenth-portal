f62a81f024f14b334ddda9a3b78da4c1c16e7ee1
Amy Chen <achen2401@gmail.com>
2021-09-29 09:27:00 -0700

    TN-3073 EMPRO training material page layout updates (#4140)
    * TN-3073 EMPRO training material page layout updates
    
    * fix based on semgrep conment
    
    Co-authored-by: Amy Chen <clone@cesium.cirg.washington.edu>

--- portal/eproms/static/js/emproResources.js
+++ portal/eproms/static/js/emproResources.js
@@ -1,25 +1,95 @@
 $(function() {
-    var handleCollapsible = function() {
-        var collapsibleElements = document.querySelectorAll(".collapsible");
-        for (var index = 0; index < collapsibleElements.length; index ++) {
-            var el = collapsibleElements[index];
-            el.addEventListener('click', function(event) {
-                var targetElement = $(event.target).closest(".collapsible");
-                var parentEl = targetElement[0].parentElement.parentElement;
-                var collapsibleItems = parentEl.querySelectorAll(".collapsible");
-                for (var subindex = 0; subindex < collapsibleItems.length; subindex++) {
-                    var item = collapsibleItems[subindex];
-                    if (item === targetElement[0]) continue;
-                    item.classList.remove("open");
-                }
-                if (targetElement[0].classList.contains("open")) {
-                    targetElement[0].classList.remove("open");
-                    return;
-                }
-                targetElement[0].classList.add("open");
-                targetElement[0].scrollIntoView();
-            });
-        }
+
+    /*
+     * remove loading indicator when video loaded
+     */
+    var checkIframeLoaded = function(iframe) {
+        if (!iframe) return;
+        setTimeout(function() {
+            $(iframe).parent(".video").addClass("loaded");
+        }, 750);
     }
-    handleCollapsible();
+
+    /*
+     * reset all videos to inactive
+     */
+    var resetAllVideo = function(el) {
+        if (!el || el.length) return;
+        $(".video-frame").each(function() {
+            if ($(this) === el) return true;
+            var srcURL = $(this).attr("src");
+             //reset all iframe autoplay setting
+            if (srcURL && srcURL.indexOf("?") !== -1) {
+                srcURL = srcURL.substring(0, srcURL.indexOf("?"));
+                $(this).attr("src", srcURL);
+            }
+            $(this).parent(".video").addClass("video-hide");
+            $(this).parent(".video").removeClass("loaded");
+            $(this).closest(".content").removeClass("active");
+        });
+    };
+    /*
+     * play the currently selected video
+     */
+    var setActiveVideo = function(el) {
+        if (!el || !el.length) return;
+        var contentElement =  el.closest(".content");
+        var videoElement = contentElement.find(".video-frame");
+        if (!videoElement.attr("src")) videoElement.attr("src", videoElement.attr("data-src"));
+        var srcURL = videoElement.attr("data-src");
+        //set autoplay for active video
+        videoElement.attr("src", srcURL+"?autoplay=true");
+        videoElement.parent(".video").removeClass("video-hide");
+        contentElement.addClass("active");
+        checkIframeLoaded(videoElement);
+        $("html, body").animate({
+            scrollTop: videoElement.parent(".video").offset().top - 48
+        }, 750);
+    };
+    /*
+     * stop currently playing video
+     */
+    var setInactiveVideo = function(el) {
+        if (!el || !el.length) return;
+        var contentElement =  el.closest(".content");
+        var videoElement = contentElement.find(".video-frame");
+        if (!videoElement.attr("src")) videoElement.attr("src", videoElement.attr("data-src"));
+        var srcURL = videoElement.attr("src");
+        //reset all iframe autoplay setting
+        if (srcURL) {
+            var videoURL = srcURL.indexOf("?") !== -1 ? srcURL.substring(0, srcURL.indexOf("?")) : srcURL;
+            videoElement.attr("src", "");
+            videoElement.attr("data-src", videoURL);
+        }
+        videoElement.parent(".video").addClass("video-hide");
+        contentElement.removeClass("active");
+    };
+    /*
+     * play video button click event
+     */
+    var handleVideoLinkClick = function(el) {
+        if(el.closest(".content").hasClass("active")) {
+            el.closest(".content").removeClass("active");
+            setInactiveVideo(el);
+            return;
+        }
+        resetAllVideo(el);
+        setActiveVideo(el);
+    };
+    /*
+     * prevent all videos from loaded at runtime
+     * load video only on demand
+     */
+    $(".video-frame").each(function() {
+        var attrSRC = $(this).attr("src");
+        $(this).attr("data-src", attrSRC);
+        $(this).attr("src", "");
+    });
+    $(".video-frame").on("load", function() {
+        checkIframeLoaded(this);
+    });
+    $(".description").on("click", function(e) {
+        e.stopPropagation();
+        handleVideoLinkClick($(this));
+    });
 });
