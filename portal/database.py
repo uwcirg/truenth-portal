dc1751dc0edf1662551c2d378e792440d7b863c0
Paul Bugni <pbugni@u.washington.edu>
2024-03-04 14:18:48 -0800

    Revert " Test theory, that the coupling of redis to sql is what causes unittest to become deadlocked when trying to drop all tables."
    This reverts commit fe0fca4f6d20ecf0a61ce37bbd2b4d6bb4f0509d.

--- portal/database.py
+++ portal/database.py
@@ -4,7 +4,4 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy_caching import CachingQuery
 
-# Test theory, that the coupling of redis to sql is what causes
-# unittests to become deadlocked when trying to drop all tables
-#db = SQLAlchemy(query_class=CachingQuery)
-db = SQLAlchemy()
+db = SQLAlchemy(query_class=CachingQuery)
