efbf1edb59e53f902def2c4ee05ebe167a48f65c
Paul Bugni <pbugni@u.washington.edu>
2024-06-18 20:01:51 -0700

    TN-3300 add `Delayed By Holiday` column to adherence report.

--- tests/fixtures/questionnaire_response.py
+++ tests/fixtures/questionnaire_response.py
@@ -41,3 +41,77 @@ def initialized_with_ss_qnr(test_user, initialized_with_ss_qb):
         db.session.add(qnr)
         db.session.commit()
     return db.session.merge(qnr)
+
+
+@fixture
+def clinician_response_holiday_delay():
+    data = {
+        "group": {
+            "question": [
+                {
+                    "text": "Action(s) Taken",
+                    "answer": [
+                        {"valueString": "Called and spoke with patient"},
+                        {"valueCoding": {
+                            "code": "ironman_ss_post_tx.1.1",
+                            "system": "https://eproms.truenth.org/api/codings/assessment"}
+                        },
+                        {"valueCoding": {
+                            "code": "ironman_ss_post_tx.1.5",
+                            "system": "https://eproms.truenth.org/api/codings/assessment"},
+                            "valueString": "Other (please specify)"
+                        },
+                        {"valueString": "just a test..."}
+                    ],
+                    "linkId": "ironman_ss_post_tx.1"
+                },
+                {
+                    "text": "Date action taken",
+                    "answer": [
+                        {"valueString": "2022-06-17"}
+                    ],
+                    "linkId": "ironman_ss_post_tx.2"
+                },
+                {
+                    "text": "Delayed due to local public holiday",
+                    "answer": [{"valueBoolean": True}],
+                    "linkId": "ironman_ss_post_tx.2.1"
+                },
+                {
+                    "text": "Action taken by",
+                    "answer": [
+                        {"valueString": "Other"},
+                        {"valueCoding": {
+                            "code": "ironman_ss_post_tx.3.4",
+                            "system": "https://eproms.truenth.org/api/codings/assessment"}
+                        }
+                    ],
+                    "linkId": "ironman_ss_post_tx.3"
+                },
+                {
+                    "text": "I confirm I have received this notification and taken action.",
+                    "answer": [{"valueBoolean": True}],
+                    "linkId": "ironman_ss_post_tx.4"
+                }
+            ]
+        },
+        "author": {
+            "display": "user info",
+            "reference": "https://eproms.truenth.org/api/me/4"
+        },
+        "source": {
+            "display": "user demographics",
+            "reference": "https://eproms.truenth.org/api/demographics/4"},
+        "status": "completed",
+        "subject": {
+            "display": "patient demographics",
+            "reference": "https://eproms.truenth.org/api/demographics/5051"
+        },
+        "authored": "2022-06-22T01:00:54Z",
+        "resourceType": "QuestionnaireResponse",
+        "questionnaire": {
+            "display": "EMPRO Post Intervention Questionnaire",
+            "reference": "https://eproms.truenth.org/api/questionnaires/ironman_ss_post_tx"}
+    }
+
+    return QuestionnaireResponse(document=data)
