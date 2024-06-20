cb69e18a89b21c5f0e1c788e71f2926b942ca49e
Paul Bugni <pbugni@u.washington.edu>
2024-06-20 15:10:05 -0700

    TN-3301 `triggered_domains` must include all hard triggers in the case of full opt-out.

--- portal/trigger_states/empro_messages.py
+++ portal/trigger_states/empro_messages.py
@@ -99,16 +99,13 @@ def staff_emails(patient, hard_triggers, opted_out_domains, initial_notification
         if c.id not in staff_list_ids:
             staff_list.append(c)
 
-    # opt-in holds hard triggers the user did NOT opt-out of
-    opt_in_domains = hard_triggers
-
     app_text_name = 'empro clinician trigger reminder'
     if initial_notification:
         app_text_name = 'empro clinician trigger notification'
     if not (set(hard_triggers) - set(opted_out_domains)):
         # All triggered were opted out of - pick up different email template
         app_text_name += " all opted out"
-        opt_in_domains = []
+        triggered_for_email = hard_triggers
         if not initial_notification:
             # seen on test, no idea how - include details in exception
             msg = (f"Patient {patient.id} all opted out: {opted_out_domains} "
@@ -117,7 +114,8 @@ def staff_emails(patient, hard_triggers, opted_out_domains, initial_notification
             app_text_name = 'empro clinician trigger notification all opted out'
     elif opted_out_domains:
         app_text_name += " partially opted out"
-        opt_in_domains = list(set(hard_triggers) - set(opted_out_domains))
+        # only include hard triggers the user did NOT opt-out of
+        triggered_for_email = list(set(hard_triggers) - set(opted_out_domains))
 
     # According to spec, args need at least:
     # - study ID
@@ -145,7 +143,7 @@ def staff_emails(patient, hard_triggers, opted_out_domains, initial_notification
             label=_('View Participant Details')))
     opted_out = ", ".join(opted_out_domains) if opted_out_domains else ""
     opted_out_display = "<b>{opted_out}</b>".format(opted_out=opted_out)
-    triggered_domains = ", ".join(opt_in_domains) if opt_in_domains else ""
+    triggered_domains = ", ".join(triggered_for_email) if triggered_for_email else ""
     triggered_domains_display = "<b>{triggered_domains}</b>".format(
         triggered_domains=triggered_domains)
     args = {
