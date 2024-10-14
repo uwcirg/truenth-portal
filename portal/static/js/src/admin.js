5c12decbeb255f1316960dd6442ce71a1b0f6aaf
Amy Chen <clone@cesium.cirg.washington.edu>
2024-10-14 09:31:22 -0700

    execute callback

--- portal/static/js/src/admin.js
+++ portal/static/js/src/admin.js
@@ -1324,6 +1324,7 @@ let requestTimerId = 0;
         var tnthAjax = this.getDependency("tnthAjax");
         tableName = tableName || this.tableIdentifier;
         if (!tableName || !document.querySelector("#adminTable")) {
+          if (callback) callback();
           return false;
         }
         userId = userId || this.userId;
