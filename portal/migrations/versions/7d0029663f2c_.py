d97455ee5cd01c38fd2ff6bdf0487361c9e7d139
Paul Bugni <pbugni@u.washington.edu>
2025-06-24 10:58:57 -0700

    comparison of adherence to trigger states: completed as migration - committing before moving to a CLI call.

--- portal/migrations/versions/7d0029663f2c_.py
+++ portal/migrations/versions/7d0029663f2c_.py
@@ -14,56 +14,151 @@ from portal.database import db
 from portal.models.adherence_data import AdherenceData
 from portal.models.reporting import single_patient_adherence_data
 from portal.models.research_study import EMPRO_RS_ID
+from portal.models.role import ROLE
+from portal.models.user import unchecked_get_user
+from portal.models.user_consent import consent_withdrawal_dates
 from portal.timeout_lock import ADHERENCE_DATA_KEY, CacheModeration
 
 # revision identifiers, used by Alembic.
 revision = '7d0029663f2c'
 down_revision = '4f5daa2b48db'
 
+state_map = {
+    "inprocess": "inprocess",
+    "triggered": "triggered",
+    "resolved": "Completed",
+    "due": "Expired",
+}
+
+
+class CombinedData:
+    """Special container for comparing a patients adherence vs trigger states data"""
+
+    def __init__(self, patient_id):
+        self.patient_id = patient_id
+        self.adherence_data = None
+        self.ts_data = None
+        self.report_called = False
+        self.withdrawal_month = None
+
+    def adherence_months_by_patient(self, conn):
+        this_patient_months = {}
+        query = text(
+            "select rs_id_visit, data->>'status' as status from adherence_data where patient_id = "
+            ":patient_id and rs_id_visit like :rs1_pattern")
+        for row in conn.execute(query, {"patient_id": self.patient_id, "rs1_pattern": "1:Month%"}):
+            match = re.findall(r'\d+', row.rs_id_visit)
+            if not match:
+                raise ValueError(f"Patient {self.patient_id} has bogus rs_id_visit value: {r2.rs_id_visit}")
+            this_patient_months[int(match[-1])] = row.status
+            if row.status == 'Withdrawn':
+                self.withdrawal_month = int(match[-1])
+
+        self.adherence_data = this_patient_months
+        if self.withdrawal_month:
+            return
+
+        # if withdrawal_month wasn't found, calculate now if user has withdrawn
+        patient = unchecked_get_user(self.patient_id, allow_deleted=True)
+        _, withdrawal = consent_withdrawal_dates(patient, EMPRO_RS_ID)
+        if not withdrawal:
+            return
+        query = text(
+            "select at from qb_timeline where qb_id = 28 and status = 'due' "
+            "and research_study_id = 1 and user_id = :user_id")
+        result = conn.execute(query, {"user_id": self.patient_id}).first()[0]
+        withdrawal_month = -1
+        while True:
+            if withdrawal < result:
+                self.withdrawal_month = withdrawal_month
+                return
+            result += timedelta(days=30)
+            withdrawal_month += 1
+            assert withdrawal_month > 12
+
+    def trigger_states_months_by_patient(self, conn):
+        this_patient_ts_months = {}
+        query = text(
+            "select visit_month, state from trigger_states where user_id = :patient_id order by id"
+        )
+        for row in conn.execute(query, {"patient_id":self.patient_id}):
+            # allow to overwrite improved state given order
+            visit_month = row.visit_month + 1  # 0 index, align with adherence
+            this_patient_ts_months[visit_month] = state_map[row.state]
+        self.ts_data = this_patient_ts_months
+
+    def report(self, message):
+        if not self.report_called:
+            self.report_called = True
+            print(f"Differences for {self.patient_id} adherence | trigger states")
+        print(message)
+
+    def show_differences(self):
+        if self.patient_id == 9090:
+            pass
+            #import pdb; pdb.set_trace()
+        if self.adherence_data is None and self.ts_data is None:
+            return None
+        for i in range(1,13):
+            ad = self.adherence_data.get(i)
+            ts = self.ts_data.get(i)
+            if ad is None and ts is None:
+                continue
+            if ad and not ts:
+                if ad in ('Due', 'Expired', 'Overdue'):
+                    # trigger states often never started w/o user intervention
+                    continue
+                if ad == 'Withdrawn':
+                    # trigger states often not generated after withdrawal
+                    continue
+                if self.patient_id == 5903:
+                    # one of a kind, missing trigger_states rows for visit 11
+                    # back story: https://cirg.slack.com/archives/C5MH9NVKQ/p1725553251960589?thread_ts=1725553232.355499&cid=C5MH9NVKQ
+                    assert i == 11  # the known missing month
+                    continue
+            if ts and not ad:
+                # beyond withdrawal, ignore ts only rows
+                if self.withdrawal_month and i > self.withdrawal_month:
+                    continue
+                self.report(f"\tmissing adherence for month {i}")
+            if ad != ts:
+                note = ""
+                # at or beyond withdrawal, ts of expired is equivalent
+                if self.withdrawal_month and i >= self.withdrawal_month and ts == 'Expired':
+                    continue
+                if ad == 'Completed' and ts == 'triggered':
+                    note = "(Matching state pending clinician follow-up)"
+                if (
+                        ad in ('Due', 'Not Yet Available') and
+                        ts == 'Expired' and
+                        i == max(self.ts_data.keys()) and
+                        not self.withdrawal_month):
+                    # legit mis-mapping from state_map where both are "due"
+                    # the not-yet-avail state implies base study must first be completed
+                    continue
+                self.report(f"\tmonth {i}:\t {ad} != {ts} {note}")
+
 
 def upgrade():
     conn = op.get_bind()
-    patient_adherence_months = {}
-    patient_ts_months = {}
-    now = datetime.utcnow()
     reprocess_ids = []
     def patient_loop():
         query = "select distinct(user_id) from trigger_states order by user_id"
         for row in conn.execute(query):
             pat_id = row.user_id
-            patient_adherence_months[pat_id] = adherence_months_by_patient(pat_id)
-            patient_ts_months[pat_id] = trigger_states_months_by_patient(pat_id)
+            patient = unchecked_get_user(pat_id, allow_deleted=True)
+            if patient.has_role(ROLE.TEST.value):
+                continue
+            pat_diff = CombinedData(pat_id)
+            pat_diff.adherence_months_by_patient(conn)
+            pat_diff.trigger_states_months_by_patient(conn)
 
-            if patient_adherence_months[pat_id] != patient_ts_months[pat_id]:
-                print("Differences for ", pat_id, "adherence months < -- > trigger states months")
-                print("  ", patient_adherence_months[pat_id], "!=", patient_ts_months[pat_id])
+            if pat_diff.show_differences():
                 reprocess_ids.append(pat_id)
 
-
-    def adherence_months_by_patient(patient_id):
-        this_patient_months = set()
-        query = text(
-            "select rs_id_visit from adherence_data where patient_id = "
-            ":patient_id and rs_id_visit like :rs1_pattern")
-        for row in conn.execute(query, {"patient_id": patient_id, "rs1_pattern": "1:Month%"}):
-            match = re.findall(r'\d+', row.rs_id_visit)
-            if not match:
-                raise ValueError(f"Patient {patient_id} has bogus rs_id_visit value: {r2.rs_id_visit}")
-            this_patient_months.add(int(match[-1]))
-        return this_patient_months
-
-    def trigger_states_months_by_patient(patient_id):
-        this_patient_ts_months = set()
-        query = text(
-            "select distinct(visit_month) from trigger_states where user_id = :patient_id"
-        )
-        # trigger states visit month 0 index - add 1
-        for row in conn.execute(query, {"patient_id":patient_id}):
-            this_patient_ts_months.add(row.visit_month + 1)
-        return this_patient_ts_months
-
     patient_loop()
-    for pat_id in reprocess_ids:
+    now = datetime.utcnow()
+    for pat_id in reprocess_ids[0:0]:
         # force a rebuild of adherence data, see if results improve
         cache_moderation = CacheModeration(key=ADHERENCE_DATA_KEY.format(
             patient_id=pat_id,
@@ -73,13 +168,14 @@ def upgrade():
         AdherenceData.query.filter(AdherenceData.valid_till < valid).delete()
         db.session.commit()
         single_patient_adherence_data(patient_id=pat_id, research_study_id=EMPRO_RS_ID)
-        second_try = adherence_months_by_patient(pat_id)
+        """second_try = adherence_months_by_patient(pat_id)
         if second_try != patient_adherence_months[pat_id]:
             print(pat_id, " got better!")
             print(patient_adherence_months[pat_id], " -> ", second_try, " : ", patient_ts_months[pat_id])
     #ad_only, ts_only, mods, _ = dict_compare(adherence_months, ts_months)
     #print(ad_only)
-    raise NotImplemented("Not implemented yet")
+    """
+    raise NotImplemented("Migration to become CLI call - don't allow to run")
 
 def downgrade():
     # ### commands auto generated by Alembic - please adjust! ###
