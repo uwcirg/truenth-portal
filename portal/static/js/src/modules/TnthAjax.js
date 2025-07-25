042e9bfab108eb519f0779da1193711b6e6c684a
Amy Chen <clone@cesium.cirg.washington.edu>
2025-07-25 10:30:16 -0700

    add check for unauth/csrf token related error in request calls

--- portal/static/js/src/modules/TnthAjax.js
+++ portal/static/js/src/modules/TnthAjax.js
@@ -74,6 +74,20 @@ export default { /*global $ */
                                 callback({"error": DEFAULT_SERVER_DATA_ERROR, "data": false});
                             }
                         }).fail(function(xhr) {
+                            // catch auth error, e.g. unaunthorized or stale CSRF session token error before re-trying
+                            const doomedStatus = parseInt(xhr.status) === 400 || parseInt(xhr.status) === 401;
+                            const xhrResponseText = xhr.responseText ? String(xhr.responseText).toLowerCase() : "";
+                            if (
+                              doomedStatus &&
+                              (xhrResponseText.includes("csrf token") ||
+                                xhrResponseText.includes("unauthorize"))
+                            ) {
+                              if (callback) {
+                                callback({ error: DEFAULT_SERVER_DATA_ERROR });
+                                fieldHelper.showError(targetField);
+                              }
+                              return false;
+                            }
                             if (params.attempts < params.max_attempts) {
                                 (function(self, url, method, userId, params, callback) {
                                     setTimeout(function () {
