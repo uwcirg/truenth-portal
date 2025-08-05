80ea1721324793a728d2a813a39e15ed53070ebd
Paul Bugni <pbugni@u.washington.edu>
2025-08-05 13:51:02 -0700

    extend `adherence-cache-test` CLI to validate against `qb_timeline`

--- manage.py
+++ manage.py
@@ -108,12 +108,20 @@ def upgrade_db():
 @click.option(
     '--reprocess', '-r', is_flag=True,
     help='Reprocess adherence cache for patients showing issues')
+@click.option(
+    '--research_study_id', '-s',
+    type=click.IntRange(0,1), default=0,
+    help='Reprocess adherence cache for patients showing issues')
 @app.cli.command()
-def adherence_cache_test(reprocess=False):
+def adherence_cache_test(research_study_id=0, reprocess=False):
     """Compare current adherence cache to trigger states, generate report"""
-    from portal.trigger_states.adherence_cache_validation import validate
-    validate(reprocess)
+    from portal.models.adherence_cache_timeline_validation import validate as timeline_validate
+    from portal.trigger_states.adherence_cache_validation import validate as ts_validate
 
+    if research_study_id == 1:
+        ts_validate(reprocess)
+
+    timeline_validate(research_study_id, reprocess)
 
 def flush_cache():
     """Flush redis of all values.
